f9739eaf80c7444545dec9b77861d8496ad20a89
Sami Vaarala <sami.vaarala@iki.fi>
2013-02-02 00:27:05 +0200

    catch binding test cases, illustrate current bug

---
+++ testcases/test-dev-bug-catch-binding-2.js
@@ -0,0 +1,30 @@
+/*===
+closure throw
+closure throw
+closure throw
+===*/
+
+/* This was broken at an early point.  Catch binding variable would be mapped
+ * to a certain register for the duration of the catch clause.  A closure
+ * created from the catch clause would work incorrectly once the original
+ * invocation exited the catch clause.
+ */
+
+function f() {
+    var e = 123;
+    var func;
+
+    try {
+        throw 'throw';
+    } catch (e) {
+        func = function() { print('closure', e); }
+        func();
+    }
+
+    func();
+
+    return func;
+}
+
+f()();
+
