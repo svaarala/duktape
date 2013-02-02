a2a4fbe9a5815684ccfbbef6035217d924bb4e14
Sami Vaarala <sami.vaarala@iki.fi>
2013-02-03 01:07:18 +0200

    testcase for E5 Section B.2.6

---
+++ testcases/test-dev-date-gmtutc-func.js
@@ -0,0 +1,15 @@
+
+/*===
+true
+===*/
+
+/*
+ *  Date.prototype.toGMTString is required to have the same Function
+ *  object as Date.prototype.toUTCString in E5 Section B.2.6.
+ *
+ *  Note that while Smjs respects this, V8 does not (the Function
+ *  objects are distinct).
+ */
+
+print(Date.prototype.toGMTString === Date.prototype.toUTCString);
+
