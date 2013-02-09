7a8c98106735c459a20e0e430292531971bf2215
Sami Vaarala <sami.vaarala@iki.fi>
2013-02-09 23:58:06 +0200

    tonumber valgrind issue might be a valgrind/libc issue

--- testcases/test-dev-bug-valgrind-tonumber-1.js
+++ testcases/test-dev-bug-valgrind-tonumber-1.js
@@ -1,4 +1,20 @@
 /* This caused a valgrind error at some point.
+ *
+ * This might actually be a libc/valgrind issue?  The following example
+ * will also cause similar valgrind complaints:
+ *
+ *   #include <stdio.h>
+ *   #include <string.h>
+ *   
+ *   int main(int argc, char *argv[]) {
+ *   	double d = 0.0;
+ *   	char buf[256];
+ *   
+ *   	memset(buf, 0, sizeof(buf));
+ *   	sprintf(buf, "%s", "NaN");
+ *   	sscanf(buf, "%lg", &d);
+ *   	printf("%lg\n", d);
+ *   }
  */
 
 /*===
