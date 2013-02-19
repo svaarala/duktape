e10b89006808894521254378faf048392c45b3db
Sami Vaarala <sami.vaarala@iki.fi>
2013-02-20 01:06:01 +0200

    remove negative tests from ISO 8601 test case, as an implementation is allowed to parse whatever else in addition to the standard subset (and we do)

--- testcases/test-builtin-date-parse-iso8601.js
+++ testcases/test-builtin-date-parse-iso8601.js
@@ -1,15 +1,13 @@
 
 /*===
-0 0 0 0 0 0 NaN NaN 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
--23215049510877 -23215049511000 -23215049520000 NaN -23215075200000 -23215507200000 -23225875200000
--23215049510877 -23215049511000 -23215049520000 NaN -23215075200000 -23215507200000 -23225875200000
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+-23215049510877 -23215049511000 -23215049520000 -23215075200000 -23215507200000 -23225875200000
+-23215049510877 -23215049511000 -23215049520000 -23215075200000 -23215507200000 -23225875200000
+1262401445006 1325473445006 1325432525006 1325514365006
 791942400000 791942400000
-981173106007 NaN NaN NaN NaN NaN
-981173106000 981173106123 NaN NaN NaN
 NaN -62198755200000 -62167219200000 -62135596800000 0 1260835200000 NaN
 275760 NaN -271821 NaN
 -3217862321754994 6249223278245006
--62167121754994 NaN
 NaN
 NaN
 ===*/
@@ -17,6 +15,41 @@ NaN
 /* ISO 8601 form parsing tests. */
 
 function iso8601ParseTest() {
+    // Note: the following are not valid abbreviations but since an
+    // implementation is allowed to parse them anyway, they should
+    // not be negative tests here.
+    //
+    // Date.parse('1970-01-01T00Z')
+    // Date.parse('1970-01-01T00')
+    // Date.parse('1234-05-06T07')
+    //
+    // Similarly, out-of-range values are not allowed in the E5.1
+    // ISO 8601 subset, but an implementation can parse them anyway
+    // (like we do).
+    //
+    // Date.parse('2001-02-03T04:05:06.007Z')
+    // Date.parse('2001-02-03T04:05:60.007Z')
+    // Date.parse('2001-02-03T04:60:06.007Z')
+    // Date.parse('2001-02-03T25:05:06.007Z')
+    // Date.parse('2001-02-29T04:05:06.007Z')
+    // Date.parse('2001-13-03T04:05:06.007Z')
+    //
+    // Same format issue with milliseconds: we parse non-standard
+    // millisecond counts too.
+    //
+    // Date.parse('2001-02-03T04:05:06Z')
+    // Date.parse('2001-02-03T04:05:06.123Z')
+    // Date.parse('2001-02-03T04:05:06.12Z')
+    // Date.parse('2001-02-03T04:05:06.1Z')
+    // Date.parse('2001-02-03T04:05:06.1234Z')
+    //
+    // Same format issue for year 0; extended format must have plus sign
+    // but we parse either.
+    //
+    // Date.parse('+000000-01-02T03:04:05.006Z')
+    // Date.parse('-000000-01-02T03:04:05.006Z')
+
+
     // 1970-01-01, some combinations
     print(Date.parse('1970-01-01T00:00:00.000Z'),
           Date.parse('1970-01-01T00:00:00.000'),
@@ -24,8 +57,6 @@ function iso8601ParseTest() {
           Date.parse('1970-01-01T00:00:00'),
           Date.parse('1970-01-01T00:00Z'),
           Date.parse('1970-01-01T00:00'),
-          Date.parse('1970-01-01T00Z'),    // this is not a valid abbreviation (if HH present. mm must also be) -> NaN
-          Date.parse('1970-01-01T00'),     // -> NaN
           Date.parse('1970-01-01Z'),
           Date.parse('1970-01-01'),
           Date.parse('1970-01Z'),
@@ -49,36 +80,25 @@ function iso8601ParseTest() {
     print(Date.parse('1234-05-06T07:08:09.123Z'),
           Date.parse('1234-05-06T07:08:09'),
           Date.parse('1234-05-06T07:08Z'),
-          Date.parse('1234-05-06T07'),   // -> NaN
           Date.parse('1234-05-06'),
           Date.parse('1234-05'),
           Date.parse('1234'));
     print(Date.parse('1234-05-06T07:08:09.123Z'),  // should default to these
           Date.parse('1234-05-06T07:08:09.000Z'),
           Date.parse('1234-05-06T07:08:00.000Z'),
-          NaN,
           Date.parse('1234-05-06T00:00:00.000Z'),
           Date.parse('1234-05-01T00:00:00.000Z'),
           Date.parse('1234-01-01T00:00:00.000Z'));
 
+    // Timezone part may be missing (interpreted as UTC), 'Z' (UTC), or
+    // given as a +/- HH:mm offset.
+    print(Date.parse('2010-01-02T03:04:05.006'),
+          Date.parse('2012-01-02T03:04:05.006Z'),
+          Date.parse('2012-01-02T03:04:05.006+11:22'),
+          Date.parse('2012-01-02T03:04:05.006-11:22'));
+
     // E5.1 Section 15.9.1.15 NOTE 1
     print(Date.parse('1995-02-04T24:00'), Date.parse('1995-02-05T00:00'));
-
-    // Other components out of valid range
-    print(Date.parse('2001-02-03T04:05:06.007Z'),
-          Date.parse('2001-02-03T04:05:60.007Z'),
-          Date.parse('2001-02-03T04:60:06.007Z'),
-          Date.parse('2001-02-03T25:05:06.007Z'),
-          Date.parse('2001-02-29T04:05:06.007Z'),  // 2001 not a leap year, Feb 29 is invalid
-          Date.parse('2001-13-03T04:05:06.007Z'));
-
-    // Milliseconds must either be missing or have exactly three digits
-    // FIXME: makes sense to parse these correctly though
-    print(Date.parse('2001-02-03T04:05:06Z'),
-          Date.parse('2001-02-03T04:05:06.123Z'),
-          Date.parse('2001-02-03T04:05:06.12Z'),
-          Date.parse('2001-02-03T04:05:06.1Z'),
-          Date.parse('2001-02-03T04:05:06.1234Z'));
 
     // Extended years (from E5.1 spec)
     print(Date.parse('-283457-03-21T15:00:00.008Z'),  // out of valid E5 range
@@ -99,10 +119,6 @@ function iso8601ParseTest() {
     // So, test some 6-digit years that are actually valid
     print(Date.parse('-100000-01-02T03:04:05.006Z'),
           Date.parse('+200000-01-02T03:04:05.006Z'));
-
-    // For year 0, extended format must have plus sign
-    print(Date.parse('+000000-01-02T03:04:05.006Z'),
-          Date.parse('-000000-01-02T03:04:05.006Z'));
 
     // If Date.parse() argument does not conform to the ISO 8601 subset
     // in the E5.1 specification, the implementation may fall back to
