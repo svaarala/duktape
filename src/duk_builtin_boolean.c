28aeef3050c4bd27ba8b90a270ec41344daf2510
Sami Vaarala <sami.vaarala@iki.fi>
2013-02-22 22:35:19 +0200

    preliminary implementation of Boolean constructor, to aid in test cases

--- src/duk_builtin_boolean.c
+++ src/duk_builtin_boolean.c
@@ -5,11 +5,24 @@
 #include "duk_internal.h"
 
 int duk_builtin_boolean_constructor(duk_context *ctx) {
+	duk_hobject *this;
+
+	duk_to_boolean(ctx, 0);
+
 	if (duk_is_constructor_call(ctx)) {
-		return DUK_RET_UNIMPLEMENTED_ERROR;  /*FIXME*/
-	} else {
-		return DUK_RET_UNIMPLEMENTED_ERROR;  /*FIXME*/
-	}
+		/* FIXME: helper; rely on Boolean.prototype as being non-writable, non-configurable */
+		duk_push_this(ctx);
+		this = duk_get_hobject(ctx, -1);
+		DUK_ASSERT(this != NULL);
+		DUK_ASSERT(this->prototype == ((duk_hthread *) ctx)->builtins[DUK_BIDX_BOOLEAN_PROTOTYPE]);
+
+		DUK_HOBJECT_SET_CLASS_NUMBER(this, DUK_HOBJECT_CLASS_BOOLEAN);
+
+		duk_dup(ctx, 0);  /* -> [ val obj val ] */
+		duk_def_prop_stridx(ctx, -2, DUK_HEAP_STRIDX_INT_VALUE, DUK_PROPDESC_FLAGS_NONE);  /* FIXME: proper flags? */
+	}  /* unbalanced stack */
+
+	return 1;
 }
 
 int duk_builtin_boolean_prototype_to_string(duk_context *ctx) {
