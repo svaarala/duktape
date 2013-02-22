d447f562b82239e1304753cdc4878481f10de3ea
Sami Vaarala <sami.vaarala@iki.fi>
2013-02-23 01:33:23 +0200

    testcase which demonstrated regexp compiler recursion limit bug

---
+++ testcases/test-dev-bug-regexp-comp-reclimit.js
@@ -0,0 +1,24 @@
+
+/*===
+compiled ok
+===*/
+
+/* The regexp compiler had a bug: recursion depth was incremented but never
+ * decremented.  Hence a sequence of disjunctions, while having a low maximum
+ * recursion depth, will trigger the recursion limit.
+ */
+try {
+    var t, i;
+    var r;
+
+    t = '^';
+    for (i = 0; i < 100; i++) {
+        t = t + '(?:a|b)';
+    }
+
+    r = new RegExp(t);
+    print('compiled ok');
+} catch (e) {
+    print(e.name);
+}
+
