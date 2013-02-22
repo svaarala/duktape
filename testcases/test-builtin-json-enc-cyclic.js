4d4a203a16ea87b37c805179ca5b2d9fc2b03386
Sami Vaarala <sami.vaarala@iki.fi>
2013-02-23 01:53:42 +0200

    clean up json cyclic structure test; add wide non-cyclic test

--- testcases/test-builtin-json-enc-cyclic.js
+++ testcases/test-builtin-json-enc-cyclic.js
@@ -6,35 +6,69 @@ TypeError
 
 /* TypeError is required for cyclic structures. */
 
-obj1 = { 'foo': 1 };
-obj1.cycle = obj1;
+function testCycle1() {
+    var obj = { foo: 1 };
+    obj.cycle = obj;
+    print(JSON.stringify(obj));
+}
 
-obj2a = { 'foo': 1 };
-obj2b = { 'bar': 2 };
-obj2a.cycle = obj2b;
-obj2b.cycle = obj2a;
-
-obj3a = { 'foo': 1 };
-obj3b = { 'bar': 2 };
-obj3c = { 'quux': 3 };
-obj3a.cycle = obj3b;
-obj3b.cycle = obj3c;
-obj3c.cycle = obj3a;
-
-try {
+function testCycle2() {
+    var obj1 = { foo: 1 };
+    var obj2 = { bar: 2 };
+    obj1.cycle = obj2;
+    obj2.cycle = obj1;
     print(JSON.stringify(obj1));
+}
+
+function testCycle3() {
+    var obj1 = { foo: 1 };
+    var obj2 = { bar: 2 };
+    var obj3 = { quux: 3 };
+    obj1.cycle = obj2;
+    obj2.cycle = obj3;
+    obj3.cycle = obj1;
+    print(JSON.stringify(obj1));
+}
+
+try {
+    testCycle1();
 } catch (e) {
     print(e.name);
 }
 
 try {
-    print(JSON.stringify(obj2a));
+    testCycle2();
 } catch (e) {
     print(e.name);
 }
 
 try {
-    print(JSON.stringify(obj3a));
+    testCycle3();
+} catch (e) {
+    print(e.name);
+}
+
+/*===
+{"foo":1,"bar":2,"ref1":{"quux":3,"baz":4,"ref1":{"quuux":5},"ref2":{"quuux":5}},"ref2":{"quux":3,"baz":4,"ref1":{"quuux":5},"ref2":{"quuux":5}}}
+===*/
+
+/* The following visits the same object multiple times, but not in a cycle.
+ * This test ensures that any loop stack or hash map is unwound correctly.
+ */
+
+function testNoCycle1() {
+    var obj1 = { foo: 1, bar: 2 };
+    var obj2 = { quux: 3, baz: 4 };
+    var obj3 = { quuux: 5 };
+    obj1.ref1 = obj2;
+    obj1.ref2 = obj2;
+    obj2.ref1 = obj3;
+    obj2.ref2 = obj3;
+    print(JSON.stringify(obj1));
+}
+
+try {
+    testNoCycle1();
 } catch (e) {
     print(e.name);
 }
