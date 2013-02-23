7745878770f7b2e50c99d4fa178a9f4e89838ac9
Sami Vaarala <sami.vaarala@iki.fi>
2013-02-23 22:50:08 +0200

    remove incorrect fixme

--- src/duk_util_misc.c
+++ src/duk_util_misc.c
@@ -4,7 +4,6 @@
 
 #include "duk_internal.h"
 
-/* FIXME: move to a shared location */
 char duk_nybbles[16] = { '0', '1', '2', '3', '4', '5', '6', '7',
                          '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
