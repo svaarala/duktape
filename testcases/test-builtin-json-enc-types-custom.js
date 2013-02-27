7d94f3327bc6c0297f3f5cedd627c405ebf78804
Sami Vaarala <sami.vaarala@iki.fi>
2013-02-27 14:27:26 +0200

    placeholder test case

---
+++ testcases/test-builtin-json-enc-types-custom.js
@@ -0,0 +1,16 @@
+
+/*---
+{
+    "custom": true,
+    "skip": true
+}
+---*/
+
+/* FIXME: add as soon as an API for accessing custom encoding is implemented. */
+
+try {
+    print('FIXME');
+} catch (e) {
+    print(e.name);
+}
+
