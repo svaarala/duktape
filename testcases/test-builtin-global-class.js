abb8d13de433db56ca64489ee1b53e556a90f4ae
Sami Vaarala <sami.vaarala@iki.fi>
2013-03-01 00:35:54 +0200

    move global object class test to separate file

---
+++ testcases/test-builtin-global-class.js
@@ -0,0 +1,22 @@
+
+/*---
+{
+    "custom": true
+}
+---*/
+
+// indirect eval -> this is bound to the global object, E5 Section 10.4.2, step 1.a.
+var g = (function () { var e = eval; return e('this'); } )();
+
+/*===
+[object global]
+===*/
+
+/* [[Class]] implementation defined, but we expect 'global' */
+
+try {
+    print(Object.prototype.toString.call(g));
+} catch (e) {
+    print(e.name);
+}
+
