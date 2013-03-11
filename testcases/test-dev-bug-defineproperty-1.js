d324ba764c83525abe88416c6a909af29e083912
Sami Vaarala <sami.vaarala@iki.fi>
2013-03-11 17:54:26 +0200

    update defineProperty() bug testcase description

--- testcases/test-dev-bug-defineproperty-1.js
+++ testcases/test-dev-bug-defineproperty-1.js
@@ -5,6 +5,13 @@ TypeError
 /* This should end up in TypeError; the property is non-configurable accessor,
  * and the inbound descriptor is a data descriptor (because of its writable
  * property).  There was a bug causing this to be silently ignored.
+ *
+ * The bug happened because the "writable" flag of the current property is
+ * "not defined" initially (accessor property) but is treated like "false"
+ * internally.  This causes the algorithm to determine that since writable
+ * is being changed from false to false, and enumerable/configurable are
+ * similarly not being changed, there is nothing to check.  The attempt to
+ * change the property from accessor to data property is thus not caught.
  */
 
 try {
