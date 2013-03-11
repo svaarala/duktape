2eb9929df898fb83fd8dc09d944639f3b8c72809
Sami Vaarala <sami.vaarala@iki.fi>
2013-03-11 13:08:43 +0200

    demonstrate getOwnPropertyNames() bug for String object non-enumerable 'length' property

---
+++ testcases/test-dev-bug-string-obj-enum.js
@@ -0,0 +1,19 @@
+
+/*===
+0 string 0
+1 string 1
+2 string 2
+3 string length
+===*/
+
+/* The virtual 'length' property of a string object (as opposed to a
+ * string value) was not included in getOwnPropertyNames() output.
+ */
+
+var obj = Object.getOwnPropertyNames(new String('foo'));
+var i;
+
+for (i = 0; i < obj.length; i++) {
+    print(i, typeof obj[i], obj[i]);
+}
+
