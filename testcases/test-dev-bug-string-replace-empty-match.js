7d081c453b05c881ac196fe82e689139849e22f3
Sami Vaarala <sami.vaarala@iki.fi>
2013-03-14 14:51:48 +0200

    testcase for a bug in String.prototype.replace()

---
+++ testcases/test-dev-bug-string-replace-empty-match.js
@@ -0,0 +1,16 @@
+
+/*===
+finished
+foo
+===*/
+
+/* Empty match with a global RegExp caused an infinite loop. */
+
+try {
+    var t = 'foo'.replace(/(?:)/g, '');
+    print('finished');
+    print(t);
+} catch (e) {
+    print(e);
+}
+
