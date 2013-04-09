b76966c356bdd003e7b6ba7aa48785ad209bb8d8
Sami Vaarala <sami.vaarala@iki.fi>
2013-04-09 17:06:27 +0300

    improve numconv implementation; now pretty much works for minimal length output

--- src/duk_numconv.c
+++ src/duk_numconv.c
@@ -4,25 +4,26 @@
  *  Number-to-string conversion is based on a Dragon4 variant,
  *  with a fast path for radix 10 integers.  Limited big integer
  *  arithmetic is needed for guaranteeing that the conversion is
- *  correct and uses a minimum number of digits.  Much faster
- *  algorithms exist (e.g. one could use Grisu3 + Dragon4 hybrid
- *  with much better average performance) but that would increase
- *  code footprint considerably.
+ *  correct and uses a minimum number of digits.
+ *
+ *  See: doc/number_conversion.txt.
  */
 
 #include "duk_internal.h"
 
 #define  IEEE_DOUBLE_EXP_BIAS  1023
+#define  IEEE_DOUBLE_EXP_MIN   (-1022)   /* biased exp == 0 -> denormal, exp -1022 */
 
 /*
  *  Limited functionality bigint implementation.  Restricted to non-negative
- *  numbers with less than 32 * MAX_PARTS bits.  Operations have been tailored
- *  for number conversion needs.
+ *  numbers with less than 32 * BI_MAX_PARTS bits, with the caller responsible
+ *  for ensuring this is never exceeded.  Operations have been tailored for
+ *  number conversion needs.
  */
 
-#define  MAX_PARTS  35  /* 35x32 = 1120 bits */
+#define  BI_MAX_PARTS  35  /* 35x32 = 1120 bits */
 
-#ifdef DUK_USE_DDDEBUG
+#ifdef DUK_USE_DEBUG
 #define  BI_PRINT(name,x)  bi_print((name),(x))
 #else
 #define  BI_PRINT(name,x)
@@ -31,10 +32,10 @@
 /* Current size is about 144 bytes. */
 typedef struct {
 	int n;
-	uint32_t v[MAX_PARTS];  /* low to high */
+	uint32_t v[BI_MAX_PARTS];  /* low to high */
 } duk_bigint;
 
-#ifdef DUK_USE_DDDEBUG
+#ifdef DUK_USE_DEBUG
 static void bi_print(const char *name, duk_bigint *x) {
 	char buf[1024];
 	char *p = buf;
@@ -48,11 +49,11 @@ static void bi_print(const char *name, duk_bigint *x) {
 		p += sprintf(p, " %08x", (unsigned int) x->v[i]);
 	}
 
-	printf("%s: %s\n", name, buf);
+	DUK_DPRINT("%s: %s", name, buf);
 }
 #endif
 
-#if 0  /* FIXME: unused */
+#ifdef DUK_USE_ASSERTIONS
 static int bi_is_normalized(duk_bigint *x) {
 	return (x->n == 0) || (x->v[x->n - 1] != 0);
 }
@@ -69,6 +70,7 @@ static void bi_normalize(duk_bigint *x) {
 
 	/* Note: if 'x' is zero, x->n becomes 0 here */
 	x->n = i + 1;
+	DUK_ASSERT(bi_is_normalized(x));
 }
 
 /* y <- x */
@@ -90,12 +92,16 @@ static void bi_set_small(duk_bigint *x, uint32_t v) {
 		x->n = 1;
 		x->v[0] = v;
 	}
+	DUK_ASSERT(bi_is_normalized(x));
 }
 
 /* z <- x+y */
 static void bi_add(duk_bigint *x, duk_bigint *y, duk_bigint *z) {
 	uint64_t tmp;
 	int i, nx, ny;
+
+	DUK_ASSERT(bi_is_normalized(x));
+	DUK_ASSERT(bi_is_normalized(y));
 
 	if (y->n > x->n) {
 		duk_bigint *t;
@@ -106,6 +112,7 @@ static void bi_add(duk_bigint *x, duk_bigint *y, duk_bigint *z) {
 	nx = x->n; ny = y->n;
 	tmp = 0;
 	for (i = 0; i < nx; i++) {
+		DUK_ASSERT(i < BI_MAX_PARTS);
 		tmp += x->v[i];
 		if (i < ny) {
 			tmp += y->v[i];
@@ -114,12 +121,14 @@ static void bi_add(duk_bigint *x, duk_bigint *y, duk_bigint *z) {
 		tmp = tmp >> 32;
 	}
 	if (tmp != 0) {
-		/* FIXME: assert size */
+		DUK_ASSERT(i < BI_MAX_PARTS);
 		z->v[i++] = (uint32_t) tmp;
 	}
 	z->n = i;
+	DUK_ASSERT(z->n < BI_MAX_PARTS);
 
 	/* no need to normalize */
+	DUK_ASSERT(bi_is_normalized(z));
 }
 
 /* z <- x-y, require x >= y => z >= 0 */
@@ -128,6 +137,9 @@ static void bi_sub(duk_bigint *x, duk_bigint *y, duk_bigint *z) {
 	int i;
 	uint32_t tx, ty;
 	int64_t tmp;
+
+	DUK_ASSERT(bi_is_normalized(x));
+	DUK_ASSERT(bi_is_normalized(y));
 
 	nx = x->n;
 	ny = y->n;
@@ -149,6 +161,7 @@ static void bi_sub(duk_bigint *x, duk_bigint *y, duk_bigint *z) {
 
 	z->n = i;
 	bi_normalize(z);  /* need to normalize, may even cancel to 0 */
+	DUK_ASSERT(bi_is_normalized(z));
 }
 
 /* z <- x*y */
@@ -156,8 +169,11 @@ static void bi_mul(duk_bigint *x, duk_bigint *y, duk_bigint *z) {
 	int i, j, ny, nz;
 	uint64_t tmp;
 
+	DUK_ASSERT(bi_is_normalized(x));
+	DUK_ASSERT(bi_is_normalized(y));
+
 	nz = x->n + y->n;  /* max possible */
-	/* FIXME: ASSERT nz */
+	DUK_ASSERT(nz < BI_MAX_PARTS);
 
 	if (nz == 0) {
 		z->n = 0;
@@ -176,18 +192,23 @@ static void bi_mul(duk_bigint *x, duk_bigint *y, duk_bigint *z) {
 			tmp = tmp >> 32;
 		}
 		if (tmp > 0) {
-			/* FIXME: i+j < nz */
+			DUK_ASSERT(i + j < nz);
+			DUK_ASSERT(i + j < BI_MAX_PARTS);
 			z->v[i+j] += (uint32_t) tmp;
 		}
 	}
 
 	bi_normalize(z);
+	DUK_ASSERT(bi_is_normalized(z));
 }
 
 /* z <- x*y */
 static void bi_mul_small(duk_bigint *x, uint32_t y, duk_bigint *z) {
-	/* FIXME: optimize */
 	duk_bigint tmp;
+
+	DUK_ASSERT(bi_is_normalized(x));
+
+	/* FIXME: optimize */
 	if (y == 0) {
 		tmp.n = 0;
 	} else {
@@ -195,6 +216,8 @@ static void bi_mul_small(duk_bigint *x, uint32_t y, duk_bigint *z) {
 		tmp.v[0] = y;
 	}
 	bi_mul(x, &tmp, z);
+
+	DUK_ASSERT(bi_is_normalized(z));
 }
 
 #if 0  /* FIXME: unused */
@@ -234,7 +257,9 @@ static int bi_compare(duk_bigint *x, duk_bigint *y) {
 	int nx, ny;
 	uint32_t tx, ty;
 
-	/* FIXME: assert normalized */
+	DUK_ASSERT(bi_is_normalized(x));
+	DUK_ASSERT(bi_is_normalized(y));
+
 	nx = x->n;
 	ny = y->n;
 	if (nx > ny) {
@@ -265,10 +290,29 @@ static int bi_compare(duk_bigint *x, duk_bigint *y) {
 }
 
 static int bi_is_even(duk_bigint *x) {
+	DUK_ASSERT(bi_is_normalized(x));
 	return (x->n == 0) || ((x->v[0] & 0x01) == 0);
 }
 
-#if 0  /*FIXME*/
+#if 0  /* FIXME: unused */
+static int bi_is_zero(duk_bigint *x) {
+	DUK_ASSERT(bi_is_normalized(x));
+	return (x->n == 0);  /* this is the case for normalized numbers */
+}
+#endif
+
+/* Bigint is 2^52.  Used to detect normalized IEEE double mantissa values
+ * which are at the lowest edge (next floating point value downwards has
+ * a different exponent).  The lowest mantissa has the form:
+ *
+ *     1000........000    (52 zeroes; only "hidden bit" is set)
+ */
+static int bi_is_2to52(duk_bigint *x) {
+	DUK_ASSERT(bi_is_normalized(x));
+	return (x->n == 2) && (x->v[0] == 0) && (x->v[1] == (1 << (52-32)));
+}
+
+#if 0  /* FIXME: not implemented, would be useful */
 static void bi_shift(duk_bigint *x) {
 }
 #endif
@@ -278,7 +322,7 @@ static void bi_twoexp(duk_bigint *x, int y) {
 	int n, r;
 
 	n = (y / 32) + 1;
-	/* FIXME: n > 0 */
+	DUK_ASSERT(n > 0);
 	r = y % 32;
 	DUK_DPRINT("twoexp: y=%d, n=%d, r=%d", y, n, r);
 	memset((void *) x->v, 0, sizeof(uint32_t) * n);
@@ -321,7 +365,7 @@ typedef struct {
 } duk_numconv_stringify_ctx;
 
 #define  NUMCONV_PUTCHAR(nc_ctx,x)  do { \
-		DUK_ASSERT((nc_ctx)->out_p < (nc_ctx)->out_buf); \
+		DUK_ASSERT((nc_ctx)->out_p < ((nc_ctx)->out_buf + sizeof((nc_ctx)->out_buf))); \
 		*((nc_ctx)->out_p++) = (x); \
 	} while(0)
 
@@ -333,7 +377,7 @@ char digits[36] = {
 	'u','v','w','x','y','z'
 };
 
-static void convert_double(duk_numconv_stringify_ctx *nc_ctx) {
+static void dragon4_convert_double(duk_numconv_stringify_ctx *nc_ctx) {
 	/* FIXME: share duk_tval.h? */
 	volatile union {
 		double d;
@@ -366,7 +410,7 @@ static void convert_double(duk_numconv_stringify_ctx *nc_ctx) {
 	exp = (u.v[1] >> 20) & 0x07ffU;
 	if (exp == 0) {
 		/* denormal */
-		exp = -1022 - 52;
+		exp = IEEE_DOUBLE_EXP_MIN - 52;
 	} else {
 		/* normal: implicit leading 1-bit */
 		nc_ctx->f.v[1] |= 0x00100000U;
@@ -376,7 +420,13 @@ static void convert_double(duk_numconv_stringify_ctx *nc_ctx) {
 	nc_ctx->e = exp;
 }
 
-static void prepare(duk_numconv_stringify_ctx *nc_ctx) {
+static void dragon4_prepare(duk_numconv_stringify_ctx *nc_ctx) {
+#if 1
+	/* Assume IEEE round-to-even, so that shorter encoding can be used
+	 * when round-to-even would produce correct result.  By removing
+	 * this check (and having low_ok == high_ok == 0) the results would
+	 * still be accurate but in some cases longer than necessary.
+	 */
 	if (bi_is_even(&nc_ctx->f)) {
 		DUK_DPRINT("f is even");
 		nc_ctx->low_ok = 1;
@@ -386,15 +436,26 @@ static void prepare(duk_numconv_stringify_ctx *nc_ctx) {
 		nc_ctx->low_ok = 0;
 		nc_ctx->high_ok = 0;
 	}
+#else
+	/* Note: not honoring round-to-even should work but now generates incorrect
+	 * results.  For instance, 1e23 serializes to "a000...", i.e. the first digit
+	 * equals the radix (10).  Scaling stops one step too early in this case.
+	 * Don't know why this is the case, but since this code path is unused, it
+	 * doesn't matter.
+	 */
+	nc_ctx->low_ok = 0;
+	nc_ctx->high_ok = 0;
+#endif
 
 	if (nc_ctx->e >= 0) {
-		if (0/*FIXME*/) {
+		/* exponent non-negative (and thus not minimum exponent) */
+		if (bi_is_2to52(&nc_ctx->f)) {
 			/* (>= e 0) AND (= f (expt b (- p 1)))
 			 *
 			 * be <- (expt b e) == b^e
 			 * be1 <- (* be b) == (expt b (+ e 1)) == b^(e+1)
-			 * r <- (* f be1 2) == 2 * f * b^(e+1)     FIXME: if b==2 -> f * b^(e+2)
-			 * s <- (* b 2)   FIXME: if b==2 -> 4
+			 * r <- (* f be1 2) == 2 * f * b^(e+1) ;  if b==2 -> f * b^(e+2)
+			 * s <- (* b 2) ;  if b==2 -> 4
 			 * m+ <- be1 == b^(e+1)
 			 * m- <- be == b^e
 			 * k <- 0
@@ -402,7 +463,10 @@ static void prepare(duk_numconv_stringify_ctx *nc_ctx) {
 			 * low_ok <- round
 			 * high_ok <- round
 			 */
-			DUK_DPRINT("e >= 0 AND (= f (expt b (- p 1)))");
+
+			DUK_DPRINT("non-negative exponent (not smallest exponent); "
+			           "lowest mantissa value for this exponent -> "
+			           "unequal gaps");
 
 			bi_twoexp(&nc_ctx->t1, nc_ctx->e + 2);
 			bi_mul(&nc_ctx->f, &nc_ctx->t1, &nc_ctx->r);
@@ -413,7 +477,7 @@ static void prepare(duk_numconv_stringify_ctx *nc_ctx) {
 			/* (>= e 0) AND (not (= f (expt b (- p 1))))
 			 *
 			 * be <- (expt b e) == b^e
-			 * r <- (* f be 2) == 2 * f * b^e     FIXME: if b==2 -> f * b^(e+1)
+			 * r <- (* f be 2) == 2 * f * b^e ;  if b==2 -> f * b^(e+1)
 			 * s <- 2
 			 * m+ <- be == b^e
 			 * m- <- be == b^e
@@ -422,7 +486,10 @@ static void prepare(duk_numconv_stringify_ctx *nc_ctx) {
 			 * low_ok <- round
 			 * high_ok <- round
 			 */
-			DUK_DPRINT("e >= 0 AND (not (= f (expt b (- p 1))))");
+
+			DUK_DPRINT("non-negative exponent (not smallest exponent); "
+			           "not lowest mantissa for this exponent -> "
+			           "equal gaps");
 
 			bi_twoexp(&nc_ctx->t1, nc_ctx->e + 1);
 			bi_mul(&nc_ctx->f, &nc_ctx->t1, &nc_ctx->r);        /* r <- (* 2 f (expt b e)) = (* f (expt b (+ e 1))) */
@@ -432,25 +499,10 @@ static void prepare(duk_numconv_stringify_ctx *nc_ctx) {
 			bi_copy(&nc_ctx->t1, &nc_ctx->mm);
 		}
 	} else {
-		if (1/*FIXME*/ || 0/*FIXME*/) {
-			/* r <- (* f 2)
-			 * s <- (* (expt b (- e)) 2) == b^(1-e)
-			 * m+ <- b == 1
-			 * m- <- 1
-			 * k <- 0
-			 * B <- B
-			 * low_ok <- round
-			 * high_ok <- round
-			 */
-			DUK_DPRINT("e < 0 AND FIXME");
-
-			bi_mul_small(&nc_ctx->f, 2, &nc_ctx->r);
-			bi_twoexp(&nc_ctx->s, 1 - nc_ctx->e);
-			bi_set_small(&nc_ctx->mp, 1);
-			bi_set_small(&nc_ctx->mm, 1);
-		} else {
+		if (nc_ctx->e > IEEE_DOUBLE_EXP_MIN /*not minimum exponent*/ &&
+		    bi_is_2to52(&nc_ctx->f) /* lowest mantissa for this exponent*/) {
 			/* r <- (* f b 2) == (* f 4)
-			 * s <- (* (expt b (- 1 e)) 2) == b^(1-e) * 2 == b^(2-e)
+			 * s <- (* (expt b (- 1 e)) 2) == b^(1-e) * 2 ;  if b==2 -> b^(2-e)
 			 * m+ <- b == 2
 			 * m- <- 1
 			 * k <- 0
@@ -458,21 +510,61 @@ static void prepare(duk_numconv_stringify_ctx *nc_ctx) {
 			 * low_ok <- round
 			 * high_ok <- round
 			 */
-			DUK_DPRINT("e < 0 AND FIXME");
+
+			DUK_DPRINT("negative exponent; not minimum exponent and "
+			           "lowest mantissa for this exponent -> "
+			           "unequal gaps");
 
 			bi_mul_small(&nc_ctx->f, 4, &nc_ctx->r);
 			bi_twoexp(&nc_ctx->s, 2 - nc_ctx->e);
 			bi_set_small(&nc_ctx->mp, 2);
 			bi_set_small(&nc_ctx->mm, 1);
+		} else {
+			/* r <- (* f 2)
+			 * s <- (* (expt b (- e)) 2) == b^(-e) * 2 ;  if b==2 -> b^(1-e)
+			 * m+ <- b == 1
+			 * m- <- 1
+			 * k <- 0
+			 * B <- B
+			 * low_ok <- round
+			 * high_ok <- round
+			 */
+
+			DUK_DPRINT("negative exponent; minimum exponent or not "
+			           "lowest mantissa for this exponent -> "
+			           "equal gaps");
+
+			bi_mul_small(&nc_ctx->f, 2, &nc_ctx->r);
+			bi_twoexp(&nc_ctx->s, 1 - nc_ctx->e);
+			bi_set_small(&nc_ctx->mp, 1);
+			bi_set_small(&nc_ctx->mm, 1);
 		}
 	}
 }
 
-static void scale(duk_numconv_stringify_ctx *nc_ctx) {
+static void dragon4_scale(duk_numconv_stringify_ctx *nc_ctx) {
 	int k = 0;
 
-	/* FIXME: this loop can only go one way, i.e. increase k until it's
-	 * correct, or decrease k until it's correct.
+	/* This is essentially the 'scale' algorithm, with recursion removed.
+	 * Note that 'k' is either correct immediately, or will move in one
+	 * direction in the loop.  There's no need to do the low/high checks
+	 * on every round (like the Scheme algorithm does).
+	 *
+	 * The scheme algorithm finds 'k' and updates 's' simultaneously,
+	 * while the logical algorithm finds 'k' with 's' having its initial
+	 * value, after which 's' is updated separately (see the Burger-Dybvig
+	 * paper, Section 3.1, steps 2 and 3).
+	 */
+
+	/* FIXME: this algorithm could be optimized quite a lot by using e.g.
+	 * a logarithm based estimator for 'k' and performing B^n multiplication
+	 * using a lookup table or using some bit-representation based exp
+	 * algorithm.  Currently we just loop, with significant performance
+	 * impact for very large and very small numbers.
+	 */
+
+	/* FIXME: optimize for the case when mp === mm.  This is a VERY likely case
+	 * and saves on bigint arithmetic.
 	 */
 
 	DUK_DPRINT("scale: B=%d, low_ok=%d, high_ok=%d", nc_ctx->B, nc_ctx->low_ok, nc_ctx->high_ok);
@@ -482,7 +574,7 @@ static void scale(duk_numconv_stringify_ctx *nc_ctx) {
 	BI_PRINT("mm(init)", &nc_ctx->mm);
 
 	for (;;) {
-		DUK_DPRINT("scale loop, k=%d", k);
+		DUK_DPRINT("scale loop (inc k), k=%d", k);
 		BI_PRINT("r", &nc_ctx->r);
 		BI_PRINT("s", &nc_ctx->s);
 		BI_PRINT("m+", &nc_ctx->mp);
@@ -500,9 +592,24 @@ static void scale(duk_numconv_stringify_ctx *nc_ctx) {
 
 			bi_mul_small_copy(&nc_ctx->s, nc_ctx->B, &nc_ctx->t1);
 			k++;
-			continue;
+		} else {
+			break;
 		}
+	}
 
+	/* k > 0 -> k was too low, and cannot be too high */
+	if (k > 0) {
+		goto skip_dec_k;
+	}
+
+	for (;;) {
+		DUK_DPRINT("scale loop (dec k), k=%d", k);
+		BI_PRINT("r", &nc_ctx->r);
+		BI_PRINT("s", &nc_ctx->s);
+		BI_PRINT("m+", &nc_ctx->mp);
+		BI_PRINT("m-", &nc_ctx->mm);
+
+		bi_add(&nc_ctx->r, &nc_ctx->mp, &nc_ctx->t1);  /* t1 = (+ r m+) */
 		bi_mul_small(&nc_ctx->t1, nc_ctx->B, &nc_ctx->t2);   /* t2 = (* (+ r m+) B) */
 		if (bi_compare(&nc_ctx->t2, &nc_ctx->s) <= (nc_ctx->high_ok ? -1 : 0)) {
 			DUK_DPRINT("k is too high");
@@ -516,18 +623,18 @@ static void scale(duk_numconv_stringify_ctx *nc_ctx) {
 			bi_mul_small_copy(&nc_ctx->mp, nc_ctx->B, &nc_ctx->t1);
 			bi_mul_small_copy(&nc_ctx->mm, nc_ctx->B, &nc_ctx->t1);
 			k--;
-			continue;
+		} else {
+			break;
 		}
-
-		DUK_DPRINT("k is correct");
-		break;
 	}
+
+ skip_dec_k:
 
 	DUK_DPRINT("final k: %d", k);
 	nc_ctx->k = k;
 }
 
-static void output(duk_numconv_stringify_ctx *nc_ctx, int d, int n) {
+static void dragon4_output(duk_numconv_stringify_ctx *nc_ctx, int d, int n) {
 	int i;
 	int pos;
 
@@ -549,7 +656,7 @@ static void output(duk_numconv_stringify_ctx *nc_ctx, int d, int n) {
 	nc_ctx->out_first = 0;
 }
 
-void finish(duk_numconv_stringify_ctx *nc_ctx, int n) {
+static void dragon4_finish(duk_numconv_stringify_ctx *nc_ctx, int n) {
 	int pos;
 
 	if (nc_ctx->k >= 1) {
@@ -565,7 +672,7 @@ void finish(duk_numconv_stringify_ctx *nc_ctx, int n) {
 	}
 }
 
-void generate(duk_numconv_stringify_ctx *nc_ctx) {
+static void dragon4_generate(duk_numconv_stringify_ctx *nc_ctx) {
 	int tc1, tc2;
 	int d;
 	int count = 0;
@@ -615,17 +722,17 @@ void generate(duk_numconv_stringify_ctx *nc_ctx) {
 				bi_mul_small(&nc_ctx->r, 2, &nc_ctx->t1);  /* FIXME: shift */
 				if (bi_compare(&nc_ctx->t1, &nc_ctx->s) < 0) {  /* (< (* r 2) s) */
 					DUK_DPRINT("tc1=true, tc2=true, 2r > s: output d --> %d (k=%d)", d, nc_ctx->k);
-					output(nc_ctx, d, count);
+					dragon4_output(nc_ctx, d, count);
 				} else {
 					DUK_DPRINT("tc1=true, tc2=true, 2r <= s: output d+1 --> %d (k=%d)", d + 1, nc_ctx->k);
-					output(nc_ctx, d + 1, count);
+					dragon4_output(nc_ctx, d + 1, count);
 				}
 				count++;
 				break;
 			} else {
 				/* tc1 = true, tc2 = false */
 				DUK_DPRINT("tc1=true, tc2=false: output d --> %d (k=%d)", d, nc_ctx->k);
-				output(nc_ctx, d, count);
+				dragon4_output(nc_ctx, d, count);
 				count++;
 				break;
 			}
@@ -633,14 +740,14 @@ void generate(duk_numconv_stringify_ctx *nc_ctx) {
 			if (tc2) {
 				/* tc1 = false, tc2 = true */
 				DUK_DPRINT("tc1=false, tc2=true: output d+1 --> %d (k=%d)", d + 1, nc_ctx->k);
-				output(nc_ctx, d + 1, count);
+				dragon4_output(nc_ctx, d + 1, count);
 				count++;
 				break;
 			} else {
 				/* tc1 = false, tc2 = false */
 
 				DUK_DPRINT("tc1=false, tc2=false: output d --> %d (k=%d)", d, nc_ctx->k);
-				output(nc_ctx, d, count);
+				dragon4_output(nc_ctx, d, count);
 				count++;
 
 				/* r <- r    (updated above: r <- (remainder (* r B) s)
@@ -664,7 +771,7 @@ void generate(duk_numconv_stringify_ctx *nc_ctx) {
 
 	DUK_DPRINT("generate finished");
 
-	finish(nc_ctx, count);
+	dragon4_finish(nc_ctx, count);
 }
 
 /*
@@ -728,13 +835,18 @@ void duk_numconv_stringify(duk_context *ctx, double x, int radix, int digits) {
 	 *  normal finite values have an implicit leading 1-bit.
 	 */
 
+	/* Would be nice to do this, but the context is 1-2 kilobytes and
+	 * nothing should rely on it being zeroed.
+	 */
+#if 0
 	memset((void *) nc_ctx, 0, sizeof(*nc_ctx));  /* slow init, do only for slow path cases */
+#endif
+	nc_ctx->x = x;
 	nc_ctx->B = radix;
 	nc_ctx->out_first = 1;
-	nc_ctx->x = x;
 	nc_ctx->out_p = nc_ctx->out_buf;
 
-	convert_double(nc_ctx);   /* -> sets 'f' and 'e' */
+	dragon4_convert_double(nc_ctx);   /* -> sets 'f' and 'e' */
 	BI_PRINT("f", &nc_ctx->f);
 	DUK_DPRINT("e=%d", nc_ctx->e);
 
@@ -742,7 +854,7 @@ void duk_numconv_stringify(duk_context *ctx, double x, int radix, int digits) {
 	 *  Preparation part (flonum->digits).
 	 */
 
-	prepare(nc_ctx);  /* setup many variables in nc_ctx */
+	dragon4_prepare(nc_ctx);  /* setup many variables in nc_ctx */
 
 	DUK_DPRINT("after prepare:");
 	BI_PRINT("r", &nc_ctx->r);
@@ -754,7 +866,7 @@ void duk_numconv_stringify(duk_context *ctx, double x, int radix, int digits) {
 	 *  Scale part.
 	 */
 
-	scale(nc_ctx);
+	dragon4_scale(nc_ctx);
 
 	DUK_DPRINT("after scale; k=%d", nc_ctx->k);
 	BI_PRINT("r", &nc_ctx->r);
@@ -770,7 +882,9 @@ void duk_numconv_stringify(duk_context *ctx, double x, int radix, int digits) {
 		NUMCONV_PUTCHAR(nc_ctx, '-');
 	}
 
-	generate(nc_ctx);
+	dragon4_generate(nc_ctx);
+
+	NUMCONV_PUTCHAR(nc_ctx, '\0');
 
 	/*
 	 *  Push final string.
