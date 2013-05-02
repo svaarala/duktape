4b036bf54a74e868d13fb8d0845710677c2b978e
Sami Vaarala <sami.vaarala@iki.fi>
2013-05-02 21:06:17 +0300

    numconv test cases

---
+++ testcases/test-numconv-tostring-misc.js
@@ -0,0 +1,18 @@
+
+/*===
+1e+23
+===*/
+
+function miscTest() {
+    // Burger, Dybvig: "Printing Floating-Point Numbers Quickly
+    // and Accurately", section 3.1.  IEEE round-to-even allows
+    // this to be printed as "1e+23" instead of "9.999...".
+    print(1e23);
+}
+
+try {
+    miscTest();
+} catch (e) {
+    print(e);
+}
+
