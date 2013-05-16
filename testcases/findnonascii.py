7a2cbdad30e8c13359289fb93c3e0ca459e3fe01
Sami Vaarala <sami.vaarala@iki.fi>
2013-05-17 00:48:16 +0300

    improve nonascii find util

--- testcases/findnonascii.py
+++ testcases/findnonascii.py
@@ -14,7 +14,7 @@ for linenum, linedata in enumerate(data.split('\n')):
 	for i in xrange(len(linedata)):
 		x = ord(linedata[i])
 		if x >= 0x80:
+			print '%s: non-ascii data on line %d, char index %d, value %d (0x%02x)' % \
+				(sys.argv[1], linenum + 1, i + 1, x, x)
 			non_ascii = True
-	if non_ascii:
-		print '%s: non-ascii data on line %d' % (sys.argv[1], linenum + 1)
 
