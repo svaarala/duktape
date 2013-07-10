60096d4dba29c958ceb2deaa99226f95ed879cb9
Sami Vaarala <sami.vaarala@iki.fi>
2013-07-10 20:25:56 +0300

    api testcase 'header' file, prepended to each test

---
+++ runtests/api_testcase_main.c
@@ -0,0 +1,26 @@
+/*
+ *  Wrapper for running an API test case.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "duk_api.h"
+
+extern void test(duk_context *ctx);
+
+int main(int argc, char *argv[]) {
+	duk_context *ctx = NULL;
+
+	/* FIXME: resource limits */
+
+	ctx = duk_create_heap_default();
+	if (!ctx) {
+		fprintf(stderr, "cannot allocate heap for testcase\n");
+		exit(1);
+	}
+
+	test(ctx);
+
+	duk_destroy_heap(ctx);
+}
+
