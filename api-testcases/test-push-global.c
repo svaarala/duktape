765ea223dc3cd91bf217e376386524949d4b4e57
Sami Vaarala <sami.vaarala@iki.fi>
2013-07-12 00:44:51 +0300

    api doc and test for duk_push_global_object

---
+++ api-testcases/test-push-global.c
@@ -0,0 +1,18 @@
+/*===
+type=6
+ToString=[object global]
+duk_is_function(global.escape)=1
+===*/
+
+void test(duk_context *ctx) {
+	duk_push_global_object(ctx);
+	printf("type=%d\n", duk_get_type(ctx, -1));
+	printf("ToString=%s\n", duk_to_string(ctx, -1));
+	duk_pop(ctx);
+
+	duk_push_global_object(ctx);
+	duk_get_prop_string(ctx, -1, "escape");
+	printf("duk_is_function(global.escape)=%d\n", duk_is_function(ctx, -1));
+	duk_pop(ctx);
+}
+
