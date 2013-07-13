f6abce34c958e83808dd2a89d22587ce4600c48c
Sami Vaarala <sami.vaarala@iki.fi>
2013-07-14 01:47:22 +0300

    api doc and test for duk_throw

---
+++ api-testcases/test-throw.c
@@ -0,0 +1,17 @@
+/*===
+rc=1 -> throw me
+===*/
+
+int test_1(duk_context *ctx) {
+	duk_push_string(ctx, "throw me");
+	duk_throw(ctx);
+	return 0;
+}
+
+void test(duk_context *ctx) {
+	int rc;
+
+	rc = duk_safe_call(ctx, test_1, 0, 1, DUK_INVALID_INDEX);
+	printf("rc=%d -> %s\n", rc, duk_get_string(ctx, -1));
+}
+
