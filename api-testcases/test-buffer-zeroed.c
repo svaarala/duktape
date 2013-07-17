8298431242a9496bb86b4e1671d01fe562987467
Sami Vaarala <sami.vaarala@iki.fi>
2013-07-17 22:58:05 +0300

    api test for checking that buffers are zeroed

---
+++ api-testcases/test-buffer-zeroed.c
@@ -0,0 +1,26 @@
+/*===
+fixed: 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+dynamic: 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+===*/
+
+/* Test that buffers pushed to the stack are automatically zeroed. */
+
+void test(duk_context *ctx) {
+	int i;
+	unsigned char *p;
+
+	p = (unsigned char *) duk_push_new_fixed_buffer(ctx, 32);
+	printf("fixed:");
+	for (i = 0; i < 32; i++) {
+		printf(" %d", (int) p[i]);
+	}
+	printf("\n");
+
+	p = (unsigned char *) duk_push_new_dynamic_buffer(ctx, 32);
+	printf("dynamic:");
+	for (i = 0; i < 32; i++) {
+		printf(" %d", (int) p[i]);
+	}
+	printf("\n");
+}
+
