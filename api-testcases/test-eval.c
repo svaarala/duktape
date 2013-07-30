c78341220f63c5c4db3105c3fcb5621fdc69cc7b
Sami Vaarala <sami.vaarala@iki.fi>
2013-07-30 13:53:11 +0300

    add function expression test to eval test

--- api-testcases/test-eval.c
+++ api-testcases/test-eval.c
@@ -1,14 +1,22 @@
 /*===
 Hello world!
 return value is: 123.000000
+adder(123, 234) -> 357.000000
 ===*/
 
-/* From API doc */
-
 void test(duk_context *ctx) {
+	/* From API doc */
 	duk_push_string(ctx, "print('Hello world!'); 123;");
 	duk_eval(ctx);
 	printf("return value is: %lf\n", duk_get_number(ctx, -1));
 	duk_pop(ctx);
-}
 
+	/* Function expression */
+	duk_push_string(ctx, "(function adder(x,y) { return x+y; })");
+	duk_eval(ctx);  /* [ func ] */
+	duk_push_int(ctx, 123);
+	duk_push_int(ctx, 234);
+	duk_call(ctx, 2);  /* [ func 123 234 ] -> [ result ] */
+	printf("adder(123, 234) -> %lf\n", duk_get_number(ctx, -1));
+	duk_pop(ctx);
+}
