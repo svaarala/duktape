b7d118045b979359b20f212a98c7f61d7fbc0191
Sami Vaarala <sami.vaarala@iki.fi>
2013-08-02 17:21:27 +0300

    api doc and test for duk_push_current_function

---
+++ api-testcases/test-push-current-function.c
@@ -0,0 +1,46 @@
+/*===
+no running function
+type=1
+duk_is_object: 0
+basic case
+my_func, top=1
+type=6
+duk_is_object: 1
+final top: 2
+rc=0, result='undefined'
+===*/
+
+int my_func(duk_context *ctx) {
+	printf("my_func, top=%d\n", duk_get_top(ctx));
+
+	duk_push_current_function(ctx);
+	printf("type=%d\n", duk_get_type(ctx, -1));
+	printf("duk_is_object: %d\n", duk_is_object(ctx, -1));
+
+	/* FIXME: get the underlying function and compare against 'my_func' */
+
+	printf("final top: %d\n", duk_get_top(ctx));
+	return 0;
+}
+
+void test(duk_context *ctx) {
+	int rc;
+
+	/* first test what happens when there is no running function */
+
+	printf("no running function\n");
+	duk_push_current_function(ctx);
+	printf("type=%d\n", duk_get_type(ctx, -1));
+	printf("duk_is_object: %d\n", duk_is_object(ctx, -1));
+	duk_pop(ctx);
+
+	/* then test the basic case */
+
+	printf("basic case\n");
+	duk_push_c_function(ctx, my_func, 1 /*nargs*/);
+	duk_push_int(ctx, 123);
+	rc = duk_pcall(ctx, 1, DUK_INVALID_INDEX);
+	printf("rc=%d, result='%s'\n", rc, duk_to_string(ctx, -1));
+	duk_pop(ctx);
+}
+
