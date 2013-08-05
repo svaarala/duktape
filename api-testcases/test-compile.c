d1841a774135c68115f46f6fd0792ec49b813d68
Sami Vaarala <sami.vaarala@iki.fi>
2013-08-05 15:11:19 +0300

    api doc and tests for duk_compile() and duk_compile_string()

---
+++ api-testcases/test-compile.c
@@ -0,0 +1,73 @@
+/*===
+program
+program result: 123.000000
+final top: 0
+rc=0, result=undefined
+*** test_2
+eval result: 5.000000
+final top: 0
+rc=0, result=undefined
+*** test_3
+function result: 11.000000
+final top: 0
+rc=0, result=undefined
+===*/
+
+int test_1(duk_context *ctx) {
+	duk_set_top(ctx, 0);
+
+	duk_push_string(ctx, "print('program');\n"
+	                     "function hello() { print('Hello world!'); }\n"
+	                     "123;");
+	duk_compile(ctx, 0);
+	duk_call(ctx, 0);      /* [ func ] -> [ result ] */
+	printf("program result: %lf\n", duk_get_number(ctx, -1));
+	duk_pop(ctx);
+
+	printf("final top: %d\n", duk_get_top(ctx));
+	return 0;
+}
+
+int test_2(duk_context *ctx) {
+	duk_set_top(ctx, 0);
+
+	duk_push_string(ctx, "2+3");
+	duk_compile(ctx, DUK_COMPILE_EVAL);
+	duk_call(ctx, 0);      /* [ func ] -> [ result ] */
+	printf("eval result: %lf\n", duk_get_number(ctx, -1));
+	duk_pop(ctx);
+
+	printf("final top: %d\n", duk_get_top(ctx));
+	return 0;
+}
+
+int test_3(duk_context *ctx) {
+	duk_set_top(ctx, 0);
+
+	duk_push_string(ctx, "function (x,y) { return x+y; }");
+	duk_compile(ctx, DUK_COMPILE_FUNCTION);
+	duk_push_int(ctx, 5);
+	duk_push_int(ctx, 6);
+	duk_call(ctx, 2);      /* [ func 5 6 ] -> [ result ] */
+	printf("function result: %lf\n", duk_get_number(ctx, -1));
+	duk_pop(ctx);
+
+	printf("final top: %d\n", duk_get_top(ctx));
+	return 0;
+}
+
+#define  TEST(func)  do { \
+		printf("*** %s\n", #func); \
+		rc = duk_safe_call(ctx, (func), 0, 1, DUK_INVALID_INDEX); \
+		printf("rc=%d, result=%s\n", rc, duk_to_string(ctx, -1)); \
+		duk_pop(ctx); \
+	} while (0)
+
+void test(duk_context *ctx) {
+	int rc;
+
+	TEST(test_1);
+	TEST(test_2);
+	TEST(test_3);
+}
+
