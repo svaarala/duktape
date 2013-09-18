bfa66a96797aa056986cb52d095a57c47a24632e
Sami Vaarala <sami.vaarala@iki.fi>
2013-09-18 20:52:39 +0300

    gettingstarted examples

---
+++ examples/gettingstarted/primecheck.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "duktape.h"
+
+static int native_prime_check(duk_context *ctx) {
+    int val = duk_require_int(ctx, 0);
+    int lim = duk_require_int(ctx, 1);
+    int i;
+
+    for (i = 2; i <= lim; i++) {
+        if (val % i == 0) {
+            duk_push_false(ctx);
+            return 1;
+        }
+    }
+
+    duk_push_true(ctx);
+    return 1;
+}
+
+int main(int argc, const char *argv[]) {
+    duk_context *ctx = NULL;
+
+    ctx = duk_create_heap_default();
+    if (!ctx) { exit(1); }
+
+    duk_push_global_object(ctx);
+    duk_push_c_function(ctx, native_prime_check, 2 /*nargs*/);
+    duk_put_prop_string(ctx, -2, "primeCheckNative");
+
+    duk_eval_file(ctx, "prime.js");
+    duk_pop(ctx);  /* pop eval result */
+
+    duk_get_prop_string(ctx, -1, "primeTest");
+    duk_call(ctx, 0);
+    duk_pop(ctx);
+
+    duk_destroy_heap(ctx);
+
+    exit(0);
+}
---
+++ examples/gettingstarted/processlines.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "duktape.h"
+
+int main(int argc, const char *argv[]) {
+    duk_context *ctx = NULL;
+    char line[4096];
+    char idx;
+    int ch;
+
+    ctx = duk_create_heap_default();
+    if (!ctx) { exit(1); }
+
+    duk_eval_file(ctx, "script.js");
+    duk_pop(ctx);  /* pop eval result */
+
+    memset(line, 0, sizeof(line));
+    idx = 0;
+    for (;;) {
+        if (idx >= sizeof(line)) { exit(1); }
+
+        ch = fgetc(stdin);
+        if (ch == 0x0a) {
+            line[idx++] = '\0';
+
+            duk_push_global_object(ctx);
+            duk_get_prop_string(ctx, -1, "processLine");
+            duk_push_string(ctx, line);
+            duk_call(ctx, 1);
+            printf("%s\n", duk_to_string(ctx, -1));
+            duk_pop(ctx);
+
+            idx = 0;
+        } else if (ch == EOF) {
+            break;
+        } else {
+            line[idx++] = (char) ch;
+        }
+    }
+
+    duk_destroy_heap(ctx);
+
+    exit(0);
+}
