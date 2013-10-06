d0a94e23e80b3daee9d0c3b81e5d59db29dbcfac
Sami Vaarala <sami.vaarala@iki.fi>
2013-10-06 23:30:43 +0300

    initial replacements for fpclassify() and friends

--- src/duk_replacements.c
+++ src/duk_replacements.c
@@ -15,25 +15,56 @@ double duk_computed_infinity;
 
 #ifdef DUK_USE_REPL_FPCLASSIFY
 int duk_repl_fpclassify(double x) {
-	return 0;  /*FIXME*/
+	volatile duk_double_union u;
+	int exp;
+	int mzero;
+
+	u.d = x;
+	exp = (u.us[_DUK_IDX_US0] & 0x7ff0) >> 4;
+	if (exp > 0x000 && exp < 0x7ff) {
+		return DUK_FP_NORMAL;
+	}
+
+	mzero = (u.ui[_DUK_IDX_UI1] == 0 && (u.ui[_DUK_IDX_UI0] & 0x000fffff) == 0);
+	if (exp == 0x000) {
+		if (mzero) {
+			return DUK_FP_ZERO;
+		} else {
+			return DUK_FP_SUBNORMAL;
+		}
+	} else {
+		if (mzero) {
+			return DUK_FP_INFINITE;
+		} else {
+			return DUK_FP_NAN;
+		}
+	}
 }
 #endif
 
 #ifdef DUK_USE_REPL_SIGNBIT
 int duk_repl_signbit(double x) {
-	return 0;  /*FIXME*/
+	volatile duk_double_union u;
+	u.d = x;
+	return u.uc[_DUK_IDX_UC0] & 0x80;
 }
 #endif
 
 #ifdef DUK_USE_REPL_ISFINITE
 int duk_repl_isfinite(double x) {
-	return 0;  /*FIXME*/
+	int c = DUK_FPCLASSIFY(x);
+	if (c == DUK_FP_NAN || c == DUK_FP_INFINITE) {
+		return 0;
+	} else {
+		return 1;
+	}
 }
 #endif
 
 #ifdef DUK_USE_REPL_ISNAN
 int duk_repl_isnan(double x) {
-	return 0;  /*FIXME*/
+	int c = DUK_FPCLASSIFY(x);
+	return (c == DUK_FP_NAN);
 }
 #endif
 
