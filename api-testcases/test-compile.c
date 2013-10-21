218a72f0e74da62c0424d04dfe9af47cb93a396b
Sami Vaarala <sami.vaarala@iki.fi>
2013-10-22 01:04:41 +0300

    fix duk_compile() testcases to account for new filename argument

--- api-testcases/test-compile.c
+++ api-testcases/test-compile.c
@@ -20,8 +20,9 @@ int test_1(duk_context *ctx) {
 	duk_push_string(ctx, "print('program');\n"
 	                     "function hello() { print('Hello world!'); }\n"
 	                     "123;");
+	duk_push_string(ctx, "program");
 	duk_compile(ctx, 0);
-	duk_call(ctx, 0);      /* [ func ] -> [ result ] */
+	duk_call(ctx, 0);      /* [ func filename ] -> [ result ] */
 	printf("program result: %lf\n", duk_get_number(ctx, -1));
 	duk_pop(ctx);
 
@@ -33,6 +34,7 @@ int test_2(duk_context *ctx) {
 	duk_set_top(ctx, 0);
 
 	duk_push_string(ctx, "2+3");
+	duk_push_string(ctx, "eval");
 	duk_compile(ctx, DUK_COMPILE_EVAL);
 	duk_call(ctx, 0);      /* [ func ] -> [ result ] */
 	printf("eval result: %lf\n", duk_get_number(ctx, -1));
@@ -46,6 +48,7 @@ int test_3(duk_context *ctx) {
 	duk_set_top(ctx, 0);
 
 	duk_push_string(ctx, "function (x,y) { return x+y; }");
+	duk_push_string(ctx, "function");
 	duk_compile(ctx, DUK_COMPILE_FUNCTION);
 	duk_push_int(ctx, 5);
 	duk_push_int(ctx, 6);
