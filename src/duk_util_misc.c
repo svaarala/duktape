6b25c527ce0f3c4727a095dc236dfa0043540b69
Sami Vaarala <sami.vaarala@iki.fi>
2013-10-29 00:06:13 +0200

    comment trivia

--- src/duk_util_misc.c
+++ src/duk_util_misc.c
@@ -4,9 +4,11 @@
 
 #include "duk_internal.h"
 
-/* Lowercase digits for radix values 2 to 36.  Also doubles as lowercase
- * hex nybble table.
+/*
+ *  Lowercase digits for radix values 2 to 36.  Also doubles as lowercase
+ *  hex nybble table.
  */
+
 char duk_lc_digits[36] = { '0', '1', '2', '3', '4', '5', '6', '7',
                            '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
                            'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
@@ -15,4 +17,3 @@ char duk_lc_digits[36] = { '0', '1', '2', '3', '4', '5', '6', '7',
 
 char duk_uc_nybbles[16] = { '0', '1', '2', '3', '4', '5', '6', '7',
                             '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-
