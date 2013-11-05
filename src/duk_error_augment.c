f757424d412ee7d048bea7962cfd66f8cce9dcc6
Sami Vaarala <sami.vaarala@iki.fi>
2013-11-05 03:11:18 +0200

    fix pc handling so that lineNumber is (more) correct for errors thrown from Ecmascript code

--- src/duk_error_augment.c
+++ src/duk_error_augment.c
@@ -87,13 +87,16 @@ static void add_traceback(duk_hthread *thr, duk_hthread *thr_callstack, duk_hobj
 		arr_idx++;
 
 		/* add a number containing: pc, activation flags */
+
+		/* Add a number containing: pc, activation flag
+		 *
+		 * PC points to next instruction, find offending PC.  Note that
+		 * PC == 0 for native code.
+		 */
 		pc = thr_callstack->callstack[i].pc;
-#if 0
-		/* FIXME: this is not the case always now */
-		pc--;  /* PC points to next instruction, find offending PC; note that
-		        * PC == 0 should never be possible for an error.
-		        */
-#endif
+		if (pc > 0) {
+			pc--;
+		}
 		DUK_ASSERT(pc >= 0 && (double) pc < DUK_DOUBLE_2TO32);  /* assume PC is at most 32 bits and non-negative */
 		d = ((double) thr_callstack->callstack[i].flags) * DUK_DOUBLE_2TO32 + (double) pc;
 		duk_push_number(ctx, d);  /* -> [... arr num] */
@@ -198,13 +201,13 @@ void duk_err_augment_error(duk_hthread *thr, duk_hthread *thr_callstack, int err
 			int pc;
 			duk_uint32_t line;
 
+			/* PC points to next instruction, find offending PC.  Note that
+			 * PC == 0 for native code.
+			 */
 			pc = act->pc;
-#if 0
-			/* FIXME: this is not the case always now */
-			pc--;  /* PC points to next instruction, find offending PC; note that
-			        * PC == 0 should never be possible for an error.
-			        */
-#endif
+			if (pc > 0) {
+				pc--;
+			}
 			DUK_ASSERT(pc >= 0 && (double) pc < DUK_DOUBLE_2TO32);  /* assume PC is at most 32 bits and non-negative */
 			act = NULL;  /* invalidated by pushes, so get out of the way */
 
