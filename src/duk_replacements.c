b9e89014b0455425979cf75693a42360ded5f923
Sami Vaarala <sami.vaarala@iki.fi>
2013-11-13 21:04:48 +0200

    c typing fixes

--- src/duk_replacements.c
+++ src/duk_replacements.c
@@ -2,7 +2,8 @@
  *  Replacements for missing platform functions.
  *
  *  Unlike the originals, fpclassify() and signbit() replacements don't
- *  work on any floating point types, only doubles.
+ *  work on any floating point types, only doubles.  The C typing here
+ *  mimics the standard prototypes.
  */
 
 #ifdef DUK_USE_COMPUTED_NAN
@@ -17,17 +18,17 @@ double duk_computed_infinity;
 int duk_repl_fpclassify(double x) {
 	volatile duk_double_union u;
 	duk_uint_fast16_t exp;
-	int mzero; /* FIXME: type */
+	duk_small_int_t mzero;
 
 	u.d = x;
-	exp = (duk_uint_fast16_t) (u.us[DUK_DBL_IDX_US0] & 0x7ff0);
-	if (exp > 0x0000 && exp < 0x7ff0) {
+	exp = (duk_uint_fast16_t) (u.us[DUK_DBL_IDX_US0] & 0x7ff0UL);
+	if (exp > 0x0000UL && exp < 0x7ff0UL) {
 		/* exp values [0x001,0x7fe] = normal */
 		return DUK_FP_NORMAL;
 	}
 
-	mzero = (u.ui[DUK_DBL_IDX_UI1] == 0 && (u.ui[DUK_DBL_IDX_UI0] & 0x000fffff) == 0);
-	if (exp == 0x0000) {
+	mzero = (u.ui[DUK_DBL_IDX_UI1] == 0 && (u.ui[DUK_DBL_IDX_UI0] & 0x000fffffUL) == 0);
+	if (exp == 0x0000UL) {
 		/* exp 0x000 is zero/subnormal */
 		if (mzero) {
 			return DUK_FP_ZERO;
@@ -49,7 +50,7 @@ int duk_repl_fpclassify(double x) {
 int duk_repl_signbit(double x) {
 	volatile duk_double_union u;
 	u.d = x;
-	return (int) (u.uc[DUK_DBL_IDX_UC0] & 0x80);
+	return (int) (u.uc[DUK_DBL_IDX_UC0] & 0x80UL);
 }
 #endif
 
