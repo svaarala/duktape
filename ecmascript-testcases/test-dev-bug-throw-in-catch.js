1024619dfba6e0093127756ac782ea0e8cad4d9a
Sami Vaarala <sami.vaarala@iki.fi>
2014-01-06 00:26:30 +0200

    add bug test case for incorrect unwinding of catch/call stacks when an uncaught error causes a thread to finish and a catchstack is active

--- ecmascript-testcases/test-dev-bug-throw-in-catch.js
+++ ecmascript-testcases/test-dev-bug-throw-in-catch.js
@@ -1,49 +1,69 @@
 /*
- *  Bug test case for assertion failures related to unwinding a try-catch block
- *  when the catch block throws an error before finishing.
- *
- *  Assertions must be enabled for the errors to manifest.
+ *  Bug test case for thread termination caused by an uncaught error when a
+ *  catch stack is simultaneously active.  The longjmp handler unwound the
+ *  call stack and catch stacks in an incorrect order in this case.
+ *  This was originally modified from test-dev-coroutine-basic.js.
  */
 
+/*---
+{
+    "custom": true
+}
+---*/
+
 /*===
-works
-works
+resume tests
+yielder starting
+yielder arg: foo
+yielded with 1
+resumed with bar
+yielded with 2
+resumed with quux
+yielded with 3
+resumed with baz
+Error: error 2
+finished
 ===*/
 
-/* This causes:
-[D] duk_hthread_stacks.c:339 (duk_hthread_catchstack_unwind): unwinding catchstack idx 2: lexical environment active
-PANIC 54: (duk_js_var.c:506): assertion failed: act->var_env == NULL (duk_js_var.c:506)
-*/
+function yielder(x) {
+    var yield = __duk__.Thread.yield;
 
-function test1() {
-    try {
-        try {
-            throw new Error('error 1');
-        } catch (e) {
-            throw new Error('error 2');
-        }
-    } catch (e2) {
-        print('outer');
-    }
-    print('works');
-}
+    print('yielder starting');
+    print('yielder arg:', x);
 
-/* This causes:
-[D] duk_heap_markandsweep.c:887 (duk_heap_mark_and_sweep): garbage collect (mark-and-sweep) starting, requested flags: 0x00000000, effective flags: 0x00000000
-PANIC 54: (duk_heap_markandsweep.c:619): assertion failed: DUK_HEAPHDR_GET_REFCOUNT(curr) == 0 (duk_heap_markandsweep.c:619)
-*/
+    print('resumed with', yield(1));
+    print('resumed with', yield(2));
+    print('resumed with', yield(3));
 
-function test2() {
+    // A forever loop here was used to cause an execution step limit
+    // error, which triggered the incorrect thread termination.  The
+    // underlying cause is thread termination from an error uncaught
+    // by the thread in question, with a catchstack simultaneously
+    // active.  Here the critical thing was to throw an error from the
+    // catch block, which is uncaught but the try-catch state is still
+    // in place and was unwound in an incorrect order.
+
     try {
         throw new Error('error 1');
     } catch (e) {
         throw new Error('error 2');
     }
-    print('works');
+
+    print('yielder ending');
+    return 123;
 }
 
+var t = new __duk__.Thread(yielder);
+
 try {
-    test2();
+    print('resume tests');
+    print('yielded with', __duk__.Thread.resume(t, 'foo'));
+    print('yielded with', __duk__.Thread.resume(t, 'bar'));
+    print('yielded with', __duk__.Thread.resume(t, 'quux'));
+    print('yielded with', __duk__.Thread.resume(t, 'baz'));
 } catch (e) {
     print(e);
 }
+
+print('finished');
+
