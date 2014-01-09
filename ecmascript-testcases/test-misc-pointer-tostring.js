9dd0ea96191248283133ee5dc02de1112868fd99
Sami Vaarala <sami.vaarala@iki.fi>
2014-01-09 23:44:52 +0200

    testcase for pointer formatting (to the extent testing is possible)

---
+++ ecmascript-testcases/test-misc-pointer-tostring.js
@@ -0,0 +1,31 @@
+/*---
+{
+    "custom": true
+}
+---*/
+
+/*===
+null
+true
+===/
+
+/* Test pointer formatting.  NULL pointers are formatted consistenly and in
+ * a platform independent manner.  Non-NULL pointers don't have a fixed format
+ * but test for an expected subset.
+ */
+
+function pointerTostringTest() {
+    var ptr_null = __duk__.Pointer();
+    var ptr_nonnull = __duk__.Pointer('dummy');
+    var re_ptr_nonnull = /^[0-9a-fA-Fx:]+$/;
+
+    print(ptr_null.toString());
+    print(re_ptr_nonnull.test(ptr_nonnull.toString()));
+}
+
+try {
+    pointerTostringTest();
+} catch (e) {
+    print(e);
+}
+
