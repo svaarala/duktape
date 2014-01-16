5264803ef646a861567d0dffea44bd2f2fd8feba
Sami Vaarala <sami.vaarala@iki.fi>
2014-01-16 02:25:01 +0200

    testcase for switch-case bug: code is accepted before first case

---
+++ ecmascript-testcases/test-dev-bug-switch-code-before-case.js
@@ -0,0 +1,13 @@
+/*
+ *  Any code before a case block must be rejected.
+ */
+
+/*===
+SyntaxError
+===*/
+
+try {
+    eval('switch (2) { print("hello"); case 2: print("match 2"); }');
+} catch (e) {
+    print(e);
+}
