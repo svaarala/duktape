a43bba301590d396c84cf1ce9cd52986b3668717
Sami Vaarala <sami.vaarala@iki.fi>
2014-01-18 05:05:32 +0200

    fix for-loop continue control flow bug: control did not go through loop check

--- src/duk_js_compiler.c
+++ src/duk_js_compiler.c
@@ -4367,7 +4367,7 @@ static void parse_for_statement(duk_compiler_ctx *comp_ctx, duk_ivalue *res, int
 		patch_jump(comp_ctx, pc_jumpto_l3, pc_l3);
 		patch_jump(comp_ctx, pc_jumpto_l4, pc_l4);
 		patch_jump(comp_ctx, pc_label_site + 1, pc_l4);  /* break jump */
-		patch_jump(comp_ctx, pc_label_site + 2, pc_l2);  /* continue jump */
+		patch_jump(comp_ctx, pc_label_site + 2, pc_l1);  /* continue jump */
 	}
 	goto finished;
 
