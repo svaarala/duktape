78ac6536e80b87ad7e32f614315c6019b5b2f2ce
Sami Vaarala <sami.vaarala@iki.fi>
2014-01-18 02:09:43 +0200

    bug test for pc2line encoding error when line diff is roughly more than 8 bits; triggered by some ecma262 tests

---
+++ ecmascript-testcases/test-bug-error-linenumber-1.js
@@ -0,0 +1,53 @@
+/*
+ *  Bug test case for a pc2line encoding error.  Triggered when the full
+ *  32 bit encoding activates (diff is > 8 bits, roughly).
+ */
+
+/*===
+line 2
+TypeError 3
+line 2
+TypeError 53
+line 2
+TypeError 103
+line 2
+TypeError 153
+line 2
+TypeError 203
+===*/
+
+function rep(s, n) {
+    var res = [];
+    while (n--) { res.push(s); }
+    return res.join('');
+}
+
+function evalCheck(code) {
+    try {
+        eval(code);
+        print('successful eval (unexpected)');
+    } catch (e) {
+        print(e.name + ' ' + e.lineNumber);
+    }
+}
+
+function test() {
+    /* This problem originally occurred in test262 testing.  Here 'this' is
+     * undefined and the TypeError (from getting a property from undefined)
+     * is attributed incorrectly if there are enough many empty lines in
+     * between.
+     */
+
+    evalCheck("\nprint('line 2')\n" + "if (null.foo) {}");
+    evalCheck("\nprint('line 2')\n" + rep('\n', 50) + "if (null.foo) {}");
+    evalCheck("\nprint('line 2')\n" + rep('\n', 100) + "if (null.foo) {}");
+    evalCheck("\nprint('line 2')\n" + rep('\n', 150) + "if (null.foo) {}");
+    evalCheck("\nprint('line 2')\n" + rep('\n', 200) + "if (null.foo) {}");
+}
+
+try {
+    test();
+} catch (e) {
+    print(e);
+}
+
