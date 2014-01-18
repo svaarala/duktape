57d6128024dad0046664a878053376f8e37b7c84
Sami Vaarala <sami.vaarala@iki.fi>
2014-01-18 05:52:20 +0200

    test case improvement for for+continue case

--- ecmascript-testcases/test-bug-for-continue-1.js
+++ ecmascript-testcases/test-bug-for-continue-1.js
@@ -1,4 +1,43 @@
+/*
+ *  For-loop + continue had a few control flow bugs in the past.  The compiler
+ *  handles an empty C part (for (A;B;C)) differently from a non-empty one, and
+ *  there was a bug in the empty case with continue statements.  This testcase
+ *  exercises for+continue control paths.
+ */
+
 /*===
+check
+body 0
+update
+check
+body 1
+update
+check
+body 2
+update
+check
+body 3
+update
+check
+body 4
+update
+check
+body 5
+update
+check
+body 6
+update
+check
+body 7
+update
+check
+body 8
+update
+check
+body 9
+update
+check
+10
 check
 body 0
 check
@@ -19,6 +58,38 @@ check
 body 8
 check
 body 9
+check
+10
+check
+body 0
+update
+check
+body 1
+update
+check
+body 2
+update
+check
+body 3
+update
+check
+body 4
+update
+check
+body 5
+update
+check
+body 6
+update
+check
+body 7
+update
+check
+body 8
+update
+check
+body 9
+update
 check
 10
 check
@@ -55,11 +126,17 @@ body 3
 body 4
 ===*/
 
-/* For-loop continue must recheck condition on every loop.  There was a bug
- * in this behavior.
- */
+function forContinueTest1a() {
+    for (var i = 0; print('check'), i < 10; print('update')) {
+        print('body', i);
+        i++;
+        continue;
+        i += 100;
+    }
+    print(i);
+}
 
-function forContinueTest1() {
+function forContinueTest1b() {
     for (var i = 0; print('check'), i < 10;) {
         print('body', i);
         i++;
@@ -69,7 +146,19 @@ function forContinueTest1() {
     print(i);
 }
 
-function forContinueTest2() {
+
+function forContinueTest2a() {
+    var i;
+    for (i = 0; print('check'), i < 10; print('update')) {
+        print('body', i);
+        i++;
+        continue;
+        i += 100;
+    }
+    print(i);
+}
+
+function forContinueTest2b() {
     var i;
     for (i = 0; print('check'), i < 10;) {
         print('body', i);
@@ -79,6 +168,7 @@ function forContinueTest2() {
     }
     print(i);
 }
+
 
 function forContinueTest3() {
     for (var i in [1,2,3,4,5]) {
@@ -97,8 +187,10 @@ function forContinueTest4() {
 
 
 try {
-    forContinueTest1();
-    forContinueTest2();
+    forContinueTest1a();
+    forContinueTest1b();
+    forContinueTest2a();
+    forContinueTest2b();
     forContinueTest3();
     forContinueTest4();
 } catch (e) {
