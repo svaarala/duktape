792775b8969b1ca35706aca67e85d65022a5a032
Sami Vaarala <sami.vaarala@iki.fi>
2014-01-19 19:23:23 +0200

    add a bug testcase to illustrate current misbehavior in array initializers when Array.prototype contains numeric index properties which are non-writable but configurable (this is a pretty obscure case)

---
+++ ecmascript-testcases/test-bug-array-proto-protected-1.js
@@ -0,0 +1,53 @@
+/*
+ *  If the Array.prototype contains numeric elements which are write protected
+ *  (but configurable), it should be possible to create an array literal with
+ *  an overriding value.  A normal property assignment would fail because the
+ *  inherited property is not writable, but an array initializer is supposed
+ *  to use [[DefineOwnProperty]] which allows an own property to be created if
+ *  the property is configurable (bypassing the write protection).
+ */
+
+/*===
+defineProperty success
+0 inherit undefined
+2 foo bar
+0 inherit undefined
+2 shouldsucceed bar
+===*/
+
+function test() {
+    var arr;
+
+    Object.defineProperty(Array.prototype,
+                          '0',
+                          {
+                              value: 'inherit',
+                              writable: false,
+                              enumerable: true,
+                              configurable: true 
+                          });
+    print('defineProperty success');
+
+    // No problem here: '0' not accessed
+    arr = [];
+    print(arr.length, arr[0], arr[1]);
+
+    // Again should succeed
+    var arr = ['foo', 'bar'];
+    print(arr.length, arr[0], arr[1]);
+
+    // Assignment should not work unless an own property already exists
+    arr = [];
+    arr[0] = 'shouldfail';  // failure is silent
+    print(arr.length, arr[0], arr[1]);
+
+    arr = ['foo', 'bar'];
+    arr[0] = 'shouldsucceed';
+    print(arr.length, arr[0], arr[1]);
+}
+
+try {
+    test();
+} catch (e) {
+    print(e);
+}
