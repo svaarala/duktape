c722ae1057950515ab4047fed1bf989067a62785
Sami Vaarala <sami.vaarala@iki.fi>
2014-01-23 17:11:16 +0200

    improve test262 filtered output

--- filter262log.py
+++ filter262log.py
@@ -17,8 +17,11 @@ def main():
 	]
 
 	in_failed_tests = False
-
-	tofix_count = 0  # count of bugs that will be fixed (no uncertainty about proper behavior etc)
+	tofix_count = 0     # count of bugs that will be fixed (no uncertainty about proper behavior etc)
+	known_errors = []
+	diagnosed_errors = []
+	unknown_errors = []
+	other_errors = []
 
 	for line in sys.stdin:
 		if len(line) > 1 and line[-1] == '\n':
@@ -35,14 +38,14 @@ def main():
 
 		# Augment error list with "known bugs"
 
+		print(line)  # print error list as is, then refined version later
+
 		if line == 'Failed tests':
 			in_failed_tests = True
-			print(line)
 			continue
 
-		if in_failed_tests and line == '':
+		if in_failed_tests and line.strip() == '':
 			in_failed_tests = False
-			print(line)
 			continue
 
 		if in_failed_tests:
@@ -56,13 +59,13 @@ def main():
 					continue
 				if kn.has_key('diagnosed'):
 					tofix_count += 1
-					print(line + '   // diagnosed: ' + kn['diagnosed'])
+					diagnosed_errors.append(line + '   // diagnosed: ' + kn['diagnosed'])
 				elif kn.has_key('known'):
 					# don't bump tofix_count, as testcase expected result is not certain
-					print(line + '   // KNOWN: ' + kn['known'])
+					known_errors.append(line + '   // KNOWN: ' + kn['known'])
 				else:
 					tofix_count += 1
-					print(line + '   // ??? (rule matches))')
+					unknown_errors.append(line + '   // ??? (rule matches))')
 				kn['used'] = True  # mark rule used
 				matched = True
 				break
@@ -71,15 +74,21 @@ def main():
 				continue
 
 			# no match, to fix
+			other_errors.append(line)
 			tofix_count += 1
 
-		# Otherwise print normally
-
-		print(line)
-
 	print('')
-	print('TO-FIX COUNT: ' + str(tofix_count))
-	print('  = test case failures which need fixing (Duktape bugs, uninvestigated)')
+	print('=== CATEGORISED ERRORS ===')
+	print('')
+
+	for i in known_errors:
+		print(i)
+	for i in diagnosed_errors:
+		print(i)
+	for i in unknown_errors:
+		print(i)
+	for i in other_errors:
+		print(i)
 
 	# Check for unused rules (e.g. bugs fixed)
 
@@ -87,6 +96,13 @@ def main():
 	for kn in known_issues:
 		if not kn.has_key('used'):
 			print('WARNING: unused rule: ' + json.dumps(kn))
+
+	# To fix count
+
+	print('')
+	print('TO-FIX COUNT: ' + str(tofix_count))
+	print('  = test case failures which need fixing (Duktape bugs, uninvestigated)')
+
 
 if __name__ == '__main__':
 	main()
