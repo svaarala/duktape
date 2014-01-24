5944e15f628f5e0bf80ba586ef27d8ebe524ba35
Sami Vaarala <sami.vaarala@iki.fi>
2014-01-24 14:26:01 +0200

    inijs legacy funcs: addr, refc

--- src/duk_initjs_legacy.js
+++ src/duk_initjs_legacy.js
@@ -4,9 +4,16 @@
  *  Compatibility properties / wrapper functions here allow Duktape to remain
  *  compatible for user code when core features are changed, without burdening
  *  the main C code with compatibility stuff.
+ *
+ *  This file goes through UglifyJS to minify it.  UglifyJS renames variables,
+ *  removes comments, and is clever enough to drop any "if (false) { ... }"
+ *  blocks altogether, so that's an effective way to disable currently unneeded
+ *  code.
  */
 
 (function(D) {
+    'use strict';
+
     function def(name, value) {
         Object.defineProperty(D, name, {
             value: value,
@@ -15,7 +22,23 @@
             configurable: true
         });
     }
-    def('build', '');                 // removed in Duktape 0.9.0
-    def('setFinalizer', D.setFin);    // renamed in Duktape 0.9.0
-    def('getFinalizer', D.getFin);    // renamed in Duktape 0.9.0
+
+    // legacy properties
+    if (true) {
+        // removed in Duktape 0.9.0
+        def('build', '');
+        def('setFinalizer', D.setFin);
+        def('getFinalizer', D.getFin);
+        def('addr', function(v) { return D.info(v)[1]; });
+        def('refc', function(v) { return D.info(v)[2]; });
+    }
+
+    // info convenience
+    if (false) {
+        def('infox', function (v) {
+            var t = D.info(v);
+            if (!t) { return t; }
+            return { type: t[0], addr: t[1], refc: t[2], hdrsize: t[3], addsize: t[4], bcsize: t[5] };
+        });
+    }
 })(Duktape);
