97b96453e6c773d8273dd2e11a74a75ab21ecb9e
Sami Vaarala <sami.vaarala@iki.fi>
2014-01-26 01:10:12 +0200

    while statement tests

--- ecmascript-testcases/test-stmt-while.js
+++ ecmascript-testcases/test-stmt-while.js
@@ -2,17 +2,80 @@
  *  While statement (E5 Section 12.10).
  */
 
-/*---
-{
-    "skip": true
-}
----*/
-
 /*FIXME*/
 
 /*===
+4
+3
+2
+1
+0
+final -1
+final -1
+4
+3
+2
+1
+0
+final -1
+4
+3
+2
+1
+final 1
+in while
+4
+in while
+3
+in while
+2
+in while
+1
+in while
+0
+in while
+final -1
 ===*/
 
-/*FIXME:break*/
-/*FIXME:continue*/
+function whileTest() {
+    var i;
 
+    i = 5;
+    while (i--) {
+        print(i);
+    }
+    print('final', i);
+
+    i = 0;
+    while (i--) {
+        print(i);
+    }
+    print('final', i);
+
+    i = 5;
+    while (i--) {
+        print(i);
+        continue;
+    }
+    print('final', i);
+
+    i = 5;
+    while (i--) {
+        print(i);
+        if (i >= 4) { continue; }
+        if (i == 1) { break; }
+    }
+    print('final', i);
+
+    i = 5;
+    while (print('in while'), i--) {
+        print(i);
+    }
+    print('final', i);
+}
+
+try {
+    whileTest();
+} catch (e) {
+    print(e);
+}
