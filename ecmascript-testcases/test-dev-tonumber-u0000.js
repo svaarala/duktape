6e508b6994791568e589430af8a5e74bf453a743
Sami Vaarala <sami.vaarala@iki.fi>
2014-01-29 22:07:30 +0200

    testcase for ToNumber of '\u0000'

---
+++ ecmascript-testcases/test-dev-tonumber-u0000.js
@@ -0,0 +1,19 @@
+/*===
+0 Infinity
+NaN
+===*/
+
+/* An empty string ToNumber() coerces to +0 (this is explicitly specified).
+ * A string which cannot be interpreted as a StringNumericLiteral must
+ * produce a NaN.
+ *
+ * A string containing just a U+0000 character is easily confused with an
+ * empty string in C code, so this testcases ensures that doesn't happen.
+ */
+
+try {
+    print(+'', 1/(+''));
+    print(+'\u0000');
+} catch (e) {
+    print(e);
+}
