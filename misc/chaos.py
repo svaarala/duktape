2494405a2e33c2c512a396a2d611bd9262dcefc3
Sami Vaarala <sami.vaarala@iki.fi>
2014-02-02 00:35:25 +0200

    misc util to play test-dev-chaos.js output

---
+++ misc/chaos.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#
+#  $ ./duk test-dev-chaos.js | python chaos.py > chaos.raw
+#  $ sox -r 8k -e unsigned -b 8 -c 1 chaos.raw chaos.wav
+
+import os
+import sys
+
+data = sys.stdin.read()
+data = data.strip()
+data = data.decode('hex')
+sys.stdout.write(data)
+
