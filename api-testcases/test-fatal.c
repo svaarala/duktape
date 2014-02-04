593f9e994d9cf6c5a15544029fe6434d01477f4f
Sami Vaarala <sami.vaarala@iki.fi>
2014-02-04 23:27:36 +0200

    api test case fix to match new fatal prototype

--- api-testcases/test-fatal.c
+++ api-testcases/test-fatal.c
@@ -1,10 +1,10 @@
 /*===
 my_func
-fatal error: 123456
+fatal error: 123456 (reason)
 ===*/
 
-void my_fatal_handler(duk_context *ctx, int code) {
-	printf("fatal error: %d\n", code);
+void my_fatal_handler(duk_context *ctx, int code, const char *msg) {
+	printf("fatal error: %d (%s)\n", code, msg);
 
 	/* A fatal error handler must not return, so exit here */
 	exit(0);
@@ -12,7 +12,7 @@ void my_fatal_handler(duk_context *ctx, int code) {
 
 int my_func(duk_context *ctx) {
 	printf("my_func\n");
-	duk_fatal(ctx, 123456);
+	duk_fatal(ctx, 123456, "reason");
 	printf("never here\n");
 	return 0;
 }
@@ -24,5 +24,3 @@ void test(duk_context *ctx) {
 	duk_safe_call(new_ctx, my_func, 0, 1, DUK_INVALID_INDEX);
 	printf("duk_safe_call() returned, should not happen\n");
 }
-
-
