64bbd5ff1c42a177b8901595663dca032f1e22cf
Sami Vaarala <sami.vaarala@iki.fi>
2014-02-13 01:31:36 +0200

    update emscripten compatibility info: regexp fix should not be needed soon

--- util/fix_emscripten.py
+++ util/fix_emscripten.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 #
 #  Fix the single regexp syntax error from Emscripten output.
+#
+#  This has been fixed in the Emscripten repository and should no longer
+#  be necessary.
+#
+#  (https://github.com/kripken/emscripten/commit/277ac5239057721ebe3c6e7813dc478eeab2cea0)
+#
 
 import os
 import sys
