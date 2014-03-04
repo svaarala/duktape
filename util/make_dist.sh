3a5e413a6e9a8a26e538c72b8aaa8fc98b263487
Sami Vaarala <sami.vaarala@iki.fi>
2014-03-04 23:54:32 +0200

    add eventloop example to dist, add README.txt files to dist

--- util/make_dist.sh
+++ util/make_dist.sh
@@ -50,6 +50,7 @@ mkdir $DIST/examples
 mkdir $DIST/examples/hello
 mkdir $DIST/examples/eval
 mkdir $DIST/examples/cmdline
+mkdir $DIST/examples/eventloop
 mkdir $DIST/examples/guide
 mkdir $DIST/examples/coffee
 
@@ -176,25 +177,42 @@ cat src/duktape.h | sed \
 	> $DISTSRCSEP/duktape.h
 
 for i in \
+	README.txt \
 	duk_cmdline.c \
 	; do
 	cp examples/cmdline/$i $DIST/examples/cmdline/
 done
 
 for i in \
+	README.txt \
+	Makefile \
+	eventloop.c \
+	eventloop.js \
+	main.c \
+	duk_ncurses.c \
+	duk_socket.c \
+	duk_fileio.c \
+	curses-timers.js \
+	; do
+	cp examples/eventloop/$i $DIST/examples/eventloop/
+done
+
+for i in \
+	README.txt \
 	hello.c \
 	; do
 	cp examples/hello/$i $DIST/examples/hello/
 done
 
 for i in \
+	README.txt \
 	eval.c \
 	; do
 	cp examples/eval/$i $DIST/examples/eval/
 done
 
-
 for i in \
+	README.txt \
 	fib.js \
 	process.js \
 	processlines.c \
@@ -206,6 +224,7 @@ for i in \
 done
 
 for i in \
+	README.txt \
 	Makefile \
 	globals.coffee \
 	hello.coffee \
