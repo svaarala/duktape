1bbce17f065342b8e04e4df0c0c6d9d62c5ee3e7
Sami Vaarala <sami.vaarala@iki.fi>
2014-04-01 23:56:03 +0300

    buffer constructor changes: number argument creates buffer of specified size, buffer arguments are returned as is (possibly object promoted)

--- src/duk_bi_buffer.c
+++ src/duk_bi_buffer.c
@@ -9,13 +9,46 @@
  */
 
 int duk_bi_buffer_constructor(duk_context *ctx) {
-	if (duk_get_top(ctx) == 0) {
-		(void) duk_push_fixed_buffer(ctx, 0);
-	} else {
-		duk_to_buffer(ctx, 0, NULL);
+	duk_size_t buf_size;
+	duk_small_int_t buf_dynamic;
+	duk_uint8_t *buf_data;
+	const duk_uint8_t *src_data;
+
+	/*
+	 *  Constructor arguments are currently somewhat compatible with
+	 *  (keep it that way if possible):
+	 *
+	 *    http://nodejs.org/api/buffer.html
+	 *
+	 */
+
+	buf_dynamic = duk_get_boolean(ctx, 1);  /* default to false */
+
+	switch (duk_get_type(ctx, 0)) {
+	case DUK_TYPE_NUMBER:
+		/* new buffer of specified size */
+		buf_size = (duk_size_t) duk_to_int(ctx, 0);
+		(void) duk_push_buffer(ctx, buf_size, buf_dynamic);
+		break;
+	case DUK_TYPE_BUFFER:
+		/* return input buffer, converted to a Buffer object if called as a
+		 * constructor (no change if called as a function).
+		 */
+		duk_set_top(ctx, 1);
+		break;
+	case DUK_TYPE_STRING:
+		/* new buffer with string contents */
+		src_data = (const duk_uint8_t *) duk_get_lstring(ctx, 0, &buf_size);
+		DUK_ASSERT(src_data != NULL);  /* even for zero-length string */
+		buf_data = duk_push_buffer(ctx, buf_size, buf_dynamic);
+		DUK_MEMCPY((void *) buf_data, (const void *) src_data, (size_t) buf_size);
+		break;
+	case DUK_TYPE_NONE:
+	default:
+		return DUK_RET_TYPE_ERROR;
 	}
-	DUK_ASSERT(duk_is_buffer(ctx, 0));
-	duk_set_top(ctx, 1);
+
+	/* stack is unbalanced, but: [ <something> buf ] */
 
 	if (duk_is_constructor_call(ctx)) {
 		duk_push_object_helper(ctx,
@@ -25,7 +58,7 @@ int duk_bi_buffer_constructor(duk_context *ctx) {
 		                       DUK_BIDX_BUFFER_PROTOTYPE);
 
 		/* Buffer object internal value is immutable */
-		duk_dup(ctx, 0);
+		duk_dup(ctx, -2);
 		duk_def_prop_stridx(ctx, -2, DUK_STRIDX_INT_VALUE, DUK_PROPDESC_FLAGS_NONE);
 	}
 	/* Note: unbalanced stack on purpose */
