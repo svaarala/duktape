62d514c178bb4fca4658a2fc3729ec9e95d9b396
Sami Vaarala <sami.vaarala@iki.fi>
2014-04-05 17:01:43 +0300

    extend duk_eval_raw() and duk_compile_raw() with a safe flag; c++ header fix for duk_log()

--- src/duk_api_compile.c
+++ src/duk_api_compile.c
@@ -4,37 +4,53 @@
 
 #include "duk_internal.h"
 
-/* FIXME: placeholder */
-
 /* Eval is just a wrapper now. */
-void duk_eval_raw(duk_context *ctx) {
+int duk_eval_raw(duk_context *ctx, int flags) {
 	int comp_flags;
+	int rc;
 
 	/* [ ... source filename ] */
 
-	comp_flags = DUK_COMPILE_EVAL;
+	comp_flags = flags;
+	comp_flags |= DUK_COMPILE_EVAL;
 	if (duk_is_strict_call(ctx)) {
 		comp_flags |= DUK_COMPILE_STRICT;
 	}
-	duk_compile(ctx, comp_flags);
+	rc = duk_compile_raw(ctx, comp_flags);  /* may be safe, or non-safe depending on flags */
 
-	/* [ ... closure ] */
+	/* [ ... closure/error ] */
 
-	duk_call(ctx, 0);
+	if (rc != DUK_EXEC_SUCCESS) {
+		return DUK_EXEC_ERROR;
+	}
 
-	/* [ ... result ] */
+	if (flags & DUK_COMPILE_SAFE) {
+		rc = duk_pcall(ctx, 0, DUK_INVALID_INDEX);  /* FIXME: user control? */
+	} else {
+		duk_call(ctx, 0);
+		rc = DUK_EXEC_SUCCESS;
+	}
+
+	/* [ ... result/error ] */
+
+	return rc;
 }
 
-void duk_compile(duk_context *ctx, int flags) {
+/* Helper which can be called both directly and with duk_safe_call(). */
+static int duk__do_compile(duk_context *ctx) {
 	duk_hthread *thr = (duk_hthread *) ctx;
+	int flags;
 	int comp_flags;
 	duk_hcompiledfunction *h_templ;
 
+	/* [ ... source filename flags ] */
+
+	flags = duk_get_number(ctx, -1);
+	duk_pop(ctx);
+
 	/* [ ... source filename ] */
 
-	/* FIXME: flags */
 	/* FIXME: unnecessary translation of flags */
-
 	comp_flags = 0;
 	if (flags & DUK_COMPILE_EVAL) {
 		comp_flags = DUK_JS_COMPILE_FLAG_EVAL;
@@ -61,5 +77,19 @@ void duk_compile(duk_context *ctx, int flags) {
 	duk_remove(ctx, -2);  /* -> [ ... closure ] */
 
 	/* [ ... closure ] */
+
+	return 1;
+}
+
+int duk_compile_raw(duk_context *ctx, int flags) {
+	duk_push_int(ctx, flags);
+
+	if (flags & DUK_COMPILE_SAFE) {
+		int rc = duk_safe_call(ctx, duk__do_compile, 3 /*nargs*/, 1 /*nrets*/, DUK_INVALID_INDEX);
+		return rc;
+	}
+
+	(void) duk__do_compile(ctx);
+	return DUK_EXEC_SUCCESS;
 }
 
