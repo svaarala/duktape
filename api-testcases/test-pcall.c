06ac6d6cc06fa487b42c193fc86d9087cfa63cf5
Sami Vaarala <sami.vaarala@iki.fi>
2014-04-09 11:55:06 +0300

    protected call test case trivia: use duk_safe_to_string()

--- api-testcases/test-pcall.c
+++ api-testcases/test-pcall.c
@@ -14,7 +14,7 @@ void test(duk_context *ctx) {
 	duk_push_int(ctx, 10);
 	duk_push_int(ctx, 11);
 	rc = duk_pcall(ctx, 2);
-	printf("rc=%d, result='%s'\n", rc, duk_to_string(ctx, -1));
+	printf("rc=%d, result='%s'\n", rc, duk_safe_to_string(ctx, -1));
 	duk_pop(ctx);
 	printf("top after pop: %d\n", duk_get_top(ctx));
 
@@ -23,7 +23,7 @@ void test(duk_context *ctx) {
 	duk_push_int(ctx, 10);
 	duk_push_int(ctx, 11);
 	rc = duk_pcall(ctx, 2);
-	printf("rc=%d, result='%s'\n", rc, duk_to_string(ctx, -1));
+	printf("rc=%d, result='%s'\n", rc, duk_safe_to_string(ctx, -1));
 	duk_pop(ctx);
 	printf("top after pop: %d\n", duk_get_top(ctx));
 
