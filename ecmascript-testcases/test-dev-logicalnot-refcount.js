6a000bb9feb4e17885f15857730bf7a159622650
Sami Vaarala <sami.vaarala@iki.fi>
2014-04-12 01:05:48 +0300

    refactoring test case

---
+++ ecmascript-testcases/test-dev-logicalnot-refcount.js
@@ -0,0 +1,37 @@
+/*---
+{
+    "custom": true,
+    "specialoptions": "requires refcounting"
+}
+---*/
+
+/*===
+2
+3
+2
+false
+===*/
+
+/* Development time test to test refcount handling of bytecode executor
+ * logical NOT.
+ */
+
+var dummy = {};
+var z = 1;
+
+function test() {
+    var tmp;
+
+    print(Duktape.info(dummy)[2]);
+    tmp = dummy;
+    print(Duktape.info(dummy)[2]);
+    tmp = !z;
+    print(Duktape.info(dummy)[2]);
+    print(tmp);
+}
+
+try {
+    test();
+} catch (e) {
+    print(e);
+}
