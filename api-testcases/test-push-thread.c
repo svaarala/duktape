de324a0e5805f65d59505725a31a4b11f134dfe1
Sami Vaarala <sami.vaarala@iki.fi>
2014-04-13 02:26:53 +0300

    extend api thread testcase to cover fresh globals case

--- api-testcases/test-push-thread.c
+++ api-testcases/test-push-thread.c
@@ -1,12 +1,20 @@
 /*===
+*** test_1 (duk_safe_call)
 duk_is_object(1) = 1
 duk_is_thread(1) = 1
 top=2
 concat: foobarquux
 done
+==> rc=0, result='undefined'
+*** test_2 (duk_safe_call)
+top: 2
+context a: bar
+context b: undefined
+==> rc=0, result='undefined'
 ===*/
 
-void test(duk_context *ctx) {
+/* Some basic tests. */
+static int test_1(duk_context *ctx) {
 	int thr_idx;
 	duk_context *new_ctx;
 
@@ -28,4 +36,36 @@ void test(duk_context *ctx) {
 	/* make new thread unreachable, so it gets GC'd */
 	duk_set_top(ctx, 0);
 	printf("done\n");
+	return 0;
+}
+
+/* Thread with shared and fresh globals. */
+static int test_2(duk_context *ctx) {
+	duk_context *ctx_a;
+	duk_context *ctx_b;
+
+	duk_eval_string_noresult(ctx, "this.globalFoo = 'bar';");
+
+	duk_push_thread(ctx);
+	ctx_a = duk_require_context(ctx, -1);
+	duk_push_thread_new_globalenv(ctx);
+	ctx_b = duk_require_context(ctx, -1);
+
+	printf("top: %d\n", duk_get_top(ctx));
+
+	/* index 0: thread with globals shared with 'ctx' (has globalFoo)
+	 * index 1: thread with globals separate with 'ctx'
+	 */
+
+	/* Print globalFoo. */
+
+	duk_peval_string_noresult(ctx_a, "print('context a: ' + String(this.globalFoo));");
+	duk_peval_string_noresult(ctx_b, "print('context b: ' + String(this.globalFoo));");
+
+	return 0;
+}
+
+void test(duk_context *ctx) {
+	TEST_SAFE_CALL(test_1);
+	TEST_SAFE_CALL(test_2);
 }
