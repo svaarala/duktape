1ecfe752df43a29a0fc01093f578c9a2aa163cb4
Sami Vaarala <sami.vaarala@iki.fi>
2014-04-27 18:00:22 +0300

    fix proxy testcase to match missing Proxy.revocable()

--- ecmascript-testcases/test-bi-proxy-subset.js
+++ ecmascript-testcases/test-bi-proxy-subset.js
@@ -8,10 +8,7 @@ Proxy exists: true function
 Proxy.length: 2
 Proxy.name: Proxy
 Proxy desc: writable=true enumerable=false configurable=true
-Proxy.revocable exists: true function
-Proxy.revocable.length: 2
-Proxy.revocable.name: revocable
-Proxy.revocable desc: writable=true enumerable=false configurable=true
+Proxy.revocable exists: false undefined
 ===*/
 
 function proxyExistenceTest() {
@@ -20,7 +17,6 @@ function proxyExistenceTest() {
     print('Proxy exists:', 'Proxy' in this, typeof this.Proxy);
     print('Proxy.length:', this.Proxy.length);
     print('Proxy.name:', this.Proxy.name);
-
     pd = Object.getOwnPropertyDescriptor(this, 'Proxy');
     if (pd) {
         print('Proxy desc:', 'writable=' + pd.writable, 'enumerable=' + pd.enumerable,
@@ -28,14 +24,15 @@ function proxyExistenceTest() {
     }
 
     print('Proxy.revocable exists:', 'revocable' in this.Proxy, typeof this.Proxy.revocable);
+/*
     print('Proxy.revocable.length:', this.Proxy.revocable.length);
     print('Proxy.revocable.name:', this.Proxy.revocable.name);
-
     pd = Object.getOwnPropertyDescriptor(this.Proxy, 'revocable');
     if (pd) {
         print('Proxy.revocable desc:', 'writable=' + pd.writable, 'enumerable=' + pd.enumerable,
               'configurable=' + pd.configurable);
     }
+*/
 }
 
 print('proxy existence');
