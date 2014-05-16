4abd05a2cc4afa3d8a5288bb529721a8bdee917d
Sami Vaarala <sami.vaarala@iki.fi>
2014-05-16 14:42:30 +0300

    context dump conditionality to DUK_USE_FILE_IO

--- src/duk_api_debug.c
+++ src/duk_api_debug.c
@@ -4,6 +4,7 @@
 
 #include "duk_internal.h"
 
+#if defined(DUK_USE_FILE_IO)
 void duk_dump_context_raw(duk_context *ctx) {
 	int idx;
 	int top;
@@ -35,3 +36,4 @@ void duk_dump_context_raw(duk_context *ctx) {
 	duk_pop(ctx);
 	DUK_ASSERT(duk_is_string(ctx, -1));
 }
+#endif  /* DUK_USE_FILE_IO */
