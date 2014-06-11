8ce769ede955e96c10a43f9abb7defa1ebb9e015
Sami Vaarala <sami.vaarala@iki.fi>
2014-06-11 18:36:47 +0300

    Add JSON __proto__ reviver corner case test (now fails)
    Add more testcases for parsing __proto__ in JSON.  The current implementation
    fails when a reviver is really clever and deletes the own property '__proto__'
    when called for another property (prior to __proto__).  This is very much a
    corner case.

--- ecmascript-testcases/test-bug-json-parse-__proto__.js
+++ ecmascript-testcases/test-bug-json-parse-__proto__.js
@@ -2,10 +2,38 @@
  *  Bug in test262 test case: ch15/15.12/15.12.2/S15.12.2_A1
  *
  *  Duktape does not parse __proto__ as a concrete property when ES6
- *  __proto__ property is enabled.
+ *  __proto__ property is enabled, but rather sets the object prototype.
  */
 
 /*===
+basic case
+[object Object]
+object
+true
+false
+true
+[object Object]
+===*/
+
+/*===
+reviver case
+reviver: __proto__
+reviver: 
+[object Object]
+object
+true
+false
+true
+[object Object]
+===*/
+
+/*===
+complex reviver case
+reviver: foo [object Object] bar
+before: this.__proto__: [object Object]
+after: this.__proto__: undefined
+reviver: __proto__ undefined [object Object]
+reviver:  [object Object] [object Object]
 [object Object]
 object
 true
@@ -15,13 +43,60 @@ true
 ===*/
 
 function test() {
-    var x = JSON.parse('{"__proto__":[]}');
-    print(x);
-    print(typeof x);
-    print(x instanceof Object);
-    print(x instanceof Array);
-    print('__proto__' in x);
-    print(Object.getOwnPropertyDescriptor(x, '__proto__'));
+    var x;
+
+    function dump(x) {
+        print(x);
+        print(typeof x);
+        print(x instanceof Object);
+        print(x instanceof Array);
+        print('__proto__' in x);
+        print(Object.getOwnPropertyDescriptor(x, '__proto__'));
+    }
+
+    // Basic parse case
+
+    print('basic case');
+    dump(JSON.parse('{"__proto__":[]}'));
+
+    // Similar issue triggered when reviving '__proto__'
+
+    print('reviver case');
+    dump(JSON.parse('{"__proto__":[]}', function (name, val) {
+        // this=holder
+        print('reviver:', name);
+        return val;
+    }));
+
+    // Here the reviver is quite devious, and modifies the holder
+    // so that an own property '__proto__' is removed before that
+    // property is revived.  Because Duktape writes the replacement
+    // value with duk_put_prop() now (in duk_bi_json.c), this runs
+    // the risk of doing a normal property put on __proto__ with no
+    // such own property in the object -- which then changes the
+    // prototype of the result (which is incorrect).
+
+    print('complex reviver case');
+    dump(JSON.parse('{"foo":"bar", "__proto__":[1,2,3]}', function (name, val) {
+        // this=holder
+        print('reviver:', name, Object.getOwnPropertyDescriptor(this, name), val);
+        if (name === 'foo') {
+            // Delete '__proto__' while reviving 'foo'.
+            print('before: this.__proto__:', Object.getOwnPropertyDescriptor(this, '__proto__'));
+            delete this.__proto__;
+            print('after: this.__proto__:', Object.getOwnPropertyDescriptor(this, '__proto__'));
+        }
+        if (name === '__proto__') {
+            // Since __proto__ is deleted, duk_bi_json.c will read its value as
+            // Object.prototype (= val) before calling us.  That's why the test
+            // case prints out Object.getOwnPropertyDescriptor(this, name) returning
+            // undefined, but 'val' being '[object Object'].  Force an array return
+            // so that it gets (incorrectly) written to __proto__ using duk_put_prop().
+            // The correct behavior here would be to set it as an own property.
+            return [];
+        }
+        return val;
+    }));
 }
 
 try {
