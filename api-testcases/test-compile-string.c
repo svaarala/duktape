0ce5e70c0fe1c7fba8c261f3a114a4061d66335d
Sami Vaarala <sami.vaarala@iki.fi>
2014-06-25 15:21:02 +0300

    API test case fixes

--- api-testcases/test-compile-string.c
+++ api-testcases/test-compile-string.c
@@ -1,15 +1,32 @@
 /*===
+*** test_string (duk_safe_call)
 program code
 return value is: '123'
 compile rc=0
 program code
 return value is: '123'
+compile rc=0
+myFile.js
+return value is: '234'
 compile rc=1 -> SyntaxError: invalid object literal (line 1)
 top: 0
+==> rc=0, result='undefined'
+*** test_lstring (duk_safe_call)
+program code
+return value is: '123'
+compile rc=0
+program code
+return value is: '123'
+compile rc=0
+myFile.js
+return value is: '234'
+compile rc=1 -> SyntaxError: invalid object literal (line 1)
+top: 0
+==> rc=0, result='undefined'
 ===*/
 
-void test(duk_context *ctx) {
-	int rc;
+static duk_ret_t test_string(duk_context *ctx) {
+	duk_int_t rc;
 
 	/* Normal compile */
 	duk_compile_string(ctx, 0, "print('program code'); 123");
@@ -19,15 +36,65 @@ void test(duk_context *ctx) {
 
 	/* Protected compile, success */
 	rc = duk_pcompile_string(ctx, 0, "print('program code'); 123");
-	printf("compile rc=%d\n", rc);
+	printf("compile rc=%ld\n", (long) rc);
+	duk_call(ctx, 0);
+	printf("return value is: '%s'\n", duk_to_string(ctx, -1));
+	duk_pop(ctx);
+
+	/* Protected compile with explicit filename, success */
+	duk_push_string(ctx, "myFile.js");
+	rc = duk_pcompile_string_filename(ctx, 0, "print(Duktape.act(-2).function.fileName); 234");
+	printf("compile rc=%ld\n", (long) rc);
 	duk_call(ctx, 0);
 	printf("return value is: '%s'\n", duk_to_string(ctx, -1));
 	duk_pop(ctx);
 
 	/* Protected compile, syntax error */
 	rc = duk_pcompile_string(ctx, 0, "print('program code'); 123; obj={");
-	printf("compile rc=%d -> %s\n", rc, duk_safe_to_string(ctx, -1));
+	printf("compile rc=%ld -> %s\n", (long) rc, duk_safe_to_string(ctx, -1));
 	duk_pop(ctx);
 
-	printf("top: %d\n", duk_get_top(ctx));
+	printf("top: %ld\n", (long) duk_get_top(ctx));
+	return 0;
+}
+
+static duk_ret_t test_lstring(duk_context *ctx) {
+	duk_int_t rc;
+	const char *src1 = "print('program code'); 123@";
+	const char *src2 = "print(Duktape.act(-2).function.fileName); 234@";
+	const char *src3 = "print('program code'); 123; obj={@";
+
+	/* Normal compile */
+	duk_compile_lstring(ctx, 0, src1, strlen(src1) - 1);
+	duk_call(ctx, 0);
+	printf("return value is: '%s'\n", duk_to_string(ctx, -1));
+	duk_pop(ctx);
+
+	/* Protected compile, success */
+	rc = duk_pcompile_lstring(ctx, 0, src1, strlen(src1) - 1);
+	printf("compile rc=%ld\n", (long) rc);
+	duk_call(ctx, 0);
+	printf("return value is: '%s'\n", duk_to_string(ctx, -1));
+	duk_pop(ctx);
+
+	/* Protected compile with explicit filename, success */
+	duk_push_string(ctx, "myFile.js");
+	rc = duk_pcompile_lstring_filename(ctx, 0, src2, strlen(src2) - 1);
+	printf("compile rc=%ld\n", (long) rc);
+	duk_call(ctx, 0);
+	printf("return value is: '%s'\n", duk_to_string(ctx, -1));
+	duk_pop(ctx);
+
+	/* Protected compile, syntax error */
+	rc = duk_pcompile_lstring(ctx, 0, src3, strlen(src3) - 1);
+	printf("compile rc=%ld -> %s\n", (long) rc, duk_safe_to_string(ctx, -1));
+	duk_pop(ctx);
+
+	printf("top: %ld\n", (long) duk_get_top(ctx));
+	return 0;
+}
+
+void test(duk_context *ctx) {
+	TEST_SAFE_CALL(test_string);
+	TEST_SAFE_CALL(test_lstring);
 }
--- api-testcases/test-eval-string.c
+++ api-testcases/test-eval-string.c
@@ -1,4 +1,5 @@
 /*===
+*** test_string (duk_safe_call)
 Hello world!
 return value is: 123.000000
 result is: 'TESTSTRING'
@@ -14,10 +15,28 @@ no result, rc=0
 top=0
 no result, rc=1
 top: 0
+==> rc=0, result='undefined'
+*** test_lstring (duk_safe_call)
+Hello world!
+return value is: 123.000000
+result is: 'TESTSTRING'
+Hello world!
+return value is: 123 (rc=0)
+return value is: Error: eval error (rc=1)
+return value is: SyntaxError: invalid object literal (line 1) (rc=1)
+top=0
+Hello world!
+top=0
+Hello world!
+no result, rc=0
+top=0
+no result, rc=1
+top: 0
+==> rc=0, result='undefined'
 ===*/
 
-void test(duk_context *ctx) {
-	int rc;
+static duk_ret_t test_string(duk_context *ctx) {
+	duk_int_t rc;
 
 	duk_eval_string(ctx, "print('Hello world!'); 123;");
 	printf("return value is: %lf\n", duk_get_number(ctx, -1));
@@ -28,29 +47,80 @@ void test(duk_context *ctx) {
 	duk_pop(ctx);
 
 	rc = duk_peval_string(ctx, "print('Hello world!'); 123;");
-	printf("return value is: %s (rc=%d)\n", duk_safe_to_string(ctx, -1), rc);
+	printf("return value is: %s (rc=%ld)\n", duk_safe_to_string(ctx, -1), (long) rc);
 	duk_pop(ctx);
 
 	rc = duk_peval_string(ctx, "throw new Error('eval error');");
-	printf("return value is: %s (rc=%d)\n", duk_safe_to_string(ctx, -1), rc);
+	printf("return value is: %s (rc=%ld)\n", duk_safe_to_string(ctx, -1), (long) rc);
 	duk_pop(ctx);
 
 	rc = duk_peval_string(ctx, "throw new Error('eval error'); obj = {");
-	printf("return value is: %s (rc=%d)\n", duk_safe_to_string(ctx, -1), rc);
+	printf("return value is: %s (rc=%ld)\n", duk_safe_to_string(ctx, -1), (long) rc);
 	duk_pop(ctx);
 
 	/* noresult variants */
 
-	printf("top=%d\n", duk_get_top(ctx));
+	printf("top=%ld\n", (long) duk_get_top(ctx));
 	duk_eval_string_noresult(ctx, "print('Hello world!'); 123;");
 
-	printf("top=%d\n", duk_get_top(ctx));
+	printf("top=%ld\n", (long) duk_get_top(ctx));
 	rc = duk_peval_string_noresult(ctx, "print('Hello world!'); 123;");
-	printf("no result, rc=%d\n", rc);
+	printf("no result, rc=%ld\n", (long) rc);
 
-	printf("top=%d\n", duk_get_top(ctx));
+	printf("top=%ld\n", (long) duk_get_top(ctx));
 	rc = duk_peval_string_noresult(ctx, "print('Hello world!'); obj = {");
-	printf("no result, rc=%d\n", rc);
+	printf("no result, rc=%ld\n", (long) rc);
 
-	printf("top: %d\n", duk_get_top(ctx));
+	printf("top: %ld\n", (long) duk_get_top(ctx));
+	return 0;
+}
+
+static duk_ret_t test_lstring(duk_context *ctx) {
+	duk_int_t rc;
+	const char *src1 = "print('Hello world!'); 123;@";
+	const char *src2 = "'testString'.toUpperCase()@";
+	const char *src3 = "throw new Error('eval error');@";
+	const char *src4 = "throw new Error('eval error'); obj = {@";
+	const char *src5 = "print('Hello world!'); obj = {@";
+
+	duk_eval_lstring(ctx, src1, strlen(src1) - 1);
+	printf("return value is: %lf\n", duk_get_number(ctx, -1));
+	duk_pop(ctx);
+
+	duk_eval_lstring(ctx, src2, strlen(src2) - 1);
+	printf("result is: '%s'\n", duk_get_string(ctx, -1));
+	duk_pop(ctx);
+
+	rc = duk_peval_lstring(ctx, src1, strlen(src1) - 1);
+	printf("return value is: %s (rc=%ld)\n", duk_safe_to_string(ctx, -1), (long) rc);
+	duk_pop(ctx);
+
+	rc = duk_peval_lstring(ctx, src3, strlen(src3) - 1);
+	printf("return value is: %s (rc=%ld)\n", duk_safe_to_string(ctx, -1), (long) rc);
+	duk_pop(ctx);
+
+	rc = duk_peval_lstring(ctx, src4, strlen(src4) - 1);
+	printf("return value is: %s (rc=%ld)\n", duk_safe_to_string(ctx, -1), (long) rc);
+	duk_pop(ctx);
+
+	/* noresult variants */
+
+	printf("top=%ld\n", (long) duk_get_top(ctx));
+	duk_eval_lstring_noresult(ctx, src1, strlen(src1) - 1);
+
+	printf("top=%ld\n", (long) duk_get_top(ctx));
+	rc = duk_peval_lstring_noresult(ctx, src1, strlen(src1) - 1);
+	printf("no result, rc=%ld\n", (long) rc);
+
+	printf("top=%ld\n", (long) duk_get_top(ctx));
+	rc = duk_peval_lstring_noresult(ctx, src5, strlen(src5) - 1);
+	printf("no result, rc=%ld\n", (long) rc);
+
+	printf("top: %ld\n", (long) duk_get_top(ctx));
+	return 0;
+}
+
+void test(duk_context *ctx) {
+	TEST_SAFE_CALL(test_string);
+	TEST_SAFE_CALL(test_lstring);
 }
