95c076f9102119bd66bfe5d7732794c6754550e5
Sami Vaarala <sami.vaarala@iki.fi>
2014-07-21 12:35:17 +0300

    Internal property test: invalid utf-8 but no 0xFF
    If a string does not begin with 0xFF it won't be treated as an internal
    string even if it is invalid UTF-8.

--- ecmascript-testcases/test-dev-internal-property-basics.js
+++ ecmascript-testcases/test-dev-internal-property-basics.js
@@ -11,15 +11,33 @@
 
 /*===
 for-in: foo
-JSON.stringify: {"foo":1}
-JX encoded: {foo:1}
-Object.keys: foo
-Object.getOwnPropertyNames: foo,bar
+for-in:  <255>quux
+JSON.stringify: {"foo":1," <195><191>quux":4}
+JX encoded: {foo:1," \xffquux":4}
+Object.keys: foo, <255>quux
+Object.getOwnPropertyNames: foo,bar, <255>quux
 direct access: 3
 ===*/
 
+function sanitizedPrint(x) {
+    var buf, i, n;
+    var tmp = '';
+
+    buf = Duktape.Buffer(x);
+    for (i = 0, n = buf.length; i < n; i++) {
+        if (buf[i] < 0x20 || buf[i] > 0x7e || buf[i] == '<' || buf[i] == '>') {
+            tmp += '<' + String(buf[i]) + '>';
+        } else {
+            tmp += String.fromCharCode(buf[i]);
+        }
+    }
+
+    print(tmp);
+}
+
 function test() {
     var internalKey;
+    var invalidUtf8Key;
     var obj;
 
     // 'foo' is enumerable, writable, configurable
@@ -35,27 +53,35 @@ function test() {
     internalKey = Duktape.dec('hex', 'ffff616263');  // \xFF\xFFabc
     obj[internalKey] = 3;
 
+    // The key \x20\xFFquux is invalid UTF-8 but not an internal string,
+    // so the property will be enumerable.  Behavior for invalid UTF-8
+    // varies.  For example, JSON.stringify() will encounter the invalid
+    // UTF-8 initial byte \xFF and serialize it like it had encountered
+    // the codepoint U+00FF (writing out the bytes c3 bf).
+    invalidUtf8Key = Duktape.dec('hex', '20ff71757578');  // \x20\xFFquux
+    obj[invalidUtf8Key] = 4;
+
     // For-in only lists enumerable keys
     for (k in obj) {
-        print('for-in:', k);
+        sanitizedPrint('for-in: ' + k);
     }
 
     // JSON.stringify() also only lists enumerable keys
-    print('JSON.stringify:', JSON.stringify(obj));
+    sanitizedPrint('JSON.stringify: ' + JSON.stringify(obj));
 
     // Even the Duktape-specific JX format doesn't list internal keys
-    print('JX encoded:', Duktape.enc('jx', obj));
+    sanitizedPrint('JX encoded: ' + Duktape.enc('jx', obj));
 
     // Object.keys() only returns enumerable keys (but not internal keys,
     // even if they're technically enumerable)
-    print('Object.keys:', Object.keys(obj));
+    sanitizedPrint('Object.keys: ' + Object.keys(obj));
 
     // Object.getOwnPropertyNames() returns also non-enumerable keys,
     // but not internal keys
-    print('Object.getOwnPropertyNames:', Object.getOwnPropertyNames(obj));
+    sanitizedPrint('Object.getOwnPropertyNames: ' + Object.getOwnPropertyNames(obj));
 
     // Direct property access to the internal property still works.
-    print('direct access:', obj[internalKey]);
+    sanitizedPrint('direct access: ' + obj[internalKey]);
 }
 
 try {
