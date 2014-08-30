1d2e0ac2614fa33b11100d8c6757a42b73ac1fc3
Sami Vaarala <sami.vaarala@iki.fi>
2014-08-30 21:46:18 +0300

    Incomplete test case for fast return code paths

---
+++ ecmascript-testcases/test-dev-fast-return-cases.js
@@ -0,0 +1,43 @@
+/*
+ *  Exercise various code paths related to fast return handling.
+ */
+
+/* FIXME: incomplete, finish when fast return implemented. */
+
+/*===
+returned 123
+returned undefined
+returned undefined
+returned 123
+===*/
+
+/* Basic case, call to test1() is from Ecmascript, so a fast return is
+ * allowed.
+ */
+function testReturn1() {
+    return 123;
+}
+
+/* Basic case, implicit return value (undefined). */
+function testReturn2() {
+    return;
+}
+
+/* Basic case, implicit return value with no explicit return statement. */
+function testReturn3() {
+}
+
+/* Return from inside a label site (created by for-loop) */
+function testForLoop1() {
+    for (;;) {
+        return 123;
+    }
+}
+try {
+    print('returned', testReturn1());
+    print('returned', testReturn2());
+    print('returned', testReturn3());
+    print('returned', testForLoop1());
+} catch (e) {
+    print(e);
+}
