ccd81c481bb4775f654f0739ad1989f8b748385c
Sami Vaarala <sami.vaarala@iki.fi>
2014-09-01 16:48:00 +0300

    Clean up Array concat() non-existent trailer test

--- ecmascript-testcases/test-bi-array-proto-concat-nonstd-trailing.js
+++ ecmascript-testcases/test-bi-array-proto-concat-nonstd-trailing.js
@@ -1,10 +1,10 @@
 /*
- *  The algorithm in E5.1 Section 15.4.4.4 seems to require that trailing
- *  non-existent elements of an array argument are effectively ignored, and
- *  don't count towards the final length of the result.  There is no explicit
- *  write for 'length' and step 5.b.iii.3.b is only executed if the element
- *  exists.  So, for non-existent trailing elements, no [[DefineOwnProperty]]
- *  is called and the result length shouldn't get updated.
+ *  The algorithm for concat() in E5.1 Section 15.4.4.4 seems to require that
+ *  trailing non-existent elements of an array argument are effectively
+ *  ignored, and don't count towards the final length of the result.  There
+ *  is no explicit write for 'length' and step 5.b.iii.3.b is only executed
+ *  if the element exists.  So, for non-existent trailing elements, no
+ *  [[DefineOwnProperty]] is called and the result length shouldn't get updated.
  *
  *  Duktape 0.11.0 had this behavior:
  *
@@ -65,15 +65,10 @@
 ---*/
 
 /*===
-1,2,3
-3
-4,5,
-3
-6,7,
-3
-1,2,3,4,5,,6,7,
-9
-true true true true true false true true false
+3 1 2 3
+3 4 5 nonexistent
+3 6 7 nonexistent
+9 1 2 3 4 5 nonexistent 6 7 nonexistent
 ===*/
 
 function test() {
@@ -81,23 +76,19 @@ function test() {
     var b = [ 4, 5, , ];  // intermediate trailing elements are not ignored
     var c = [ 6, 7, , ];  // trailing ones are ignored by standard behavior
     var d = a.concat(b, c);
-    var i;
-    var tmp;
 
-    print(a);
-    print(a.length);
-    print(b);
-    print(b.length);
-    print(c);
-    print(c.length);
-    print(d);
-    print(d.length);
-
-    tmp = [];
-    for (i = 0; i < d.length; i++) {
-        tmp.push(i in d);
+    function dump(v) {
+        var tmp = [];
+        for (var i = 0; i < v.length; i++) {
+            if (i in v) { tmp.push(v[i]) } else { tmp.push('nonexistent'); }
+        }
+        print(v.length, tmp.join(' '));
     }
-    print(tmp.join(' '));
+
+    dump(a);
+    dump(b);
+    dump(c);
+    dump(d);
 }
 
 try {
