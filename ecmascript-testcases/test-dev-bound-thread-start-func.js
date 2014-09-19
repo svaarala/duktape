a9bf21e0c4e7d28d97ec6bc8eebb132e950bba85
Sami Vaarala <sami.vaarala@iki.fi>
2014-09-19 16:56:14 +0300

    Testcase for bound initial function of a thread
    Currently, this testcase fails.

---
+++ ecmascript-testcases/test-dev-bound-thread-start-func.js
@@ -0,0 +1,44 @@
+/*
+ *  Bound function as initial function of a Thread.
+ */
+
+/*---
+{
+    "custom": true,
+    "knownissue": "initial function of a new coroutine cannot be bound"
+}
+---*/
+
+/*===
+non-bound
+func called: 321
+bound
+func called: 123
+===*/
+
+function nonBoundTest() {
+    var func = function (x) {
+        print('func called:', x);
+    };
+    var t = new Duktape.Thread(func);
+    Duktape.Thread.resume(t, 321);
+}
+
+function boundTest() {
+    var func = function (x) {
+        print('func called:', x);
+    };
+    var boundFunc = func.bind('myThis', 123);
+    var t = new Duktape.Thread(boundFunc);
+    Duktape.Thread.resume(t, 321);  // arg ignored because bound
+}
+
+try {
+    print('non-bound');
+    nonBoundTest();
+
+    print('bound');
+    boundTest();
+} catch (e) {
+    print(e.stack);
+}
