96c3ae830fc9b71d3d7fc28795647a160e7dba30
Sami Vaarala <sami.vaarala@iki.fi>
2014-09-30 13:59:23 +0300

    Sandbox example placeholder

---
+++ examples/sandbox/sandbox.c
@@ -0,0 +1,17 @@
+/*
+ *  Sandboxing example
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "duktape.h"
+
+int main(int argc, char *argv[]) {
+	/* FIXME */
+
+	/* Suppress warnings. */
+	(void) argc;
+	(void) argv;
+
+	return 1;
+}
