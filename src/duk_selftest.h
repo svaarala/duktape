526ffa6e0d2ec659f847c05c5e0bd9eeec5c6a7c
Sami Vaarala <sami.vaarala@iki.fi>
2014-10-25 03:27:34 +0300

    Fix compile warning when selftests disabled

--- src/duk_selftest.h
+++ src/duk_selftest.h
@@ -5,6 +5,8 @@
 #ifndef DUK_SELFTEST_H_INCLUDED
 #define DUK_SELFTEST_H_INCLUDED
 
+#if defined(DUK_USE_SELF_TESTS)
 DUK_INTERNAL_DECL void duk_selftest_run_tests(void);
+#endif
 
 #endif  /* DUK_SELFTEST_H_INCLUDED */
