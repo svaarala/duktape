80f179b2d67edde5810520c87f6a0112c92ae959
Sami Vaarala <sami.vaarala@iki.fi>
2015-01-04 01:00:35 +0200

    Example util file and include testcase

---
+++ ecmascript-testcases/util-helloworld.js
@@ -0,0 +1,13 @@
+/*
+ *  Test utility function.
+ */
+
+function testHelloWorldUtility () {
+    'use strict';
+
+    // It's cleanest to make all helpers 'use strict' because the helpers
+    // may be embedded both into strict and non-strict programs and must
+    // identically in both cases.
+
+    print('Hello world!');
+}
