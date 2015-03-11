9451e6b5e724e9099472c6921dab3241bdc51780
Sami Vaarala <sami.vaarala@iki.fi>
2015-03-11 22:54:22 +0200

    Update GH-131 testcase

--- ecmascript-testcases/test-bug-comma-expr-gh131.js
+++ ecmascript-testcases/test-bug-comma-expr-gh131.js
@@ -15,6 +15,8 @@ test3var 2
 test4 2
 test4notail 2
 test4var 2
+test5 2
+test5notail 2
 ===*/
 
 function test1() {
@@ -70,6 +72,17 @@ function test4var() {
     return ret;
 }
 
+// Follow-up issue where the expression after the function call is not a
+// constant but a register, but still emits no opcodes because it's a
+// register bound variable ('a').
+function test5() {
+    return (function() { var a = 1; return (function() { a = 2 })(), a })();
+}
+
+function test5notail() {
+    return (function() { var a = 1; return (function() { 'use duk notail'; a = 2 })(), a })();
+}
+
 try {
     print('test1', test1());
     print('test1notail', test1notail());
@@ -83,6 +96,8 @@ try {
     print('test4', test4());
     print('test4notail', test4notail());
     print('test4var', test4var());
+    print('test5', test5());
+    print('test5notail', test5notail());
 } catch (e) {
     print(e.stack || e);
 }
