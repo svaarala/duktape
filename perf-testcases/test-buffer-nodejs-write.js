318e6e947c1a68eea463e8c90c1c58602c491235
Sami Vaarala <sami.vaarala@iki.fi>
2015-04-10 22:30:20 +0300

    Buffer read/write perf testcases

---
+++ perf-testcases/test-buffer-nodejs-write.js
@@ -0,0 +1,16 @@
+function test() {
+    var b = new Buffer(4096);
+    var i;
+
+    print(typeof b);
+
+    for (i = 0; i < 1e8; i++) {
+        b[100] = 123;
+    }
+}
+
+try {
+    test();
+} catch (e) {
+    print(e.stack || e);
+}
