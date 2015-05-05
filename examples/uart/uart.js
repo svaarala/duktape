0293b9e8806094a3da568698130fb79c9886b874
Shanjin Yang <1150660744@qq.com>
2015-05-05 18:03:26 +0800

    Now uart can be controlled by js.
    Signed-off-by: Shanjin Yang <sjyangv0@qq.com>

--- examples/uart/uart.js
+++ examples/uart/uart.js
@@ -1,9 +1,12 @@
 function test() {
 	var fd = uart.open('/dev/ttyUSB0');
-	fd = 3;
-	//print('received message, type: %d', (int) fd);
 	uart.write(fd, 'hello world\n');
-	uart.read(fd);
+	var i = 10;
+	while (i--) {
+		var buf = uart.read(fd);
+		print('JS read: ' + buf + '\n');
+	}
 	uart.close(fd);
 }
+
 test();
--- examples/uart/uart_bind.c
+++ examples/uart/uart_bind.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015 Nanchao Inc. All rights reserved.
+ * Written by Shanjin Yang <sjyangv0@qq.com>.
  */
 
 #include "duktape.h"
@@ -7,9 +8,7 @@
 
 static int ruff_uart_open(duk_context *ctx)
 {
-	char filename[32];
 	char *uart_name;
-
 	uart_name = (char *)duk_to_string(ctx, -1);
 
 	int uart_fd = uart_open(uart_name);
@@ -18,12 +17,9 @@ static int ruff_uart_open(duk_context *ctx)
 	if (uart_fd < 0) {
 		return DUK_RET_API_ERROR;
 	} else {
-		duk_push_string(ctx, filename);
-		return 0;
+		duk_push_int(ctx, uart_fd);
+		return 1;
 	}
-	duk_push_int(ctx, uart_fd);
-
-	return uart_fd;
 }
 
 static int ruff_uart_close(duk_context *ctx)
@@ -38,20 +34,25 @@ static int ruff_uart_close(duk_context *ctx)
 
 static int ruff_uart_read(duk_context *ctx)
 {
-	void *p;
+	char *p;
 	int size = 1024;
 	char buf[size];
+	memset(buf, '\0', size); 
 	int uart_fd = duk_to_int(ctx, 0);
-	fprintf(stderr, "uart %d read...\n", uart_fd);
+	/*fprintf(stderr, "uart %d read...\n", uart_fd);*/
+	uart_read(uart_fd, buf, size);
+#if 0
 	/*TODO*/
 	int i = 10;
 	while (i--) {
 		uart_read(uart_fd, buf, size);
 		fprintf(stderr, "uart %d read: %c\n", uart_fd, (char)buf[0]);
 	}
+#endif 
 	p = duk_push_buffer(ctx, size, 0);
-	//fprintf(stderr, "%c", (char *)p);
-	return 0;
+	duk_push_string(ctx, buf);
+
+	return 1;
 }
 
 static int ruff_uart_write(duk_context *ctx)
@@ -59,7 +60,7 @@ static int ruff_uart_write(duk_context *ctx)
 	int uart_fd = duk_to_int(ctx, 0);
 	char *write_buf;
 	write_buf = (char *)duk_to_string(ctx, 1);
-	fprintf(stderr, "uart %d write: %s", uart_fd, write_buf);
+	fprintf(stderr, "uart fd %d write: %s", uart_fd, write_buf);
 	/*TODO*/
 	uart_write(uart_fd, write_buf, strlen(write_buf));
 	return 0;
@@ -94,11 +95,7 @@ int main(int argc, char **argv)
 	duk_put_global_string(ctx, "uart");
 
 	/* my_module is now registered in the global object. */
-	//duk_eval_string(ctx, "uart.open('/dev/ttyUSB0')");
 	duk_eval_file_noresult(ctx, "uart.js");
-	//duk_eval_string(ctx, "uart.write('hello world\n')");
-
-	/* ... */
 
 	duk_destroy_heap(ctx);
 
