a45d20ac1ae09d9e24e772353b14b12559311588
Shanjin Yang <1150660744@qq.com>
2015-05-09 12:53:48 +0800

    Implement lcd_print funciton.
    It can print string now.
    
    Signed-off-by: Shanjin Yang <sjyangv0@qq.com>

--- examples/lcd/lcd.js
+++ examples/lcd/lcd.js
@@ -12,6 +12,7 @@
 // Define GPIO to LCD mapping
 LCD_CHR = 1;
 LCD_CMD = 0;
+LCD_WIDTH = 16;
 
 False = 0;
 True = 1;
@@ -27,6 +28,10 @@ var d5 = gpio.open(24, 1);
 var d6 = gpio.open(23, 1);
 var d7 = gpio.open(18, 1);
 
+function delay(ms) {
+	while(ms--);
+}
+
 function lcd_init() {
 	/*Initialise display*/
 	lcd_byte(0x33,LCD_CMD);  // 110011 Initialise
@@ -36,14 +41,12 @@ function lcd_init() {
 	lcd_byte(0x28,LCD_CMD);  // 101000 Data length, number of lines, font size
 	lcd_byte(0x01,LCD_CMD);  // 000001 Clear display
 	/*TODO*/
-	var i = 10000;
-	while (i--);
+	delay(10000);
 }
 
 function lcd_toggle_enable() {
 	gpio.write(en, 1);
-	var i = 10000;
-	while (i--);
+	delay(10000);
 	gpio.write(en, 0);
 }
 
@@ -97,17 +100,25 @@ function lcd_byte(bits, mode) {
 	lcd_toggle_enable();
 }
 
+function lcd_print(str) {
+        var length = str.length;
+	lcd_byte(LCD_LINE_1, LCD_CMD);
+        for(var i = 0; (i < length) && (i < 16); i++) {
+		lcd_byte(str.charCodeAt(i), LCD_CHR);
+	}
+
+	lcd_byte(LCD_LINE_2, LCD_CMD);
+	for (; (i > 8) && (i < length); i++) {
+		lcd_byte(str.charCodeAt(i), LCD_CHR);
+	}
+}
+
 function main() {
-	//TODO: set bcm mode
 	lcd_init();
 	
 	while (1) {
-		//lcd_string("hello world\n");
-		lcd_byte(LCD_LINE_1, LCD_CMD);
-		lcd_byte(0x41, LCD_CHR);
-
-		var i = 1000000;
-		while (i--);
+		lcd_print("hello world Shanjin Yang cool cool");
+		delay(1000000);
 	}
 }
 
