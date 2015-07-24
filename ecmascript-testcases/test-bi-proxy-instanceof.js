ef9e8f991ba3b95ae22a7bb4df20c3af0ca75236
Sami Vaarala <sami.vaarala@iki.fi>
2015-07-24 05:47:55 +0300

    Add testcases for Proxy instanceof
    Basic testcase for Proxy without a getPrototypeOf trap.  Instanceof
    should traverse to the Proxy target as if the trap was missing.

---
+++ ecmascript-testcases/test-bi-proxy-instanceof.js
@@ -0,0 +1,45 @@
+/*
+ *  Proxy values in instanceof operator
+ */
+
+/*===
+true
+true
+true
+===*/
+
+/* 'instanceof' operator with a Proxy target that doesn't implement the
+ * 'getPrototypeOf' trap.
+ */
+function instanceofWithoutTrapTest() {
+    // Normal object
+    var a = new Error('foo');
+
+    // Proxy of an error
+    var b = new Proxy(a, {});
+
+    // Proxy object, target object's prototype is also a Proxy
+    function MyError(msg) {
+        this.message = msg;
+    }
+    MyError.prototype = new Proxy(Error.prototype, {});
+    var c = new Proxy(new MyError('bar'), {});
+
+    // FIXME: test for:
+    //     var d = new Proxy(b), {})
+    // once Proxy of a Proxy is supported
+
+    print(a instanceof Error);
+    print(b instanceof Error);
+    print(c instanceof Error);
+}
+
+try {
+    instanceofWithoutTrapTest();
+} catch (e) {
+    print(e.stack || e);
+}
+
+// FIXME: add test for instanceof with a getPrototypeOf trap
+
+// FIXME: Proxy of a Proxy
