32d293905001c83fb737eaba93e0dd03e1cc74d9
Sami Vaarala <sami.vaarala@iki.fi>
2015-08-10 22:42:49 +0300

    Add duk_pnew() API test

--- tests/api/test-new.c
+++ tests/api/test-new.c
@@ -1,10 +1,20 @@
 /*===
-*** test_1 (duk_safe_call)
+*** test_new (duk_safe_call)
 duk_is_constructor_call: 1
 key='own_key', value='own_value'
 key='name', value='my prototype'
 duk_is_constructor_call: 1
 key='name', value='replacement'
+final top: 0
+==> rc=0, result='undefined'
+*** test_pnew_1 (duk_safe_call)
+pnew returned: 0
+result: Error: my error message
+final top: 0
+==> rc=0, result='undefined'
+*** test_pnew_2 (duk_safe_call)
+pnew returned: 1
+result: TypeError: not constructable
 final top: 0
 ==> rc=0, result='undefined'
 ===*/
@@ -39,7 +49,7 @@ static duk_ret_t my_func_2(duk_context *ctx) {
 	return 1;
 }
 
-static duk_ret_t test_1(duk_context *ctx) {
+static duk_ret_t test_new(duk_context *ctx) {
 	/* Create a constructor (function object).  Its "prototype" property
 	 * controls the internal prototype of created instances.
 	 */
@@ -70,6 +80,40 @@ static duk_ret_t test_1(duk_context *ctx) {
 	return 0;
 }
 
+/* Successful duk_pnew(). */
+static duk_ret_t test_pnew_1(duk_context *ctx) {
+	duk_int_t rc;
+
+	duk_eval_string(ctx, "Error");  /* constructor */
+	duk_push_string(ctx, "my error message");
+	rc = duk_pnew(ctx, 1);
+	printf("pnew returned: %ld\n", (long) rc);
+	printf("result: %s\n", duk_safe_to_string(ctx, -1));
+	duk_pop(ctx);
+
+	printf("final top: %ld\n", (long) duk_get_top(ctx));
+	return 0;
+}
+
+/* Failed duk_pnew(). */
+static duk_ret_t test_pnew_2(duk_context *ctx) {
+	duk_int_t rc;
+
+	duk_eval_string(ctx, "null");  /* constructor: not callable */
+	duk_push_string(ctx, "whatever");
+	duk_push_string(ctx, "whatever");
+	duk_push_string(ctx, "whatever");
+	rc = duk_pnew(ctx, 3);
+	printf("pnew returned: %ld\n", (long) rc);
+	printf("result: %s\n", duk_safe_to_string(ctx, -1));
+	duk_pop(ctx);
+
+	printf("final top: %ld\n", (long) duk_get_top(ctx));
+	return 0;
+}
+
 void test(duk_context *ctx) {
-	TEST_SAFE_CALL(test_1);
+	TEST_SAFE_CALL(test_new);
+	TEST_SAFE_CALL(test_pnew_1);
+	TEST_SAFE_CALL(test_pnew_2);
 }
