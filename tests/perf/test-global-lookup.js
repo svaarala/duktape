7c0349af45254ac72ceae046b7f239e48fc7598f
Sami Vaarala <sami.vaarala@iki.fi>
2015-08-12 00:01:27 +0300

    Perf test for global variable lookup

---
+++ tests/perf/test-global-lookup.js
@@ -0,0 +1,128 @@
+/*
+ *  Global variable lookup.  This matters because all calls like
+ *  "print('hello');" go through a slow path GETVAR lookup.
+ */
+
+function test() {
+    var i;
+
+    for (i = 0;i < 1e6; i++) {
+        /* Repeat 100x to ensure loop control overhead is negligible. */
+
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+        void print;
+    }
+}
+
+try {
+    test();
+} catch (e) {
+    print(e.stack || e);
+}
