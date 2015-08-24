0c6686a29ff73d2bd9b594e19e3e1fcb6b48beec
Sami Vaarala <sami.vaarala@iki.fi>
2015-08-25 01:19:48 +0300

    Perf test cases improvements: LDCONST, LDREG

---
+++ tests/perf/test-reg-load.lua
@@ -0,0 +1,127 @@
+local function test()
+    local t
+    local r0 = 123.0
+    local r1 = 123.1
+    local r2 = 123.2
+    local r3 = 123.3
+    local r4 = 123.4
+    local r5 = 123.5
+    local r6 = 123.6
+    local r7 = 123.7
+    local r8 = 123.8
+    local r9 = 123.9
+
+    for i=1,1e7 do
+        t = r0
+        t = r1
+        t = r2
+        t = r3
+        t = r4
+        t = r5
+        t = r6
+        t = r7
+        t = r8
+        t = r9
+
+        t = r0
+        t = r1
+        t = r2
+        t = r3
+        t = r4
+        t = r5
+        t = r6
+        t = r7
+        t = r8
+        t = r9
+
+        t = r0
+        t = r1
+        t = r2
+        t = r3
+        t = r4
+        t = r5
+        t = r6
+        t = r7
+        t = r8
+        t = r9
+
+        t = r0
+        t = r1
+        t = r2
+        t = r3
+        t = r4
+        t = r5
+        t = r6
+        t = r7
+        t = r8
+        t = r9
+
+        t = r0
+        t = r1
+        t = r2
+        t = r3
+        t = r4
+        t = r5
+        t = r6
+        t = r7
+        t = r8
+        t = r9
+
+        t = r0
+        t = r1
+        t = r2
+        t = r3
+        t = r4
+        t = r5
+        t = r6
+        t = r7
+        t = r8
+        t = r9
+
+        t = r0
+        t = r1
+        t = r2
+        t = r3
+        t = r4
+        t = r5
+        t = r6
+        t = r7
+        t = r8
+        t = r9
+
+        t = r0
+        t = r1
+        t = r2
+        t = r3
+        t = r4
+        t = r5
+        t = r6
+        t = r7
+        t = r8
+        t = r9
+
+        t = r0
+        t = r1
+        t = r2
+        t = r3
+        t = r4
+        t = r5
+        t = r6
+        t = r7
+        t = r8
+        t = r9
+
+        t = r0
+        t = r1
+        t = r2
+        t = r3
+        t = r4
+        t = r5
+        t = r6
+        t = r7
+        t = r8
+        t = r9
+    end
+end
+
+test()
