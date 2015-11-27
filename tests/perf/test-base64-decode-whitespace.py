aacdaf13d1333217cd9940d3a8eba19d71f47189
Sami Vaarala <sami.vaarala@iki.fi>
2015-11-27 14:31:45 +0200

    Perf tests for base64

---
+++ tests/perf/test-base64-decode-whitespace.py
@@ -0,0 +1,29 @@
+import math
+import random
+
+def test():
+	tmp1 = []
+	tmp2 = []
+
+	print('build')
+	for i in xrange(1024):
+		tmp1.append('%x' % math.floor(random.random() * 16))
+	tmp1 = ''.join(tmp1)
+	for i in xrange(1024):
+		tmp2.append(tmp1)
+	tmp2 = ''.join(tmp2)
+	tmp2 = tmp2.encode('base64')
+
+	tmp3 = []
+	i = 0
+	while i < len(tmp2):
+		tmp3.append(tmp2[i:i+77])
+		i += 77
+	tmp2 = '\n'.join(tmp3) + '\n'
+
+	print(len(tmp2))
+	print('run')
+	for i in xrange(1000):
+		ign = tmp2.decode('base64')
+
+test()
---
+++ tests/perf/test-base64-encode.py
@@ -0,0 +1,21 @@
+import math
+import random
+
+def test():
+	tmp1 = []
+	tmp2 = []
+
+	print('build')
+	for i in xrange(1024):
+		tmp1.append(chr(int(math.floor(random.random() * 256))))
+	tmp1 = ''.join(tmp1)
+	for i in xrange(1024):
+		tmp2.append(tmp1)
+	tmp2 = ''.join(tmp2)
+
+	print(len(tmp2))
+	print('run')
+	for i in xrange(1000):
+		ign = tmp2.encode('base64')
+
+test()
