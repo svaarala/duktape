d72a0016869245033cf48021479ee343596d99e0
Sami Vaarala <sami.vaarala@iki.fi>
2016-01-17 14:24:58 +0200

    Regexp literal curly brace testcase improvements
    * Rename testcase for consistency with existing cases.
    
    * Test for a few more invalid quantifier cases.
    
    * Testcase for quantifier digit count

---
+++ tests/ecmascript/test-dev-regexp-quantifier-digits.js
@@ -0,0 +1,63 @@
+/*
+ *  Duktape has an internal digit limit (9 digits) for regexp quantifier
+ *  min/max counts.
+ */
+
+/*---
+{
+    "custom": true
+}
+---*/
+
+/*===
+["xxx"]
+null
+===*/
+
+// 8 digits
+try {
+    print(eval("JSON.stringify(/x{3,99999999}/.exec('xxx'))"));
+} catch (e) {
+    print(e);
+}
+try {
+    print(eval("JSON.stringify(/x{88888888,99999999}/.exec('xxx'))"));
+} catch (e) {
+    print(e);
+}
+
+/*===
+["xxx"]
+null
+===*/
+
+// 9 digits, still accepted
+try {
+    print(eval("JSON.stringify(/x{3,999999999}/.exec('xxx'))"));
+} catch (e) {
+    print(e);
+}
+try {
+    print(eval("JSON.stringify(/x{333333333,999999999}/.exec('xxx'))"));
+} catch (e) {
+    print(e);
+}
+
+/*===
+null
+null
+===*/
+
+// 10 digits: SyntaxError without non-standard literal curly braces
+// (DUK_USE_NONSTD_REGEXP_BRACES), treated as a literal with non-standard
+// curly braces.
+try {
+    print(eval("JSON.stringify(/x{3,9999999999}/.exec('xxx'))"));
+} catch (e) {
+    print(e);
+}
+try {
+    print(eval("JSON.stringify(/x{3333333333,9999999999}/.exec('xxx'))"));
+} catch (e) {
+    print(e);
+}
