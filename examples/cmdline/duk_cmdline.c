89dbef2baba9b9606be154c9816398a969454353
Sami Vaarala <sami.vaarala@iki.fi>
2016-02-02 12:58:51 +0200

    Add trivial file I/O to duk command for testing
    Add global bindings readFile() and writeFile() for easier testing
    of JSON parsing etc.  These are in no way official Duktape bindings,
    just part of the 'duk' command (when enabled).

--- examples/cmdline/duk_cmdline.c
+++ examples/cmdline/duk_cmdline.c
@@ -46,6 +46,9 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #endif
+#if defined(DUK_CMDLINE_FILEIO)
+#include <errno.h>
+#endif
 #if defined(EMSCRIPTEN)
 #include <emscripten.h>
 #endif
@@ -85,6 +88,10 @@ void AJS_Free(void *udata, void *ptr);
 #define  LINEBUF_SIZE       65536
 
 static int interactive_mode = 0;
+
+/*
+ *  Misc helpers
+ */
 
 #if !defined(NO_RLIMIT)
 static void set_resource_limits(rlim_t mem_limit_value) {
@@ -278,6 +285,10 @@ static int wrapped_compile_execute(duk_context *ctx) {
 
 	return 0;  /* duk_safe_call() cleans up */
 }
+
+/*
+ *  Execute from file handle etc
+ */
 
 static int handle_fh(duk_context *ctx, FILE *f, const char *filename, const char *bytecode_filename) {
 	char *buf = NULL;
@@ -600,6 +611,108 @@ static int handle_interactive(duk_context *ctx) {
 }
 #endif  /* NO_READLINE */
 
+/*
+ *  Simple file read/write bindings
+ */
+
+#if defined(DUK_CMDLINE_FILEIO)
+static duk_ret_t fileio_read_file(duk_context *ctx) {
+	const char *fn;
+	char *buf;
+	size_t len;
+	size_t off;
+	int rc;
+	FILE *f;
+
+	fn = duk_require_string(ctx, 0);
+	f = fopen(fn, "rb");
+	if (!f) {
+		duk_error(ctx, DUK_ERR_TYPE_ERROR, "cannot open file %s for reading, errno %ld: %s",
+		          fn, (long) errno, strerror(errno));
+	}
+
+	rc = fseek(f, 0, SEEK_END);
+	if (rc < 0) {
+		(void) fclose(f);
+		duk_error(ctx, DUK_ERR_TYPE_ERROR, "fseek() failed for %s, errno %ld: %s",
+		          fn, (long) errno, strerror(errno));
+	}
+	len = (size_t) ftell(f);
+	rc = fseek(f, 0, SEEK_SET);
+	if (rc < 0) {
+		(void) fclose(f);
+		duk_error(ctx, DUK_ERR_TYPE_ERROR, "fseek() failed for %s, errno %ld: %s",
+		          fn, (long) errno, strerror(errno));
+	}
+
+	buf = (char *) duk_push_fixed_buffer(ctx, (duk_size_t) len);
+	for (off = 0; off < len;) {
+		size_t got;
+		got = fread((void *) (buf + off), 1, len - off, f);
+		if (ferror(f)) {
+			(void) fclose(f);
+			duk_error(ctx, DUK_ERR_TYPE_ERROR, "error while reading %s", fn);
+		}
+		if (got == 0) {
+			if (feof(f)) {
+				break;
+			} else {
+				(void) fclose(f);
+				duk_error(ctx, DUK_ERR_TYPE_ERROR, "error while reading %s", fn);
+			}
+		}
+		off += got;
+	}
+
+	if (f) {
+		(void) fclose(f);
+	}
+
+	return 1;
+}
+
+static duk_ret_t fileio_write_file(duk_context *ctx) {
+	const char *fn;
+	const char *buf;
+	size_t len;
+	size_t off;
+	FILE *f;
+
+	fn = duk_require_string(ctx, 0);
+	f = fopen(fn, "wb");
+	if (!f) {
+		duk_error(ctx, DUK_ERR_TYPE_ERROR, "cannot open file %s for writing, errno %ld: %s",
+		          fn, (long) errno, strerror(errno));
+	}
+
+	len = 0;
+	buf = (char *) duk_to_buffer(ctx, 1, &len);
+	for (off = 0; off < len;) {
+		size_t got;
+		got = fwrite((const void *) (buf + off), 1, len - off, f);
+		if (ferror(f)) {
+			(void) fclose(f);
+			duk_error(ctx, DUK_ERR_TYPE_ERROR, "error while writing %s", fn);
+		}
+		if (got == 0) {
+			(void) fclose(f);
+			duk_error(ctx, DUK_ERR_TYPE_ERROR, "error while writing %s", fn);
+		}
+		off += got;
+	}
+
+	if (f) {
+		(void) fclose(f);
+	}
+
+	return 0;
+}
+#endif  /* DUK_CMDLINE_FILEIO */
+
+/*
+ *  Duktape heap lifecycle
+ */
+
 #if defined(DUK_CMDLINE_DEBUGGER_SUPPORT)
 static void debugger_detached(void *udata) {
 	duk_context *ctx = (duk_context *) udata;
@@ -718,6 +831,13 @@ static duk_context *create_duktape_heap(int alloc_provider, int debugger, int aj
 	}
 #endif
 
+#if defined(DUK_CMDLINE_FILEIO)
+	duk_push_c_function(ctx, fileio_read_file, 1 /*nargs*/);
+	duk_put_global_string(ctx, "readFile");
+	duk_push_c_function(ctx, fileio_write_file, 2 /*nargs*/);
+	duk_put_global_string(ctx, "writeFile");
+#endif
+
 	if (debugger) {
 #if defined(DUK_CMDLINE_DEBUGGER_SUPPORT)
 		fprintf(stderr, "Debugger enabled, create socket and wait for connection\n");
@@ -781,6 +901,10 @@ static void destroy_duktape_heap(duk_context *ctx, int alloc_provider) {
 	ajsheap_free();
 #endif
 }
+
+/*
+ *  Main
+ */
 
 int main(int argc, char *argv[]) {
 	duk_context *ctx = NULL;
