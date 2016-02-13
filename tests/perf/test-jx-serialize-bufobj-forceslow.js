9e455b80d6804575fb12ffb7feaa4346ed235397
Sami Vaarala <sami.vaarala@iki.fi>
2016-02-13 02:44:11 +0200

    Perf testcases for JSON/JX

---
+++ tests/perf/test-jx-serialize-bufobj-forceslow.js
@@ -0,0 +1,36 @@
+if (typeof print !== 'function') { print = console.log; }
+
+function build() {
+    var obj = {};
+    var ab = new ArrayBuffer(32);
+    var vw = new Uint32Array(ab, 4, 3);
+    var i;
+    for (i = 0; i < ab.length; i++) {
+      ab[i] = i;
+    }
+
+    return {
+        foo: [ ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab ],
+        bar: [ vw, vw, vw, vw, vw, vw, vw, vw, vw, vw, vw, vw, vw, vw, vw, vw ],
+        toJSON: 123  // forces slow path in Duktape 1.5.x, but not called
+    };
+}
+
+function test() {
+    var obj;
+    var i;
+    var ignore;
+
+    obj = build();
+    for (i = 0; i < 3e5; i++) {
+        ignore = Duktape.enc('jx', obj);
+    }
+    //print(ignore);
+}
+
+try {
+    test();
+} catch (e) {
+    print(e.stack || e);
+    throw e;
+}
---
+++ tests/perf/test-jx-serialize-bufobj.js
@@ -0,0 +1,35 @@
+if (typeof print !== 'function') { print = console.log; }
+
+function build() {
+    var obj = {};
+    var ab = new ArrayBuffer(32);
+    var vw = new Uint32Array(ab, 4, 3);
+    var i;
+    for (i = 0; i < ab.length; i++) {
+      ab[i] = i;
+    }
+
+    return {
+        foo: [ ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab, ab ],
+        bar: [ vw, vw, vw, vw, vw, vw, vw, vw, vw, vw, vw, vw, vw, vw, vw, vw ]
+    };
+}
+
+function test() {
+    var obj;
+    var i;
+    var ignore;
+
+    obj = build();
+    for (i = 0; i < 3e5; i++) {
+        ignore = Duktape.enc('jx', obj);
+    }
+    //print(ignore);
+}
+
+try {
+    test();
+} catch (e) {
+    print(e.stack || e);
+    throw e;
+}
