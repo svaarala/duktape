0175141b13b6244ca9b1b1c3315ac5ea52e5f35b
Sami Vaarala <sami.vaarala@iki.fi>
2016-03-01 00:47:10 +0200

    Perf testcase for object/string garbage

---
+++ tests/perf/test-string-garbage.lua
@@ -0,0 +1,29 @@
+local function test()
+    local s = 'x'
+    local t
+
+    for i=1,5e5 do
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+        t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'; t = s .. 'y'
+    end
+end
+
+test()
---
+++ tests/perf/test-string-garbage.py
@@ -0,0 +1,26 @@
+def test():
+	s = 'x'
+
+	for i in xrange(int(5e5)):
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+		t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'; t = s + 'y'
+
+test()
