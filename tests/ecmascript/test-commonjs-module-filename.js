86572c684ca371d820287885d5c0c8c04a06ecc8
Sami Vaarala <sami.vaarala@iki.fi>
2016-04-01 17:52:30 +0300

    Testcase fix for module.filename rename

--- tests/ecmascript/test-commonjs-module-filename.js
+++ tests/ecmascript/test-commonjs-module-filename.js
@@ -1,5 +1,5 @@
 /*
- *  Duktape 1.5 added module.fileName and module.name support.
+ *  Duktape 1.5 added module.filename and module.name support.
  */
 
 /*===
@@ -9,7 +9,7 @@ test/foo2
 2
 foo2
 TIME INF test/foo2: test
-override .name and .fileName
+override .name and .filename
 test/bar2 test/bar2 undefined undefined
 my_source.js
 2
@@ -40,27 +40,27 @@ function test() {
     };
 
     // Default behavior, module wrapper .fileName is resolved module ID,
-    // .name is last component of resolved ID (module.fileName and module.name
+    // .name is last component of resolved ID (module.filename and module.name
     // are not set by default which triggers the default values).
 
     Duktape.modSearch = function modSearch(id, require, exports, module) {
-        print(id, module.id, module.fileName, module.name);
+        print(id, module.id, module.filename, module.name);
         return testSource;
     };
 
     print('default behavior');
     var tmp = require('test/foo1/../foo2');
 
-    // Override module.fileName and module.name in modSearch().
+    // Override module.filename and module.name in modSearch().
 
     Duktape.modSearch = function modSearch(id, require, exports, module) {
-        print(id, module.id, module.fileName, module.name);
-        module.fileName = 'my_source.js';  // match source filename for example
+        print(id, module.id, module.filename, module.name);
+        module.filename = 'my_source.js';  // match source filename for example
         module.name = 'my_module';
         return testSource;
     };
 
-    print('override .name and .fileName');
+    print('override .name and .filename');
     var tmp = require('test/bar1/../bar2');
 
     // Test that the forced .name property of the module wrapper function
@@ -73,8 +73,8 @@ function test() {
     global.myFileName = 123;  // this should be visible
 
     Duktape.modSearch = function modSearch(id, require, exports, module) {
-        print(id, module.id, module.fileName, module.name);
-        module.fileName = 'myFileName';
+        print(id, module.id, module.filename, module.name);
+        module.filename = 'myFileName';
         return 'print(typeof Math); print(typeof myFileName); print(myFileName);';
     };
 
