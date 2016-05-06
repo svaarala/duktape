b9540424e884fa181972cab25c9c0791169b0794
Sami Vaarala <sami.vaarala@iki.fi>
2016-05-06 19:58:00 +0300

    Add context dump v1 compat implementations

--- extras/duk-v1-compat/duk_v1_compat.c
+++ extras/duk-v1-compat/duk_v1_compat.c
@@ -1,1 +1,15 @@
-/* FIXME */
+#include <stdio.h>
+#include "duktape.h"
+#include "duk_v1_compat.h"
+
+void duk_dump_context_stdout(duk_context *ctx) {
+	duk_push_context_dump(ctx);
+	fprintf(stdout, "%s\n", duk_safe_to_string(ctx, -1));
+	duk_pop(ctx);
+}
+
+void duk_dump_context_stderr(duk_context *ctx) {
+	duk_push_context_dump(ctx);
+	fprintf(stderr, "%s\n", duk_safe_to_string(ctx, -1));
+	duk_pop(ctx);
+}
--- extras/duk-v1-compat/duk_v1_compat.h
+++ extras/duk-v1-compat/duk_v1_compat.h
@@ -1,6 +1,9 @@
 #if !defined(DUK_V1_COMPAT_INCLUDED)
 #define DUK_V1_COMPAT_INCLUDED
 
-/* FIXME */
+#include "duktape.h"
+
+extern void duk_dump_context_stdout(duk_context *ctx);
+extern void duk_dump_context_stderr(duk_context *ctx);
 
 #endif  /* DUK_V1_COMPAT_INCLUDED */
