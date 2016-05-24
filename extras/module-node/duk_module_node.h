fc78f9b9472629c93aeaa3318758c49f0b0e8caa
Bruce Pascoe <fatcerberus@icloud.com>
2016-05-24 12:36:36 -0400

    Add a Node.js-compatible module loader extra
    This loader has the following features:
    
      * Application-provided callbacks for module resolution and loading
      * Module caching based on resolved module ID
      * Tolerates circular `require()` chains
      * Supports `module.exports`, `module.loaded`, and `module.require`
      * Supports `require.cache`
      * Supports `__filename` (but not `__dirname`)

---
+++ extras/module-node/duk_module_node.h
@@ -0,0 +1,8 @@
+#if !defined(DUK_MODULE_NODE_H_INCLUDED)
+#define DUK_MODULE_NODE_H_INCLUDED
+
+#include "duktape.h"
+
+extern void duk_module_node_init (duk_context *ctx);
+
+#endif  /* DUK_MODULE_NODE_H_INCLUDED */
