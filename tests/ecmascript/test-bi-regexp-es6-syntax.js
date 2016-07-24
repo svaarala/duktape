8ca309733adb41e5ee7d182cbcb663f8b6fde561
Sami Vaarala <sami.vaarala@iki.fi>
2016-07-24 23:30:25 +0300

    Testcase changes for ES6 literal right bracket

---
+++ tests/ecmascript/test-bi-regexp-es6-syntax.js
@@ -0,0 +1,44 @@
+/*
+ *  Test for currently supported ES6 Section B.1.4 syntax, enabled by
+ *  DUK_USE_ES6_REGEXP_SYNTAX.
+ */
+
+/*===
+["{"]
+["}"]
+["{1234}"]
+["]"]
+SyntaxError
+["$"]
+===*/
+
+function test() {
+    var r;
+
+    function test(re, inp) {
+        try {
+            var res = new RegExp(re).exec(inp);
+            print(Duktape.enc('jx', res));
+        } catch (e) {
+            print(e.name);
+        }
+    }
+
+    // literal curly braces
+    test('{', 'abc{xyz');
+    test('}', 'abc}xyz');
+    test('{\\d+}', 'foo{1234}bar');
+
+    // literal brackets
+    test(']', 'foo]bar');
+    test('[', 'foo[bar');  // left bracket not yet supported
+
+    // escaping dollar sign
+    test('\\$', 'foo$bar');
+}
+
+try {
+    test();
+} catch (e) {
+    print(e.stack || e);
+}
