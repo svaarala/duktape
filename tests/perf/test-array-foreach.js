42d73460167f02942ea69367949b8741fe24f047
Sami Vaarala <sami.vaarala@iki.fi>
2016-08-02 01:01:26 +0300

    Add a few array perf tests

---
+++ tests/perf/test-array-foreach.js
@@ -0,0 +1,25 @@
+/*
+ *  Basic array.forEach() performance
+ */
+
+if (typeof print !== 'function') { print = console.log; }
+
+function test() {
+    var arr = [];
+    var i;
+
+    for (i = 0; i < 1e3; i++) {
+        arr.push('value-' + i);
+    }
+
+    for (i = 0; i < 2e4; i++) {
+        arr.forEach(function () {});
+    }
+}
+
+try {
+    test();
+} catch (e) {
+    print(e.stack || e);
+    throw e;
+}
