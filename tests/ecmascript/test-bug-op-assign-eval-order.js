059f2ef3125d0024041159543344d20816dfa599
Sami Vaarala <sami.vaarala@iki.fi>
2016-10-04 19:05:02 +0300

    Testcase for <op>= eval order (and related bug)

---
+++ tests/ecmascript/test-bug-op-assign-eval-order.js
@@ -0,0 +1,34 @@
+/*
+ *  In X <op>= Y the LHS value should be evaluated first, before evaluating
+ *  the RHS.  Duktape 1.5.x and before evaluates RHS first, which matters in
+ *  some chained <op>= statements.
+ *
+ *  See: https://github.com/svaarala/duktape/pull/987#issuecomment-251432738.
+ */
+
+/*===
+Write x: 10
+TEST
+Read x: 10
+Read x: 10
+Write x: 40
+Write x: 50
+Read x: 50
+Final x: 50
+===*/
+
+if (typeof print === 'undefined') { print = console.log; }
+
+var obj = {};
+var my_x;
+Object.defineProperty(obj, 'x', {
+    set: function (v) { print('Write x:', v); my_x = v; return true; },
+    get: function () { print('Read x:', my_x); return my_x; }
+});
+
+with (obj) {
+    x = 10;
+    print('TEST');
+    x += (x *= 4);
+}
+print('Final x:', obj.x);
