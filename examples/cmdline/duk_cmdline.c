dc096435255f1b859d49da51595654610c3d531c
Sami Vaarala <sami.vaarala@iki.fi>
2016-11-13 05:39:13 +0200

    Add linenoise hints to 'duk'

--- examples/cmdline/duk_cmdline.c
+++ examples/cmdline/duk_cmdline.c
@@ -24,7 +24,7 @@
 /* Helper define to enable a feature set; can also use separate defines. */
 #if defined(DUK_CMDLINE_FANCY)
 #define DUK_CMDLINE_LINENOISE
-#define DUK_CMDLINE_LINENOISE_COMPLETION
+#define DUK_CMDLINE_LINENOISE_COMPLETION  /* Enables completion and hints. */
 #define DUK_CMDLINE_RLIMIT
 #define DUK_CMDLINE_SIGNAL
 #endif
@@ -351,191 +351,172 @@ static duk_ret_t wrapped_compile_execute(duk_context *ctx, void *udata) {
 #if defined(DUK_CMDLINE_LINENOISE_COMPLETION)
 static duk_context *completion_ctx;
 
-static int completion_idpart(unsigned char c) {
-	/* Very simplified "is identifier part" check. */
-	if ((c >= (unsigned char) 'a' && c <= (unsigned char) 'z') ||
-	    (c >= (unsigned char) 'A' && c <= (unsigned char) 'Z') ||
-	    (c >= (unsigned char) '0' && c <= (unsigned char) '9') ||
-	    c == (unsigned char) '$' || c == (unsigned char) '_') {
-		return 1;
-	}
-	return 0;
-}
 
-static int completion_digit(unsigned char c) {
-	return (c >= (unsigned char) '0' && c <= (unsigned char) '9');
-}
+static const char *linenoise_completion_script =
+	"(function linenoiseCompletion(input, addCompletion) {\n"
+	"    // Find maximal trailing string which looks like a property\n"
+	"    // access.  Look up all the components (starting from the global\n"
+	"    // object now) except the last; treat the last component as a\n"
+	"    // partial name and use it as a filter for possible properties.\n"
+	"    var match, propseq, obj, i, partial, names, name;\n"
+	"\n"
+	"    if (!input) { return; }\n"
+	"    match = /^.*?((?:\\w+\\.)*\\w*)$/.exec(input);\n"
+	"    if (!match || !match[1]) { return; }\n"
+	"    var propseq = match[1].split('.');\n"
+	"\n"
+	"    obj = Function('return this')();\n"
+	"    for (i = 0; i < propseq.length - 1; i++) {\n"
+	"        if (obj === void 0 || obj === null) { return; }\n"
+	"        obj = obj[propseq[i]];\n"
+	"    }\n"
+	"    if (obj === void 0 || obj === null) { return; }\n"
+	"\n"
+	"    partial = propseq[propseq.length - 1];\n"
+	"    while (obj != null) {\n"
+	"        names = Object.getOwnPropertyNames(Object(obj));\n"
+	"        for (i = 0; i < names.length; i++) {\n"
+	"            name = names[i];\n"
+	"            if (Number(name) >= 0) { continue; }  // ignore array keys\n"
+	"            if (name.substring(0, partial.length) !== partial) { continue; }\n"
+	"            if (name === partial) { addCompletion(input + '.'); continue; }\n"
+	"            addCompletion(input + name.substring(partial.length));\n"
+	"        }\n"
+	"        obj = Object.getPrototypeOf(Object(obj));\n"
+	"    }\n"
+	"})";
 
-static duk_ret_t linenoise_completion_lookup(duk_context *ctx, void *udata) {
-	duk_size_t len;
-	const char *orig;
-	const unsigned char *p;
-	const unsigned char *p_curr;
-	const unsigned char *p_end;
-	const char *key;
-	const char *prefix;
+static const char *linenoise_hints_script =
+	"(function linenoiseHints(input) {\n"
+	"    // Similar to completions but different handling for final results.\n"
+	"    var match, propseq, obj, i, partial, names, name, results;\n"
+	"\n"
+	"    if (!input) { return; }\n"
+	"    match = /^.*?((?:\\w+\\.)*\\w*)$/.exec(input);\n"
+	"    if (!match || !match[1]) { return; }\n"
+	"    var propseq = match[1].split('.');\n"
+	"\n"
+	"    obj = Function('return this')();\n"
+	"    for (i = 0; i < propseq.length - 1; i++) {\n"
+	"        if (obj === void 0 || obj === null) { return; }\n"
+	"        obj = obj[propseq[i]];\n"
+	"    }\n"
+	"    if (obj === void 0 || obj === null) { return; }\n"
+	"\n"
+	"    partial = propseq[propseq.length - 1];\n"
+	"    results = [];\n"
+	"    while (obj != null) {\n"
+	"        names = Object.getOwnPropertyNames(Object(obj));\n"
+	"        for (i = 0; i < names.length; i++) {\n"
+	"            name = names[i];\n"
+	"            if (Number(name) >= 0) { continue; }  // ignore array keys\n"
+	"            if (name.substring(0, partial.length) !== partial) { continue; }\n"
+	"            if (name === partial) { continue; }\n"
+	"            results.push(results.length === 0 ? name.substring(partial.length) : name);\n"
+	"        }\n"
+	"        obj = Object.getPrototypeOf(Object(obj));\n"
+	"    }\n"
+	"    return { hints: results.join(' | '), color: 35, bold: 1 };\n"
+	"})";
+
+static duk_ret_t linenoise_add_completion(duk_context *ctx) {
 	linenoiseCompletions *lc;
-	duk_idx_t idx_obj;
 
-	(void) udata;
-
-	orig = duk_require_string(ctx, -3);
-	p_curr = (const unsigned char *) duk_require_lstring(ctx, -2, &len);
-	p_end = p_curr + len;
+	duk_push_current_function(ctx);
+	duk_get_prop_string(ctx, -1, "lc");
 	lc = duk_require_pointer(ctx, -1);
 
-	duk_push_global_object(ctx);
-	idx_obj = duk_require_top_index(ctx);
+	linenoiseAddCompletion(lc, duk_require_string(ctx, 0));
+	return 0;
+}
 
-	while (p_curr <= p_end) {
-		/* p_curr == p_end allowed on purpose, to handle 'Math.' for example. */
-		p = p_curr;
-		while (p < p_end && p[0] != (unsigned char) '.') {
-			p++;
-		}
-		/* 'p' points to a NUL (p == p_end) or a period. */
-		prefix = duk_push_lstring(ctx, (const char *) p_curr, (duk_size_t) (p - p_curr));
+static char *linenoise_hints(const char *buf, int *color, int *bold) {
+	duk_context *ctx;
+	duk_int_t rc;
 
-#if 0
-		fprintf(stderr, "Completion check: '%s'\n", prefix);
-		fflush(stderr);
-#endif
-
-		if (p == p_end) {
-			/* 'idx_obj' points to the object matching the last
-			 * full component, use [p_curr,p[ as a filter for
-			 * that object.
-			 */
-
-			duk_enum(ctx, idx_obj, DUK_ENUM_INCLUDE_NONENUMERABLE);
-			while (duk_next(ctx, -1, 0 /*get_value*/)) {
-				key = duk_get_string(ctx, -1);
-#if 0
-				fprintf(stderr, "Key: %s\n", key ? key : "");
-				fflush(stderr);
-#endif
-				if (!key) {
-					/* Should never happen, just in case. */
-					goto next;
-				}
-
-				/* Ignore array index keys: usually not desirable, and would
-				 * also require ['0'] quoting.
-				 */
-				if (completion_digit(key[0])) {
-					goto next;
-				}
-
-				/* XXX: There's no key quoting now, it would require replacing the
-				 * last component with a ['foo\nbar'] style lookup when appropriate.
-				 */
-
-				if (strlen(prefix) == 0) {
-					/* Partial ends in a period, e.g. 'Math.' -> complete all Math properties. */
-					duk_push_string(ctx, orig);  /* original, e.g. 'Math.' */
-					duk_push_string(ctx, key);
-					duk_concat(ctx, 2);
-					linenoiseAddCompletion(lc, duk_require_string(ctx, -1));
-					duk_pop(ctx);
-				} else if (prefix && strcmp(key, prefix) == 0) {
-					/* Full completion, add a period, e.g. input 'Math' -> 'Math.'. */
-					duk_push_string(ctx, orig);  /* original, including partial last component */
-					duk_push_string(ctx, ".");
-					duk_concat(ctx, 2);
-					linenoiseAddCompletion(lc, duk_require_string(ctx, -1));
-					duk_pop(ctx);
-				} else if (prefix && strncmp(key, prefix, strlen(prefix)) == 0) {
-					/* Last component is partial, complete. */
-					duk_push_string(ctx, orig);  /* original, including partial last component */
-					duk_push_string(ctx, key + strlen(prefix));  /* completion to last component */
-					duk_concat(ctx, 2);
-					linenoiseAddCompletion(lc, duk_require_string(ctx, -1));
-					duk_pop(ctx);
-				}
-
-			 next:
-				duk_pop(ctx);
-			}
-			return 0;
-		} else {
-			if (duk_get_prop(ctx, idx_obj)) {
-				duk_to_object(ctx, -1);  /* for properties of plain strings etc */
-				duk_replace(ctx, idx_obj);
-				p_curr = p + 1;
-			} else {
-				/* Not found. */
-				return 0;
-			}
-		}
+	ctx = completion_ctx;
+	if (!ctx) {
+		return NULL;
 	}
 
-	return 0;
+	duk_push_global_stash(ctx);
+	duk_get_prop_string(ctx, -1, "linenoiseHints");
+	if (!buf) {
+		duk_push_undefined(ctx);
+	} else {
+		duk_push_string(ctx, buf);
+	}
+
+	rc = duk_pcall(ctx, 1 /*nargs*/);  /* [ stash func ] -> [ stash result ] */
+	if (rc != 0) {
+		const char *res;
+		res = strdup(duk_safe_to_string(ctx, -1));
+		*color = 31;  /* red */
+		*bold = 1;
+		duk_pop_2(ctx);
+		return (char *) res;
+	}
+
+	if (duk_is_object(ctx, -1)) {
+		const char *tmp;
+		const char *res = NULL;
+
+		duk_get_prop_string(ctx, -1, "hints");
+		tmp = duk_get_string(ctx, -1);
+		if (tmp) {
+			res = strdup(tmp);
+		}
+		duk_pop(ctx);
+
+		duk_get_prop_string(ctx, -1, "color");
+		*color = duk_to_int(ctx, -1);
+		duk_pop(ctx);
+
+		duk_get_prop_string(ctx, -1, "bold");
+		*bold = duk_to_int(ctx, -1);
+		duk_pop(ctx);
+
+		duk_pop_2(ctx);
+		return (char *) res;
+	}
+
+	duk_pop_2(ctx);
+	return NULL;
+}
+
+static void linenoise_freehints(void *ptr) {
+#if 0
+	printf("free hint: %p\n", (void *) ptr);
+#endif
+	free(ptr);
 }
 
 static void linenoise_completion(const char *buf, linenoiseCompletions *lc) {
 	duk_context *ctx;
-	const unsigned char *p_start;
-	const unsigned char *p_end;
-	const unsigned char *p;
 	duk_int_t rc;
 
-	if (!buf) {
-		return;
-	}
 	ctx = completion_ctx;
 	if (!ctx) {
 		return;
 	}
 
-	p_start = (const unsigned char *) buf;
-	p_end = (const unsigned char *) (buf + strlen(buf));
-	p = p_end;
+	duk_push_global_stash(ctx);
+	duk_get_prop_string(ctx, -1, "linenoiseCompletion");
 
-	/* Scan backwards for a maximal string which looks like a property
-	 * chain (e.g. foo.bar.quux).
-	 */
-
-	while (--p >= p_start) {
-		if (p[0] == (unsigned char) '.') {
-			if (p <= p_start) {
-				break;
-			}
-			if (!completion_idpart(p[-1])) {
-				/* Catches e.g. 'foo..bar' -> we want 'bar' only. */
-				break;
-			}
-		} else if (!completion_idpart(p[0])) {
-			break;
-		}
+	if (!buf) {
+		duk_push_undefined(ctx);
+	} else {
+		duk_push_string(ctx, buf);
 	}
-	/* 'p' will either be p_start - 1 (ran out of buffer) or point to
-	 * the first offending character.
-	 */
-	p++;
-	if (p < p_start || p >= p_end) {
-		return;  /* should never happen, but just in case */
-	}
-
-	/* 'p' now points to a string of the form 'foo.bar.quux'.  Look up
-	 * all the components except the last; treat the last component as
-	 * a partial name which is used as a filter for the previous full
-	 * component.  All lookups are from the global object now.
-	 */
-
-#if 0
-	fprintf(stderr, "Completion starting point: '%s'\n", p);
-	fflush(stderr);
-#endif
-
-	duk_push_string(ctx, (const char *) buf);
-	duk_push_lstring(ctx, (const char *) p, (duk_size_t) (p_end - p));
+	duk_push_c_function(ctx, linenoise_add_completion, 2 /*nargs*/);
 	duk_push_pointer(ctx, (void *) lc);
+	duk_put_prop_string(ctx, -2, "lc");
 
-	rc = duk_safe_call(ctx, linenoise_completion_lookup, NULL /*udata*/, 3 /*nargs*/, 1 /*nrets*/);
-	if (rc != DUK_EXEC_SUCCESS) {
-		fprintf(stderr, "Completion handling failure: %s\n", duk_safe_to_string(ctx, -1));
+	rc = duk_pcall(ctx, 2 /*nargs*/);  /* [ stash func callback ] -> [ stash result ] */
+	if (rc != 0) {
+		linenoiseAddCompletion(lc, duk_safe_to_string(ctx, -1));
 	}
-	duk_pop(ctx);
+	duk_pop_2(ctx);
 }
 #endif  /* DUK_CMDLINE_LINENOISE_COMPLETION */
 
@@ -744,6 +725,14 @@ static int handle_interactive(duk_context *ctx) {
 	linenoiseHistorySetMaxLen(64);
 #if defined(DUK_CMDLINE_LINENOISE_COMPLETION)
 	linenoiseSetCompletionCallback(linenoise_completion);
+	linenoiseSetHintsCallback(linenoise_hints);
+	linenoiseSetFreeHintsCallback(linenoise_freehints);
+	duk_push_global_stash(ctx);
+	duk_eval_string(ctx, linenoise_completion_script);
+	duk_put_prop_string(ctx, -2, "linenoiseCompletion");
+	duk_eval_string(ctx, linenoise_hints_script);
+	duk_put_prop_string(ctx, -2, "linenoiseHints");
+	duk_pop(ctx);
 #endif
 
 	for (;;) {
