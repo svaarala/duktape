55800e85ecee1f0cab0de22cc5d73270808c169c
Sami Vaarala <sami.vaarala@iki.fi>
2016-11-16 04:12:12 +0200

    Cover 0o123 and 0b10001 in ToNumber() test
    Also some other coverage improvements.

--- tests/ecmascript/test-conv-tonumber.js
+++ tests/ecmascript/test-conv-tonumber.js
@@ -54,30 +54,69 @@ test(Number.NEGATIVE_INFINITY);
 
 /*===
 -1 nz
+-1 nz
 0 neg
 0 pos
 0 pos
 1 nz
+17 nz
+19 nz
 -Infinity nz
 Infinity nz
 Infinity nz
 3735928559 nz
+3735928559 nz
+3735928559 nz
+NaN nz
 ===*/
 
 /* String to number */
 
-test("-1");
-test("-0");
-test("+0");
-test("0");
-test("1");
+test('-1');
+test('   -1    ');  // whitespace is trimmed
+test('-0');
+test('+0');
+test('0');
+test('1');
+test('000017');  // lead zeroes allowed, not interpreted as octal!
+test('000019');
 
-test("-Infinity");
-test("+Infinity");
-test("Infinity");
+test('  -Infinity');
+test(' +Infinity');
+test('   Infinity');
 
-test("0xdeadbeef");
+test('0xdeadbeef');
+test('0Xdeadbeef');
+test('    \n0xdeadbeef\n');
+test('    \n0xdeadbeefg\n');  // trailing garbage not allowed -> NaN
 
-/* XXX: octal */
+/*===
+83 nz
+83 nz
+83 nz
+83 nz
+NaN nz
+17 nz
+17 nz
+17 nz
+17 nz
+NaN nz
+===*/
+
+/* In ES6 0o123, and 0b10001 are recognized.  Whitespace is also allowed.
+ * Note that whitespace is trimmed so these forms cannot be recognized
+ * by just peeking at the first few characters.
+ */
+
+test('0o123');
+test('0O123');
+test('\n0o123  \t');
+test('\n0o000000123  \t');
+test('\n0o000000123  x\t');
+test('0b10001');
+test('0B10001');
+test('\t0b10001  \r\n');
+test('\t0b0000010001  \r\n');
+test('\t0b00000000000100012  \r\n');  // '2' is garbage to binary
 
 /* XXX: object coercion */
