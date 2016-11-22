f45c3fa862983ae5775c12a2b5f6364db2438695
Sami Vaarala <sami.vaarala@iki.fi>
2016-11-22 17:08:07 +0200

    Add testcase for log2(2^N) for full N range

---
+++ tests/ecmascript/test-bi-math-log2-2n.js
@@ -0,0 +1,33 @@
+/*
+ *  While not required by ES6, Math.log2(2^N) should return exactly N, at least
+ *  with common C99 log2() implementations.
+ */
+
+/*===
+positive exponents done
+negative exponents done
+===*/
+
+function test() {
+    var expect, x;
+
+    for (expect = 0, x = 1; x !== 1/0; x *= 2, expect++) {
+        if (Math.log2(x) != expect) {
+            console.log('FAIL', x, Math.log2(x));
+        }
+    }
+    console.log('positive exponents done');
+
+    for (expect = 0, x = 1; x !== 0; x /= 2, expect--) {
+        if (Math.log2(x) != expect) {
+            console.log('FAIL', x, Math.log2(x));
+        }
+    }
+    console.log('negative exponents done');
+}
+
+try {
+    test();
+} catch (e) {
+    console.log(e.stack || e);
+}
