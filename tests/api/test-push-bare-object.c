0a80b48c12d35cbf6e9d84b9799992bfa754ef45
Sami Vaarala <sami.vaarala@iki.fi>
2016-12-01 23:09:12 +0200

    Testcase updates for duk_push_bare_object()

--- tests/api/test-push-bare-object.c
+++ tests/api/test-push-bare-object.c
@@ -1,6 +1,6 @@
 /*===
 duk_is_object(1) = 1
-.toString rc=1 -> function toString() {"native"}
+.toString rc=0 -> undefined
 json encoded: {"meaningOfLife":42}
 top=2
 ===*/
@@ -11,11 +11,9 @@ void test(duk_context *ctx) {
 
 	duk_push_int(ctx, 123);  /* dummy */
 
-	obj_idx = duk_push_object(ctx);
+	obj_idx = duk_push_bare_object(ctx);
 	duk_push_int(ctx, 42);
 	duk_put_prop_string(ctx, obj_idx, "meaningOfLife");
-
-	/* object is now: { "meaningOfLife": 42 } */
 
 	printf("duk_is_object(%ld) = %d\n", (long) obj_idx, (int) duk_is_object(ctx, obj_idx));
 
