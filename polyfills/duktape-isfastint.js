e2c2efc7d38e4de0b9af7dfc87ff4deedab42cd8
Sami Vaarala <sami.vaarala@iki.fi>
2016-12-06 21:21:08 +0200

    Fix isFastint() polyfill to use revised info()

--- polyfills/duktape-isfastint.js
+++ polyfills/duktape-isfastint.js
@@ -16,7 +16,7 @@ if (typeof Duktape === 'object') {
     if (typeof Duktape.fastintTag === 'undefined') {
         Object.defineProperty(Duktape, 'fastintTag', {
             /* Tag number depends on duk_tval packing. */
-            value: (Duktape.info(true)[1] >= 0xfff0) ?
+            value: (Duktape.info(true).itag >= 0xfff0) ?
                     0xfff1 /* tag for packed duk_tval */ :
                     1 /* tag for unpacked duk_tval */,
             writable: false,
@@ -27,8 +27,8 @@ if (typeof Duktape === 'object') {
     if (typeof Duktape.isFastint === 'undefined') {
         Object.defineProperty(Duktape, 'isFastint', {
             value: function (v) {
-                return Duktape.info(v)[0] === 4 &&                 /* public type is DUK_TYPE_NUMBER */
-                       Duktape.info(v)[1] === Duktape.fastintTag;  /* internal tag is fastint */
+                return Duktape.info(v).type === 4 &&                 /* public type is DUK_TYPE_NUMBER */
+                       Duktape.info(v).itag === Duktape.fastintTag;  /* internal tag is fastint */
             },
             writable: false,
             enumerable: false,
