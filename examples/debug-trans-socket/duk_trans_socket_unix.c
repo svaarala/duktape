bf406169c39156e35225793a9efb73b3cfa3ab58
kolban <kolban@us.ibm.com>
2016-12-19 12:25:59 -0600

    Implementation of #1200

--- examples/debug-trans-socket/duk_trans_socket_unix.c
+++ examples/debug-trans-socket/duk_trans_socket_unix.c
@@ -5,12 +5,28 @@
  *  After that data is just passed through.
  */
 
+/*
+ * The select() vs poll() story.  The two system calls called
+ * "select()" and "poll()" are similar but not necessarily available
+ * on all platforms.  The default implementation is to use "poll()"
+ * but we can switch that by defining "USE_SELECT" within this
+ * file.  For example:
+ * 
+ * #define USE_SELECT
+ * 
+ * If set, instead of leveraging "poll()", this code will leverage
+ * "select()".   Discussions on "poll()" vs "select()" can be read
+ * about here: https://daniel.haxx.se/docs/poll-vs-select.html
+ * 
+ */
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#ifndef USE_SELECT
 #include <poll.h>
+#endif
 #include <errno.h>
 #include "duktape.h"
 
@@ -253,8 +269,12 @@ duk_size_t duk_trans_socket_write_cb(void *udata, const char *buffer, duk_size_t
 }
 
 duk_size_t duk_trans_socket_peek_cb(void *udata) {
+#ifdef USE_SELECT
+	fd_set rfds;
+#else
 	struct pollfd fds[1];
 	int poll_rc;
+#endif
 
 	(void) udata;  /* not needed by the example */
 
@@ -266,7 +286,20 @@ duk_size_t duk_trans_socket_peek_cb(void *udata) {
 	if (client_sock < 0) {
 		return 0;
 	}
-
+#ifdef USE_SELECT
+	FD_ZERO(&rfds);
+	FD_SET(client_sock, &rfds);
+	struct timeval tm;
+	tm.tv_sec = tm.tv_usec = 0;
+	int selectRc = select(client_sock+1, &rfds, NULL, NULL, &tm);
+	if (selectRc == 0) {
+		return 0;
+	}
+	if (selectRc == 1) {
+		return 1;
+	}
+	goto fail;
+#else
 	fds[0].fd = client_sock;
 	fds[0].events = POLLIN;
 	fds[0].revents = 0;
@@ -287,7 +320,7 @@ duk_size_t duk_trans_socket_peek_cb(void *udata) {
 	} else {
 		return 1;  /* something to read */
 	}
-
+#endif
  fail:
 	if (client_sock >= 0) {
 		(void) close(client_sock);
