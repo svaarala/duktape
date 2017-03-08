22966b96dd44e58180e0196a596d7298d8258b40
Sami Vaarala <sami.vaarala@iki.fi>
2017-03-08 23:42:07 +0200

    Use _snprintf() prior to MSVC 2015

--- extras/module-duktape/duk_module_duktape.c
+++ extras/module-duktape/duk_module_duktape.c
@@ -5,6 +5,14 @@
 #include "duktape.h"
 #include "duk_module_duktape.h"
 
+/* (v)snprintf() is missing before MSVC 2015.  Note that _(v)snprintf() does
+ * NOT NUL terminate on truncation, but that's OK here.
+ * http://stackoverflow.com/questions/2915672/snprintf-and-visual-studio-2010
+ */
+#if defined(_MSC_VER) && (_MSC_VER < 1900)
+#define snprintf _snprintf
+#endif
+
 #if 0  /* Enable manually */
 #define DUK__ASSERT(x) do { \
 		if (!(x)) { \
