e11753082ff64d4e4cd06af3d0f592c815d2c007
Sami Vaarala <sami.vaarala@iki.fi>
2017-03-29 06:10:50 +0300

    Use DUK_COMPILE_xxx flags are internally
    Avoids unnecessary translation.

--- src-input/duk_bi_function.c
+++ src-input/duk_bi_function.c
@@ -61,7 +61,7 @@ DUK_INTERNAL duk_ret_t duk_bi_function_constructor(duk_context *ctx) {
 	DUK_ASSERT_TOP(ctx, 3);
 
 	/* strictness is not inherited, intentional */
-	comp_flags = DUK_JS_COMPILE_FLAG_FUNCEXPR;
+	comp_flags = DUK_COMPILE_FUNCEXPR;
 
 	duk_push_hstring_stridx(ctx, DUK_STRIDX_COMPILE);  /* XXX: copy from caller? */  /* XXX: ignored now */
 	h_sourcecode = duk_require_hstring(ctx, -2);  /* no symbol check needed; -2 is concat'd code */
