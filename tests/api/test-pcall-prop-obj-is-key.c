44518b6cb9b46f43231305826723566e3f6b3347
Sami Vaarala <sami.vaarala@iki.fi>
2017-06-12 03:11:45 +0300

    Improve testcases for protected call invalid args

---
+++ tests/api/test-pcall-prop-obj-is-key.c
@@ -0,0 +1,93 @@
+/*
+ *  duk_pcall_prop() allows a situation where the nargs count is such that
+ *  the target object is identified as the key or even part of the argument
+ *  list.
+ *
+ *  This behavior is not very useful but test for the current behavior.
+ */
+
+/*===
+*** test_obj_is_key (duk_safe_call)
+top before: 4
+toString() called
+myProp called
+foo bar
+duk_pcall_prop() rc: 0
+final top: 1
+==> rc=0, result='undefined'
+*** test_obj_is_arg (duk_safe_call)
+top before: 5
+myProp called
+toString() called
+myProp foo
+duk_pcall_prop() rc: 0
+final top: 1
+==> rc=0, result='undefined'
+===*/
+
+static duk_ret_t test_obj_is_key(duk_context *ctx) {
+	duk_int_t rc;
+
+	duk_eval_string(ctx,
+		"({\n"
+		"    toString: function () { print('toString() called'); return 'myProp' },\n"
+		"    myProp: function (a, b) { print('myProp called'); print(a, b); }\n"
+		"})\n");
+
+	duk_push_string(ctx, "foo");
+	duk_push_string(ctx, "bar");
+	duk_push_string(ctx, "quux");
+
+	/* [ obj 'foo' 'bar' 'quux' ]
+	 *
+	 *  <key> <--- args ------>
+	 *
+	 *   ^
+	 *   `-- obj_idx
+	 */
+
+	printf("top before: %ld\n", (long) duk_get_top(ctx));
+
+	rc = duk_pcall_prop(ctx, -4, 3 /*nargs */);
+	printf("duk_pcall_prop() rc: %ld\n", (long) rc);
+
+	printf("final top: %ld\n", (long) duk_get_top(ctx));
+	return 0;
+}
+
+static duk_ret_t test_obj_is_arg(duk_context *ctx) {
+	duk_int_t rc;
+
+	duk_push_string(ctx, "myProp");
+
+	duk_eval_string(ctx,
+		"({\n"
+		"    toString: function () { print('toString() called'); return 'myProp' },\n"
+		"    myProp: function (a, b) { print('myProp called'); print(a, b); }\n"
+		"})\n");
+
+	duk_push_string(ctx, "foo");
+	duk_push_string(ctx, "bar");
+	duk_push_string(ctx, "quux");
+
+	/* [ 'myProp' obj 'foo' 'bar' 'quux' ]
+	 *
+	 *    <key>   <------- args ------>
+	 *
+	 *             ^
+	 *             `-- obj_idx
+	 */
+
+	printf("top before: %ld\n", (long) duk_get_top(ctx));
+
+	rc = duk_pcall_prop(ctx, -4, 4 /*nargs */);
+	printf("duk_pcall_prop() rc: %ld\n", (long) rc);
+
+	printf("final top: %ld\n", (long) duk_get_top(ctx));
+	return 0;
+}
+
+void test(duk_context *ctx) {
+	TEST_SAFE_CALL(test_obj_is_key);
+	TEST_SAFE_CALL(test_obj_is_arg);
+}
---
+++ tests/api/test-safe-call-invalid-nrets.c
@@ -0,0 +1,84 @@
+/*
+ *  duk_safe_call() with invalid nrets
+ */
+
+/*===
+*** test_nrets_too_large (duk_safe_call)
+top before: 3
+duk_safe_call rc: 0
+final top: 1025
+==> rc=0, result='undefined'
+*** test_nrets_too_large_fixed (duk_safe_call)
+top before: 3
+duk_safe_call rc: 0
+final top: 1025
+==> rc=0, result='undefined'
+*** test_nrets_negative (duk_safe_call)
+top before: 3
+==> rc=1, result='Error: invalid call args'
+===*/
+
+static duk_ret_t dummy(duk_context *ctx) {
+	(void) ctx;
+	return 0;
+}
+
+static duk_ret_t test_nrets_too_large(duk_context *ctx) {
+	duk_int_t rc;
+
+	duk_push_null(ctx);
+	duk_push_null(ctx);
+	duk_push_null(ctx);
+	printf("top before: %ld\n", (long) duk_get_top(ctx));
+
+	/* Here 'nrets' is too large, beyond the current value stack
+	 * reserve.  (This succeeds in Duktape 2.1 and prior because
+	 * the implementation did a reserve check.)
+	 */
+	rc = duk_safe_call(ctx, dummy, 2 /*nargs*/, 1024 /*nrets, too large*/);
+	printf("duk_safe_call rc: %ld\n", (long) rc);
+
+	printf("final top: %ld\n", (long) duk_get_top(ctx));
+	return 0;
+}
+
+static duk_ret_t test_nrets_too_large_fixed(duk_context *ctx) {
+	duk_int_t rc;
+
+	duk_push_null(ctx);
+	duk_push_null(ctx);
+	duk_push_null(ctx);
+	printf("top before: %ld\n", (long) duk_get_top(ctx));
+
+	/* Here duk_check_stack() is used to extend the value stack
+	 * reserve to accommodate 'nrets'.  The required reserve is:
+	 *     current_top - nargs + nrets
+	 * which may be negative if nargs >= nrets.
+	 */
+	rc = duk_check_stack(ctx, duk_get_top(ctx) - 2 + 1024);
+	rc = duk_safe_call(ctx, dummy, 2 /*nargs*/, 1024 /*nrets, too large*/);
+	printf("duk_safe_call rc: %ld\n", (long) rc);
+
+	printf("final top: %ld\n", (long) duk_get_top(ctx));
+	return 0;
+}
+static duk_ret_t test_nrets_negative(duk_context *ctx) {
+	duk_int_t rc;
+
+	duk_push_null(ctx);
+	duk_push_null(ctx);
+	duk_push_null(ctx);
+	printf("top before: %ld\n", (long) duk_get_top(ctx));
+
+	rc = duk_safe_call(ctx, dummy, 2 /*nargs*/, -1 /*nrets, negative*/);
+	printf("duk_safe_call rc: %ld\n", (long) rc);
+
+	printf("final top: %ld\n", (long) duk_get_top(ctx));
+	return 0;
+}
+
+void test(duk_context *ctx) {
+	TEST_SAFE_CALL(test_nrets_too_large);
+	TEST_SAFE_CALL(test_nrets_too_large_fixed);
+	TEST_SAFE_CALL(test_nrets_negative);
+}
