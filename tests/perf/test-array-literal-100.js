ba197aea0fd9ab30b23c09de9fca4f28e96f29eb
Sami Vaarala <sami.vaarala@iki.fi>
2017-07-01 16:34:08 +0300

    Perf tests for array literals

---
+++ tests/perf/test-array-literal-100.js
@@ -0,0 +1,40 @@
+/*
+ *  Create Array using a literal
+ */
+
+if (typeof print !== 'function') { print = console.log; }
+
+function test() {
+    var arr;
+    var i;
+
+    for (i = 0; i < 1e6; i++) {
+        arr = [ 'foo', 'bar', 'quux', 'baz', 'quuux',
+                '1', '2', '3', '4', '5',
+                'foo', 'bar', 'quux', 'baz', 'quuux',
+                '1', '2', '3', '4', '5',
+                'foo', 'bar', 'quux', 'baz', 'quuux',
+                '1', '2', '3', '4', '5',
+                'foo', 'bar', 'quux', 'baz', 'quuux',
+                '1', '2', '3', '4', '5',
+                'foo', 'bar', 'quux', 'baz', 'quuux',
+                '1', '2', '3', '4', '5',
+                'foo', 'bar', 'quux', 'baz', 'quuux',
+                '1', '2', '3', '4', '5',
+                'foo', 'bar', 'quux', 'baz', 'quuux',
+                '1', '2', '3', '4', '5',
+                'foo', 'bar', 'quux', 'baz', 'quuux',
+                '1', '2', '3', '4', '5',
+                'foo', 'bar', 'quux', 'baz', 'quuux',
+                '1', '2', '3', '4', '5',
+                'foo', 'bar', 'quux', 'baz', 'quuux',
+                '1', '2', '3', '4', '5' ];
+    }
+}
+
+try {
+    test();
+} catch (e) {
+    print(e.stack || e);
+    throw e;
+}
---
+++ tests/perf/test-array-literal-3.js
@@ -0,0 +1,21 @@
+/*
+ *  Create Array using a literal
+ */
+
+if (typeof print !== 'function') { print = console.log; }
+
+function test() {
+    var arr;
+    var i;
+
+    for (i = 0; i < 1e6; i++) {
+        arr = [ 'foo', 'bar', 'quux' ];
+    }
+}
+
+try {
+    test();
+} catch (e) {
+    print(e.stack || e);
+    throw e;
+}
