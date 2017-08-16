9764a57ea223eb45495ccf130a394032cc1f8ce6
Sami Vaarala <sami.vaarala@iki.fi>
2017-08-16 23:30:00 +0300

    Testcase for debugger StepOut + tailcall bug

---
+++ tests/ecmascript/test-dev-debugger-stepout-tailcall.js
@@ -0,0 +1,42 @@
+/*
+ *  Test illustrating GH-1684.
+ *
+ *  Run with debugger attached, and StepOut from test1 and test2.
+ */
+
+/*===
+entered test1
+entered anotherFunction
+entered test2
+entered anotherFunction
+done
+===*/
+
+function anotherFunction() {
+    var ret;
+    print('entered anotherFunction');
+    ret = 'foo' + 123;
+    return ret;
+}
+
+function test1() {
+    print('entered test1');
+    return anotherFunction();
+}
+
+function test2() {
+    var ret;
+
+    print('entered test2');
+    ret = anotherFunction();
+    return ret;
+}
+
+try {
+    test1();
+    test2();
+} catch (e) {
+    print(e.stack || e);
+}
+
+print('done');
