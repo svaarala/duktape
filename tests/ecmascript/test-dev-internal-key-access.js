026ed087856ce8c9fd536ff04fff2000a0844c3f
Sami Vaarala <sami.vaarala@iki.fi>
2017-09-12 22:22:18 +0300

    Testcase fixes for \x82 internal prefix

--- tests/ecmascript/test-dev-internal-key-access.js
+++ tests/ecmascript/test-dev-internal-key-access.js
@@ -19,12 +19,12 @@
 
 /*===
 date: 1970-01-01T00:02:03.456Z
-using ArrayBuffer, date \xFFValue: 123456
-using Duktape.dec, date \xFFValue: 123456
+using ArrayBuffer, date \x82Value: 123456
+using Duktape.dec, date \x82Value: 123456
 ===*/
 
 function test() {
-    var dt = new Date(123456);  // has internal property \xFFValue
+    var dt = new Date(123456);  // has internal property \x82Value
     var buf;
     var u8;
     var key;
@@ -34,18 +34,18 @@ function test() {
     // Using ArrayBuffer()
     buf = new ArrayBuffer(6);
     u8 = new Uint8Array(buf);
-    u8[0] = 0xff;
+    u8[0] = 0x82;
     u8[1] = 'V'.charCodeAt(0);
     u8[2] = 'a'.charCodeAt(0);
     u8[3] = 'l'.charCodeAt(0);
     u8[4] = 'u'.charCodeAt(0);
     u8[5] = 'e'.charCodeAt(0);
     key = bufferToStringRaw(buf);  // currently uses String.fromBufferRaw(), custom binding provided by 'duk'
-    print('using ArrayBuffer, date \\xFFValue:', dt[key]);
+    print('using ArrayBuffer, date \\x82Value:', dt[key]);
 
     // Using Duktape.dec()
-    key = bufferToStringRaw(Duktape.dec('hex', 'ff56616c7565'));  // \xFFValue
-    print('using Duktape.dec, date \\xFFValue:', dt[key]);
+    key = bufferToStringRaw(Duktape.dec('hex', '8256616c7565'));  // \x82Value
+    print('using Duktape.dec, date \\x82Value:', dt[key]);
 }
 
 try {
