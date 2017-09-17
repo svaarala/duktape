dc918c141bfa0b094cd3f63dfbcce09d8b882220
Sami Vaarala <sami.vaarala@iki.fi>
2017-09-17 23:28:00 +0300

    Add JSON output to time_multi.py

--- util/time_multi.py
+++ util/time_multi.py
@@ -5,28 +5,47 @@
 
 import os
 import sys
+import json
 import time
 import optparse
 import subprocess
 
 def main():
     parser = optparse.OptionParser()
-    parser.add_option('--count', type='int', dest='count', default=3)
-    parser.add_option('--mode', dest='mode', default='min')
-    parser.add_option('--sleep', type='float', dest='sleep', default=0.0)
-    parser.add_option('--sleep-factor', type='float', dest='sleep_factor', default=0.0)
-    parser.add_option('--rerun-limit', type='int', dest='rerun_limit', default=30)
-    parser.add_option('--verbose', action='store_true', dest='verbose', default=False)
+    parser.add_option('--count', type='int', dest='count', default=3, help='Number of test runs')
+    parser.add_option('--mode', dest='mode', default='min', help='Mode for choosing result: min, max, avg, all')
+    parser.add_option('--sleep', type='float', dest='sleep', default=0.0, help='Fixed sleep value between runs')
+    parser.add_option('--sleep-factor', type='float', dest='sleep_factor', default=0.0, help='Relative sleep value between runs, e.g. 2.0 means sleep twice as long as previous test run')
+    parser.add_option('--rerun-limit', type='int', dest='rerun_limit', default=30, help='Run test only once if test run time exceeds this time limit')
+    parser.add_option('--verbose', action='store_true', dest='verbose', default=False, help='Verbose output')
+    parser.add_option('--output', default=None, help='Output JSON file')
+
     (opts, args) = parser.parse_args()
 
     time_min = None
     time_max = None
     time_sum = 0.0
-    time_list = []
 
     if opts.verbose:
         sys.stderr.write('Running:')
         sys.stderr.flush()
+
+    doc = {
+        'count': opts.count,
+        'mode': opts.mode,
+        'sleep': opts.sleep,
+        'sleep_factor': opts.sleep_factor,
+        'rerun_limit': opts.rerun_limit,
+        'runs': [],
+        'args': args,
+        'time_list': [],
+        'time_min': None,
+        'time_max': None,
+        'time_avg': None
+    }
+
+    if opts.output is not None and os.path.exists(opts.output):
+        os.unlink(opts.output)
 
     for i in xrange(opts.count):
         time.sleep(opts.sleep)
@@ -43,12 +62,19 @@ def main():
         retval = p.wait()
         #print(i, retval, stdout, stderr)
 
+        run = {
+            'cmd': cmd,
+            'retval': retval
+        }
+        doc['runs'].append(run)
+
         if retval == 139:
-            print 'segv'
-            sys.exit(1)
+            doc['failed'] = True
+            doc['sigsegv'] = True
+            break
         elif retval != 0:
-            print 'n/a'
-            sys.exit(1)
+            doc['failed'] = True
+            break
 
         time_this = float(stderr)
         #print(i, time_this)
@@ -67,34 +93,53 @@ def main():
             sys.stderr.write(' %f' % time_this)
             sys.stderr.flush()
 
-        time_list.append(time_this)
+        doc['time_list'].append(time_this)
 
         # Sleep time dependent on test time is useful for thermal throttling.
-        time.sleep(opts.sleep_factor * time_this)
+        time_sleep = opts.sleep_factor * time_this + opts.sleep
+
+        run['time'] = time_this
+        run['sleep_time'] = time_sleep
+
+        time.sleep(time_sleep)
 
         # If run takes too long, there's no point in trying to get an accurate
         # estimate.
         if time_this >= opts.rerun_limit:
+            doc['rerun_limit_reached'] = True
             break
 
     if opts.verbose:
         sys.stderr.write('\n')
         sys.stderr.flush()
 
-    time_avg = time_sum / float(len(time_list))
-
     # /usr/bin/time has only two digits of resolution
-    if opts.mode == 'min':
-        print('%.02f' % time_min)
-    elif opts.mode == 'max':
-        print('%.02f' % time_max)
-    elif opts.mode == 'avg':
-        print('%.02f' % time_avg)
-    elif opts.mode == 'all':
-        print('min=%.02f, max=%.02f, avg=%0.2f, count=%d: %r' % \
-              (time_min, time_max, time_avg, len(time_list), time_list))
+    if doc.get('sigsegv', False):
+        print('segv')
+    elif doc.get('failed', False):
+        print('n/a')
     else:
-        print('invalid mode: %r' % opts.mode)
+        time_avg = time_sum / float(len(doc['time_list']))
+
+        doc['time_avg'] = time_avg
+        doc['time_min'] = time_min
+        doc['time_max'] = time_max
+
+        if opts.mode == 'min':
+            print('%.02f' % time_min)
+        elif opts.mode == 'max':
+            print('%.02f' % time_max)
+        elif opts.mode == 'avg':
+            print('%.02f' % time_avg)
+        elif opts.mode == 'all':
+            print('min=%.02f, max=%.02f, avg=%0.2f, count=%d: %r' % \
+                  (time_min, time_max, time_avg, len(doc['time_list']), doc['time_list']))
+        else:
+            print('invalid mode: %r' % opts.mode)
+
+    if opts.output is not None:
+        with open(opts.output, 'wb') as f:
+            f.write(json.dumps(doc, indent=4) + '\n')
 
     sys.exit(0)
 
