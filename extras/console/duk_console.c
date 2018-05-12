846d9674f0c8b939ad6da099d8d9ae412c176737
Sami Vaarala <sami.vaarala@iki.fi>
2018-05-12 12:38:00 +0300

    Rename console flags for clarity, other trivia

--- extras/console/duk_console.c
+++ extras/console/duk_console.c
@@ -22,7 +22,7 @@
 
 static duk_ret_t duk__console_log_helper(duk_context *ctx, const char *error_name) {
 	duk_uint_t flags = (duk_uint_t) duk_get_current_magic(ctx);
-	FILE *output = (flags & DUK_CONSOLE_TO_STDOUT) ? stdout : stderr;
+	FILE *output = (flags & DUK_CONSOLE_STDOUT_ONLY) ? stdout : stderr;
 	duk_idx_t n = duk_get_top(ctx);
 	duk_idx_t i;
 
@@ -106,11 +106,11 @@ static void duk__console_reg_vararg_func(duk_context *ctx, duk_c_function func, 
 void duk_console_init(duk_context *ctx, duk_uint_t flags) {
 	duk_uint_t flags_orig;
 
-	/* If both DUK_CONSOLE_TO_STDOUT and DUK_CONSOLE_TO_STDERR where specified,
-	 * just turn off DUK_CONSOLE_TO_STDOUT and keep DUK_CONSOLE_TO_STDERR. */
-	if ((flags & DUK_CONSOLE_TO_STDOUT) &&
-	    (flags & DUK_CONSOLE_TO_STDERR)) {
-	    flags &= ~DUK_CONSOLE_TO_STDOUT;
+	/* If both DUK_CONSOLE_STDOUT_ONLY and DUK_CONSOLE_STDERR_ONLY where specified,
+	 * just turn off DUK_CONSOLE_STDOUT_ONLY and keep DUK_CONSOLE_STDERR_ONLY.
+	 */
+	if ((flags & DUK_CONSOLE_STDOUT_ONLY) && (flags & DUK_CONSOLE_STDERR_ONLY)) {
+	    flags &= ~DUK_CONSOLE_STDOUT_ONLY;
 	}
 	/* Remember the (possibly corrected) flags we received. */
 	flags_orig = flags;
@@ -134,10 +134,9 @@ void duk_console_init(duk_context *ctx, duk_uint_t flags) {
 	duk_put_prop_string(ctx, -2, "format");
 
 	flags = flags_orig;
-	if (!(flags & DUK_CONSOLE_TO_STDOUT) &&
-	    !(flags & DUK_CONSOLE_TO_STDERR)) {
+	if (!(flags & DUK_CONSOLE_STDOUT_ONLY) && !(flags & DUK_CONSOLE_STDERR_ONLY)) {
 	    /* No output indicators were specified; these levels go to stdout. */
-	    flags |= DUK_CONSOLE_TO_STDOUT;
+	    flags |= DUK_CONSOLE_STDOUT_ONLY;
 	}
 	duk__console_reg_vararg_func(ctx, duk__console_assert, "assert", flags);
 	duk__console_reg_vararg_func(ctx, duk__console_log, "log", flags);
@@ -146,10 +145,9 @@ void duk_console_init(duk_context *ctx, duk_uint_t flags) {
 	duk__console_reg_vararg_func(ctx, duk__console_info, "info", flags);
 
 	flags = flags_orig;
-	if (!(flags & DUK_CONSOLE_TO_STDOUT) &&
-	    !(flags & DUK_CONSOLE_TO_STDERR)) {
+	if (!(flags & DUK_CONSOLE_STDOUT_ONLY) && !(flags & DUK_CONSOLE_STDERR_ONLY)) {
 	    /* No output indicators were specified; these levels go to stderr. */
-	    flags |= DUK_CONSOLE_TO_STDERR;
+	    flags |= DUK_CONSOLE_STDERR_ONLY;
 	}
 	duk__console_reg_vararg_func(ctx, duk__console_warn, "warn", flags);
 	duk__console_reg_vararg_func(ctx, duk__console_error, "error", flags);
