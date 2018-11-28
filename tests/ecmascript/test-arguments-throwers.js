a8af7cf3f9dc9a47e4c7f4208fa850d1044d80f5
Sami Vaarala <sami.vaarala@iki.fi>
2018-11-28 02:19:03 +0200

    Test updates for strict arguments.caller removal

--- tests/ecmascript/test-arguments-throwers.js
+++ tests/ecmascript/test-arguments-throwers.js
@@ -2,22 +2,19 @@
  *  E5 Section 10.6, main algorithm step 14 requires that a strict
  *  arguments object 'callee' and 'caller' throwers must be the specific
  *  thrower defined in E5 Section 13.2.3.
+ *
+ *  ES2017 removes the strict mode .caller thrower, test revised for ES2017.
  */
 
 /*===
 test get and set
+undefined
 object
+undefined
 object
-object
-object
 true
 true
 true
-true
-true
-true
-true
-TypeError
 TypeError
 ===*/
 
@@ -40,20 +37,9 @@ print(typeof pd4);
 
 // all of these should print true
 
-print(pd1.get === pd1.set);
 print(pd2.get === pd2.set);
-print(pd3.get === pd3.set);
+print(pd2.get === pd4.set);
 print(pd4.get === pd4.set);
-
-print(pd1.get === pd2.get);
-print(pd2.get === pd3.get);
-print(pd3.get === pd4.get);
-
-try {
-    pd1.get();
-} catch (e) {
-    print(e.name);
-}
 
 try {
     pd2.get();
