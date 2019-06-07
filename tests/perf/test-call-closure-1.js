4a6ee116f3a2773ba71e51b67f89259720ccf356
Sami Vaarala <sami.vaarala@iki.fi>
2019-06-07 03:09:59 +0300

    Perf test for scope object creation

---
+++ tests/perf/test-call-closure-1.js
@@ -0,0 +1,47 @@
+/*
+ *  Nested function causes an actual scope object to be
+ *  created and closed.
+ */
+
+if (typeof print !== 'function') { print = console.log; }
+
+function func() {
+    var v0 = true;
+    var v1 = true;
+    var v2 = true;
+    var v3 = true;
+    var v4 = true;
+    var v5 = true;
+    var v6 = true;
+    var v7 = true;
+    var v8 = true;
+    var v9 = true;
+
+    // This forces the the scope of 'func' to be preserved
+    // when func() is closed.
+    return function (x) { return eval(x); };
+}
+
+function test() {
+    var i;
+    var ign;
+
+    for (var i = 0; i < 1e5; i++) {
+        ign = func();
+        ign = func();
+        ign = func();
+        ign = func();
+        ign = func();
+        ign = func();
+        ign = func();
+        ign = func();
+        ign = func();
+        ign = func();
+    }
+}
+try {
+    test();
+} catch (e) {
+    print(e.stack || e);
+    throw e;
+}
