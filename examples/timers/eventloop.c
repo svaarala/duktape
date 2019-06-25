6711132032874c7c3b9c8901d5c3680e974b5561
TheBrokenRail <connor24nolan@live.com>
2019-06-25 11:48:01 -0400

    Wait for next timer

--- examples/timers/eventloop.c
+++ examples/timers/eventloop.c
@@ -4,6 +4,24 @@
 #include <stdint.h>
 #include <sys/time.h>
 
+#ifdef WIN32
+#include <windows.h>
+#else
+#define __USE_POSIX199309
+#include <time.h>
+#endif
+
+static void sleep_ms(int milliseconds) {
+#ifdef WIN32
+    Sleep(milliseconds);
+#else
+    struct timespec ts;
+    ts.tv_sec = milliseconds / 1000;
+    ts.tv_nsec = (milliseconds % 1000) * 1000000;
+    nanosleep(&ts, NULL);
+#endif
+}
+
 #include "duktape.h"
 #include "eventloop.h"
 
@@ -192,6 +210,8 @@ static void eventloop_start(duk_context *ctx) {
         t = find_nearest_timer();
         if (!t) {
             break;
+        } else {
+            sleep_ms(t->target - get_now());
         }
     }
 }
