f81ee6710b1e1bb267a3b5695ac36a5ea67262c3
Sami Vaarala <sami.vaarala@iki.fi>
2020-03-28 00:53:46 +0200

    Add JS-based configure utils

---
+++ src-tools/lib/configure/util.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { readFileUtf8, writeFileUtf8, pathJoin, copyFile } = require('../util/fs');
+const { stripLastNewline } = require('../util/string_util');
+
+function copyFiles(fileList, srcDir, dstDir) {
+    for (let fn of fileList.sort()) {
+        let srcFn = pathJoin(srcDir, fn);
+        let dstFn = pathJoin(dstDir, fn);
+        copyFile(srcFn, dstFn);
+    }
+}
+exports.copyFiles = copyFiles;
+
+function cBlockQuoteFile(fn) {
+    var data = stripLastNewline(readFileUtf8(fn));
+    var lines = data.split('\n').map(function (line) {
+        return ' *  ' + line.replace(/[^\x20-\x7e]/g, function (c) { return '\\u' + ('0000' + c.charCodeAt(0).toString(16)).substr(-4); });
+    });
+    return '/*\n' + lines.join('\n') + '\n */';
+}
+exports.cBlockQuoteFile = cBlockQuoteFile;
+
+function copyAndCQuote(srcFn, dstFn) {
+    var data = cBlockQuoteFile(srcFn);
+    writeFileUtf8(dstFn, data);
+}
+exports.copyAndCQuote = copyAndCQuote;
